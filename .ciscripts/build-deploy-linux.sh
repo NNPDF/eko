430abd9b1b7ba2cbdc379d87e9cbcabcd24d2bd3
Stefano Carrazza <stefano.carrazza@cern.ch>
2019-10-06 22:30:28 +0200

    avoiding conda build upload to nnpdf server

--- .ciscripts/build-deploy-linux.sh
+++ .ciscripts/build-deploy-linux.sh
@@ -16,29 +16,29 @@ if [ $? != 0 ]; then
 	exit 1
 fi
 
-if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != 'master'  ] && [ "$UPLOAD_NON_MASTER" == false ];
-then
-  	echo "
-Skiping upload because this is not master and you have not
-set the UPLOAD_NON_MASTER variable."
-	exit 0
-fi
+#if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != 'master'  ] && [ "$UPLOAD_NON_MASTER" == false ];
+#then
+#  	echo "
+#Skiping upload because this is not master and you have not
+#set the UPLOAD_NON_MASTER variable."
+#	exit 0
+#fi
+#
+##This seems to be needed for "artifacts" to work.
+#cp /root/miniconda3/conda-bld/linux-64/*.tar.bz2 .
 
-#This seems to be needed for "artifacts" to work.
-cp /root/miniconda3/conda-bld/linux-64/*.tar.bz2 .
-
-echo "Uploading package to the NNPDF server"
-KEY=$( mktemp )
-#This is defined in the Travis environment variables.
-echo "$NNPDF_SSH_KEY" | base64 --decode > "$KEY"
-
-scp -i "$KEY" -o StrictHostKeyChecking=no\
-    /root/miniconda3/conda-bld/linux-64/*.tar.bz2 \
-    dummy@packages.nnpdf.science:~/packages/conda-private/linux-64
-
-if [ $? == 0 ]; then
-	echo "Conda package upload suceeded"
-else
-	echo "Conda package upload failed"
-	exit 1
-fi
+#echo "Uploading package to the NNPDF server"
+#KEY=$( mktemp )
+##This is defined in the Travis environment variables.
+#echo "$NNPDF_SSH_KEY" | base64 --decode > "$KEY"
+#
+#scp -i "$KEY" -o StrictHostKeyChecking=no\
+#    /root/miniconda3/conda-bld/linux-64/*.tar.bz2 \
+#    dummy@packages.nnpdf.science:~/packages/conda-private/linux-64
+#
+#if [ $? == 0 ]; then
+#	echo "Conda package upload suceeded"
+#else
+#	echo "Conda package upload failed"
+#	exit 1
+#fi
