3ac4de02a94177d455b652ea9f5dd515eeed7402
juacrumar <juacrumar@gmail.com>
2019-10-25 01:03:53 +0200

    black it up;

--- src/eko/mellin.py
+++ src/eko/mellin.py
@@ -12,12 +12,12 @@ might be interesting.
 import numpy as np
 import scipy.integrate as integrate
 
-from eko import t_float,t_complex
+from eko import t_float, t_complex
 
 # TODO make this module numba/C-save
 
 # TODO replace inversion with something better? (t_float!)
-def inverse_mellin_transform(f, path, jac, x, cut : t_float = 0.):
+def inverse_mellin_transform(f, path, jac, x, cut: t_float = 0.0):
     """Inverse Mellin transformation
 
     Parameters
@@ -38,18 +38,21 @@ def inverse_mellin_transform(f, path, jac, x, cut : t_float = 0.):
     -------
       res : float
     """
+
     def integrand(u):
         pathu = path(u)
-        prefactor = t_complex(complex(0.0, -1/2/np.pi))
-        xexp = np.exp(- pathu*np.log(x))
-        fofn = f(pathu)*jac(u)
+        prefactor = t_complex(complex(0.0, -1 / 2 / np.pi))
+        xexp = np.exp(-pathu * np.log(x))
+        fofn = f(pathu) * jac(u)
         # integrate.quad can only do float, as it links to QUADPACK
-        result = np.real(prefactor*xexp*fofn)
+        result = np.real(prefactor * xexp * fofn)
         return result
-    result = integrate.quad(integrand, cut, 1.0-cut)
+
+    result = integrate.quad(integrand, cut, 1.0 - cut)
     return result
 
-def get_path_Talbot(r : t_float = 1.):
+
+def get_path_Talbot(r: t_float = 1.0):
     """get Talbot path
 
     Parameters
@@ -67,16 +70,31 @@ def get_path_Talbot(r : t_float = 1.):
         derivative of Talbot path :math:`j_{\\text{Talbot}}(t) = \\frac{dp_{\\text{Talbot}}(t)}{dt}`
     """
     return (
-        lambda t: r*t_complex(np.complex(
-                    1 if t == 0.5 else np.pi*(2.*t-1.)/np.tan(np.pi*(2.*t-1.)),
-                    np.pi*(2.*t-1.))),
-        lambda t: r*np.pi*2.*t_complex(np.complex(
-                    0 if t == 0.5 else 1./np.tan(np.pi*(2.*t-1.))
-                                        - np.pi*(2.*t-1.)/(np.sin(np.pi*(2.*t-1.)))**2,
-                    1.))
+        lambda t: r
+        * t_complex(
+            np.complex(
+                1
+                if t == 0.5
+                else np.pi * (2.0 * t - 1.0) / np.tan(np.pi * (2.0 * t - 1.0)),
+                np.pi * (2.0 * t - 1.0),
+            )
+        ),
+        lambda t: r
+        * np.pi
+        * 2.0
+        * t_complex(
+            np.complex(
+                0
+                if t == 0.5
+                else 1.0 / np.tan(np.pi * (2.0 * t - 1.0))
+                - np.pi * (2.0 * t - 1.0) / (np.sin(np.pi * (2.0 * t - 1.0))) ** 2,
+                1.0,
+            )
+        ),
     )
 
-def get_path_line(m : t_float, c : t_float =1):
+
+def get_path_line(m: t_float, c: t_float = 1):
     """Get textbook path, i.e. a straight line parallel to imaginary axis
 
     Parameters
@@ -95,11 +113,12 @@ def get_path_line(m : t_float, c : t_float =1):
         :math:`j_{\\text{line}}(t) = \\frac{dp_{\\text{line}}(t)}{dt} = 2m`
     """
     return (
-      lambda t,max=m,c=c: t_complex(np.complex(c,max*(2*t-1))),
-      lambda t,max=m:     t_complex(np.complex(0,max* 2     ))
+        lambda t, max=m, c=c: t_complex(np.complex(c, max * (2 * t - 1))),
+        lambda t, max=m: t_complex(np.complex(0, max * 2)),
     )
 
-def get_path_edge(m:t_float,c:t_float=1.0):
+
+def get_path_edge(m: t_float, c: t_float = 1.0):
     """Get edged path with an angle of 45°
 
     Parameters
@@ -117,8 +136,12 @@ def get_path_edge(m:t_float,c:t_float=1.0):
         derivative of edged path
         :math:`j_{\\text{edge}}(t) = \\frac{dp_{\\text{edge}}(t)}{dt}`
     """
-    return (lambda t,max=m,c=c: c + (.5-t)*max*np.exp(np.complex(0,-np.pi*2./3.)) if t < .5 else
-                                c + (t-.5)*max*np.exp(np.complex(0, np.pi*2./3.)),
-            lambda t,max=m:            -max*np.exp(np.complex(0,-np.pi*2./3.)) if t < .5 else
-                                        max*np.exp(np.complex(0, np.pi*2./3.))
-            )
+    return (
+        lambda t, max=m, c=c: c
+        + (0.5 - t) * max * np.exp(np.complex(0, -np.pi * 2.0 / 3.0))
+        if t < 0.5
+        else c + (t - 0.5) * max * np.exp(np.complex(0, np.pi * 2.0 / 3.0)),
+        lambda t, max=m: -max * np.exp(np.complex(0, -np.pi * 2.0 / 3.0))
+        if t < 0.5
+        else max * np.exp(np.complex(0, np.pi * 2.0 / 3.0)),
+    )
--- src/eko/tests/test_sf_LO.py
+++ src/eko/tests/test_sf_LO.py
@@ -11,6 +11,7 @@ CA = constants.CA
 CF = constants.CF
 NF = 5
 
+
 def check_values(function, inputs, known_values):
     """ Takes advantages of the unified signature for all coefficients
     to check the value for N == `N1` """
@@ -18,12 +19,13 @@ def check_values(function, inputs, known_values):
         result = function(N, NF, CA, CF)
         assert_almost_equal(result, val)
 
+
 def test__S1():
     """test harmonic sum _S1"""
     # test on real axis
     known_vals = [1.0, 1.0 + 1.0 / 2.0, 1.0 + 1.0 / 2.0 + 1.0 / 3.0]
     for i, val in enumerate(known_vals):
-        result = spf_LO._S1(1 + i) # pylint: disable=protected-access
+        result = spf_LO._S1(1 + i)  # pylint: disable=protected-access
         assert_approx_equal(result, val, significant=8)
 
 
@@ -36,30 +38,41 @@ def test_number_momentum_conservation():
     # quark momentum
     input_N = [complex(2.0, 0.0)]
     known_vals = [complex(0.0, 0.0)]
+
     def _sum(*args):
-        return spf_LO.gamma_ns_0(*args) + spf_LO.gamma_gq_0(*args) # pylint: disable=no-value-for-parameter
+        return spf_LO.gamma_ns_0(*args) + spf_LO.gamma_gq_0(
+            *args
+        )  # pylint: disable=no-value-for-parameter
+
     check_values(_sum, input_N, known_vals)
     # gluon momentum
     def _sum(*args):
-        return spf_LO.gamma_qg_0(*args) + spf_LO.gamma_gg_0(*args) # pylint: disable=no-value-for-parameter
+        return spf_LO.gamma_qg_0(*args) + spf_LO.gamma_gg_0(
+            *args
+        )  # pylint: disable=no-value-for-parameter
+
     check_values(_sum, input_N, known_vals)
+
 
 def test_gamma_ps_0():
     input_N = [complex(1.0, 0.0)]
     known_vals = [complex(0.0, 0.0)]
     check_values(spf_LO.gamma_ps_0, input_N, known_vals)
 
+
 def test_gamma_qg_0():
     input_N = [complex(1.0, 0.0)]
-    known_vals = [complex(-20.0/3.0, 0.0)]
+    known_vals = [complex(-20.0 / 3.0, 0.0)]
     check_values(spf_LO.gamma_qg_0, input_N, known_vals)
+
 
 def test_gamma_gq_0():
     input_N = [complex(0.0, 1.0)]
-    known_vals = [complex(4.0,-4.0)/3.0]
+    known_vals = [complex(4.0, -4.0) / 3.0]
     check_values(spf_LO.gamma_gq_0, input_N, known_vals)
+
 
 def test_gamma_gg_0():
     input_N = [complex(0.0, 1.0)]
-    known_vals = [complex(5.195725159621,10.52008856962)]
+    known_vals = [complex(5.195725159621, 10.52008856962)]
     check_values(spf_LO.gamma_gg_0, input_N, known_vals)
