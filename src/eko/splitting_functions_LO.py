4199ac130ffb54a24941a11114e9c70152a23aa8
juacrumar <juacrumar@gmail.com>
2019-11-06 15:41:07 +0100

    numba to the splitting functions

--- src/eko/splitting_functions_LO.py
+++ src/eko/splitting_functions_LO.py
@@ -22,7 +22,10 @@ import numba as nb
 from eko import t_float, t_complex
 from _gsl_digamma import lib
 
-@nb.jit
+# Everything from this point will compile in object mode because of numba
+# not being able to deal with struct in nopython mode
+# we can deal with that problem in the future
+@nb.jit(forceobj=True)
 def _S1(N: t_complex):
     r"""Computes the simple harmonic sum
 
@@ -47,6 +50,8 @@ def _S1(N: t_complex):
     result = np.complex(c_result.r, c_result.i)
     return result + np.euler_gamma
 
+
+@nb.jit(forceobj=True)
 def gamma_ns_0(
     N: t_complex, nf: int, CA: t_float, CF: t_float
 ):  # pylint: disable=unused-argument
@@ -76,6 +81,7 @@ def gamma_ns_0(
     return result
 
 
+@nb.jit(forceobj=True)
 def gamma_ps_0(
     N: t_complex, nf: int, CA: t_float, CF: t_float
 ):  # pylint: disable=unused-argument
@@ -103,6 +109,7 @@ def gamma_ps_0(
     return 0.0
 
 
+@nb.jit(forceobj=True)
 def gamma_qg_0(
     N: t_complex, nf: int, CA: t_float, CF: t_float
 ):  # pylint: disable=unused-argument
@@ -132,6 +139,7 @@ def gamma_qg_0(
     return result
 
 
+@nb.jit(forceobj=True)
 def gamma_gq_0(
     N: t_complex, nf: int, CA: t_float, CF: t_float
 ):  # pylint: disable=unused-argument
@@ -161,6 +169,7 @@ def gamma_gq_0(
     return result
 
 
+@nb.jit(forceobj=True)
 def gamma_gg_0(
     N: t_complex, nf: int, CA: t_float, CF: t_float
 ):  # pylint: disable=unused-argument
