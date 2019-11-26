e9ddff951bafa4ff42beb0f4efea31d14107cc1b
juacrumar <juacrumar@gmail.com>
2019-11-26 15:11:44 +0100

    fix tests

--- src/eko/tests/test_cfunctions.py
+++ src/eko/tests/test_cfunctions.py
@@ -5,7 +5,8 @@
 import scipy.special
 import numpy as np
 from eko import t_float, t_complex
-from _gsl_digamma import lib
+import _gsl_digamma
+c_digamma = _gsl_digamma.lib.digamma
 
 
 def compare_functions(function_custom, function_ext, values):
@@ -19,13 +20,14 @@ def test_digamma():
     values = [t_float(1.3), t_float(0.56), t_complex(0.3 + 0.4j)]
 
     def customfun(x):
+        out = (np.empty(2))
         if isinstance(x, t_float):
-            result = lib.digamma(x, 0.0)
-            return result.r
+            c_digamma(x, 0.0, _gsl_digamma.ffi.from_buffer(out))
+            return out[0]
         elif isinstance(x, t_complex):
             r = np.real(x)
             i = np.imag(x)
-            result = lib.digamma(r, i)
-            return t_complex(np.complex(result.r, result.i))
+            c_digamma(r, i, _gsl_digamma.ffi.from_buffer(out))
+            return t_complex(np.complex(out[0], out[1]))
 
     compare_functions(customfun, scipy.special.digamma, values)
