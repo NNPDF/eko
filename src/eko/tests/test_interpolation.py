47331b74e813b18251dbd15928b8d31207bc4f22
juacrumar <juacrumar@gmail.com>
2019-11-29 18:59:05 +0100

    interpolator digested

--- src/eko/tests/test_interpolation.py
+++ src/eko/tests/test_interpolation.py
@@ -6,16 +6,7 @@ import numpy as np
 
 
 from eko import t_float, t_complex
-from eko.interpolation import (
-    get_xgrid_linear_at_id,
-    get_xgrid_linear_at_log,
-    get_Lagrange_basis_functions,
-    evaluate_Lagrange_basis_function_x,
-    evaluate_Lagrange_basis_function_N,
-    get_Lagrange_basis_functions_log,
-    evaluate_Lagrange_basis_function_log_x,
-    evaluate_Lagrange_basis_function_log_N,
-)
+from eko import interpolation
 import eko.mellin as mellin
 
 # for the numeric comparision to work, keep in mind that in Python3 the default precision is
@@ -77,55 +68,55 @@ def check_correspondence_interpolators(confs, evaluate_x, evaluate_N):
             assert_almost_equal(result_x[0], result_N)
 
 
-def test_get_Lagrange_basis_functions():
-    xgrid = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
-    for polynom_rank in range(1, len(xgrid)):
-        # check interpolation
-        check_is_interpolator(
-            get_Lagrange_basis_functions,
-            xgrid,
-            polynom_rank,
-            evaluate_Lagrange_basis_function_x,
-        )
-        # check correspondence
-        confs = get_Lagrange_basis_functions(xgrid, polynom_rank)
-        check_correspondence_interpolators(
-            confs,
-            evaluate_Lagrange_basis_function_x,
-            evaluate_Lagrange_basis_function_N,
-        )
-    # do some more explicit testing here?
-    # as we know all polynomials in an easy test case, say [0,.5,1]
+# def test_get_Lagrange_basis_functions():
+#     xgrid = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
+#     for polynom_rank in range(1, len(xgrid)):
+#         # check interpolation
+#         check_is_interpolator(
+#             get_Lagrange_basis_functions,
+#             xgrid,
+#             polynom_rank,
+#             evaluate_Lagrange_basis_function_x,
+#         )
+#         # check correspondence
+#         confs = get_Lagrange_basis_functions(xgrid, polynom_rank)
+#         check_correspondence_interpolators(
+#             confs,
+#             evaluate_Lagrange_basis_function_x,
+#             evaluate_Lagrange_basis_function_N,
+#         )
+#     # do some more explicit testing here?
+#     # as we know all polynomials in an easy test case, say [0,.5,1]
 
 
-def test_get_Lagrange_basis_functions_log():
-    xgrid = np.array(np.logspace(-1, 0, num=5))
-    for polynom_rank in range(1, len(xgrid)):
-        # check interpolation
-        check_is_interpolator(
-            get_Lagrange_basis_functions_log,
-            xgrid,
-            polynom_rank,
-            evaluate_Lagrange_basis_function_log_x,
-        )
-        # check correspondence
-        confs = get_Lagrange_basis_functions_log(xgrid, polynom_rank)
-        check_correspondence_interpolators(
-            confs,
-            evaluate_Lagrange_basis_function_log_x,
-            evaluate_Lagrange_basis_function_log_N,
-        )
+# def test_get_Lagrange_basis_functions_log():
+#     xgrid = np.array(np.logspace(-1, 0, num=5))
+#     for polynom_rank in range(1, len(xgrid)):
+#         # check interpolation
+#         check_is_interpolator(
+#             get_Lagrange_basis_functions_log,
+#             xgrid,
+#             polynom_rank,
+#             evaluate_Lagrange_basis_function_log_x,
+#         )
+#         # check correspondence
+#         confs = get_Lagrange_basis_functions_log(xgrid, polynom_rank)
+#         check_correspondence_interpolators(
+#             confs,
+#             evaluate_Lagrange_basis_function_log_x,
+#             evaluate_Lagrange_basis_function_log_N,
+#         )
 
 
 def test_get_xgrid_linear_at_id():
     """test linear@id grids"""
     grid_result = np.array([0.0, 0.5, 1.0])
-    check_xgrid(get_xgrid_linear_at_id, grid_result)
-    check_is_tfloat(get_xgrid_linear_at_id)
+    check_xgrid(interpolation.get_xgrid_linear_at_id, grid_result)
+    check_is_tfloat(interpolation.get_xgrid_linear_at_id)
 
 
 def test_get_xgrid_linear_at_log10():
     """test linear@log10 grids"""
     grid_result = np.array([1e-2, 1e-1, 1.0])
-    check_is_tfloat(get_xgrid_linear_at_log, xmin=1e-2)
-    check_xgrid(get_xgrid_linear_at_log, grid_result, xmin=1e-2)
+    check_is_tfloat(interpolation.get_xgrid_linear_at_log, xmin=1e-2)
+    check_xgrid(interpolation.get_xgrid_linear_at_log, grid_result, xmin=1e-2)
