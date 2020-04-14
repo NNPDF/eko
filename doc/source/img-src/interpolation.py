e35a6be50e2d013010331017bef1cb9d57eaf0b5
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2020-04-14 14:04:19 +0200

    update doc

---
+++ doc/source/img-src/interpolation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pathlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+from eko import interpolation
+
+# setup
+logxmin = -5
+xgrid = np.logspace(logxmin,0,9)
+polynomial_degree = 3
+disp = interpolation.InterpolatorDispatcher(xgrid,polynomial_degree,True,False)
+
+# show grid
+xs = np.logspace(logxmin,0,150)
+plt.xlabel("x")
+plt.xscale("log")
+plt.plot(xgrid,[0]*len(xgrid),"o",color="black")
+# plot
+for j in [0,1,4,8]:
+    ys = [disp[j](x) for x in xs]
+    plt.plot(xs,ys,label=f"$p_{j}(x)$")
+
+# global
+plt.title("Example configuration")
+plt.legend()
+
+# output
+plt.savefig(pathlib.Path(__file__).parent.parent/"img"/"interpolation-polynomials.png")
