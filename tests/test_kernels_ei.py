050410ac4c26a0fea69e8fe51b846ca7d7031884
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2020-10-06 10:36:45 +0200

    Improve tests

--- tests/test_kernels_ei.py
+++ tests/test_kernels_ei.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 from eko.kernels import evolution_integrals as ei
+from eko import beta
 
 
 def test_zero():
@@ -9,3 +10,60 @@ def test_zero():
     nf = 3
     for fnc in [ei.j00, ei.j01_exact, ei.j01_expanded, ei.j11_exact, ei.j11_expanded]:
         np.testing.assert_allclose(fnc(1, 1, nf), 0)
+
+
+def test_der_lo():
+    """LO derivative"""
+    nf = 3
+    a0 = 5
+    a1 = 3
+    delta_a = -1e-6
+    rhs = 1.0 / (beta.beta(0, nf) * a1)
+    lhs = (
+        ei.j00(a1 + 0.5 * delta_a, a0, nf) - ei.j00(a1 - 0.5 * delta_a, a0, nf)
+    ) / delta_a
+    np.testing.assert_allclose(rhs, lhs)
+
+
+def test_der_nlo_exp():
+    """expanded NLO derivative"""
+    nf = 3
+    a0 = 0.3
+    a1 = 0.1
+    delta_a = -1e-6
+    # 01
+    rhs = 1.0 / (beta.beta(0, nf) * a1 + beta.beta(1, nf) * a1 ** 2)
+    lhs = (
+        ei.j01_expanded(a1 + 0.5 * delta_a, a0, nf)
+        - ei.j01_expanded(a1 - 0.5 * delta_a, a0, nf)
+    ) / delta_a
+    np.testing.assert_allclose(rhs, lhs, atol=np.abs((beta.b(1, nf) * a1) ** 2))
+    # 11
+    rhs = 1.0 / (beta.beta(0, nf) + beta.beta(1, nf) * a1)
+    lhs = (
+        ei.j11_expanded(a1 + 0.5 * delta_a, a0, nf)
+        - ei.j11_expanded(a1 - 0.5 * delta_a, a0, nf)
+    ) / delta_a
+    np.testing.assert_allclose(rhs, lhs, atol=np.abs(beta.b(1, nf) * a1))
+
+
+def test_der_nlo_exa():
+    """exact NLO derivative"""
+    nf = 3
+    a0 = 0.3
+    a1 = 0.1
+    delta_a = -1e-6
+    # 01
+    rhs = 1.0 / (beta.beta(0, nf) * a1 + beta.beta(1, nf) * a1 ** 2)
+    lhs = (
+        ei.j01_exact(a1 + 0.5 * delta_a, a0, nf)
+        - ei.j01_exact(a1 - 0.5 * delta_a, a0, nf)
+    ) / delta_a
+    np.testing.assert_allclose(rhs, lhs, atol=np.abs(delta_a))  # in fact O(delta_a^2)
+    # 11
+    rhs = 1.0 / (beta.beta(0, nf) + beta.beta(1, nf) * a1)
+    lhs = (
+        ei.j11_exact(a1 + 0.5 * delta_a, a0, nf)
+        - ei.j11_exact(a1 - 0.5 * delta_a, a0, nf)
+    ) / delta_a
+    np.testing.assert_allclose(rhs, lhs, atol=np.abs(delta_a))  # in fact O(delta_a^2)
--- tests/test_kernels_ns.py
+++ tests/test_kernels_ns.py
@@ -2,6 +2,18 @@
 import numpy as np
 
 from eko.kernels import non_singlet as ns
+from eko import beta
+
+methods = [
+    "iterate-expanded",
+    "decompose-expanded",
+    "perturbative-expanded",
+    "truncated",
+    "ordered-truncated",
+    "iterate-exact",
+    "decompose-exact",
+    "perturbative-exact",
+]
 
 
 def test_zero():
@@ -10,16 +22,7 @@ def test_zero():
     ev_op_iterations = 2
     gamma_ns = np.array([1, 1])
     for order in [0, 1]:
-        for method in [
-            "iterate-expanded",
-            "decompose-expanded",
-            "perturbative-expanded",
-            "truncated",
-            "ordered-truncated",
-            "iterate-exact",
-            "decompose-exact",
-            "perturbative-exact",
-        ]:
+        for method in methods:
             np.testing.assert_allclose(
                 ns.dispatcher(order, method, gamma_ns, 1, 1, nf, ev_op_iterations), 1.0
             )
@@ -27,3 +30,47 @@ def test_zero():
                 ns.dispatcher(order, method, np.zeros(2), 2, 1, nf, ev_op_iterations),
                 1.0,
             )
+
+
+def test_ode_lo():
+    nf = 3
+    ev_op_iterations = 10
+    gamma_ns = np.random.rand(1)
+    delta_a = -1e-6
+    a0 = 0.3
+    for a1 in [0.1, 0.2]:
+        r = a1 * gamma_ns / (beta.beta(0, nf) * a1 ** 2)
+        for method in methods:
+            rhs = r * ns.dispatcher(0, method, gamma_ns, a1, a0, nf, ev_op_iterations)
+            lhs = (
+                ns.dispatcher(
+                    0, method, gamma_ns, a1 + 0.5 * delta_a, a0, nf, ev_op_iterations
+                )
+                - ns.dispatcher(
+                    0, method, gamma_ns, a1 - 0.5 * delta_a, a0, nf, ev_op_iterations
+                )
+            ) / delta_a
+            np.testing.assert_allclose(lhs, rhs, atol=np.abs(delta_a))
+
+
+# def test_ode_nlo():
+#     nf = 3
+#     ev_op_iterations = 10
+#     gamma_ns = np.random.rand(2)
+#     delta_a = -1e-6
+#     a0 = 0.3
+#     for a1 in [0.1, 0.2]:
+#         r = (a1 * gamma_ns[0] + a1 ** 2 * gamma_ns[1]) / (
+#             beta.beta(0, nf) * a1 ** 2 + beta.beta(1, nf) * a1 ** 3
+#         )
+#         for method in ["iterate-expanded"]:
+#             rhs = r * ns.dispatcher(1, method, gamma_ns, a1, a0, nf, ev_op_iterations)
+#             lhs = (
+#                 ns.dispatcher(
+#                     1, method, gamma_ns, a1 + 0.5 * delta_a, a0, nf, ev_op_iterations
+#                 )
+#                 - ns.dispatcher(
+#                     1, method, gamma_ns, a1 - 0.5 * delta_a, a0, nf, ev_op_iterations
+#                 )
+#             ) / delta_a
+#             np.testing.assert_allclose(lhs, rhs, atol=np.abs(delta_a))
