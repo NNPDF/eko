5c0864dad396d876107c89e9e71a5445677c49a9
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2020-11-10 10:24:23 +0100

    Fix flavor test

--- tests/test_op_flavors.py
+++ tests/test_op_flavors.py
@@ -25,38 +25,37 @@ class TestOpMember:
 
 
 def test_pids_from_intrinsic_evol():
-    raw = dict(zip(br.flavor_basis_pids, np.zeros(len(br.flavor_basis_pids))))
+    def get(d):
+        raw = np.zeros(len(br.flavor_basis_pids))
+        for pid, w in d.items():
+            raw[br.flavor_basis_pids.index(pid)] = w
+        return raw
     # g
-    g = raw.copy()
-    g.update({21: 1})
-    assert pytest.approx(g) == flavors.pids_from_intrinsic_evol("g", 3)
-    assert pytest.approx(g) == flavors.pids_from_intrinsic_evol("g", 4)
+    g = get({21: 1})
+    assert pytest.approx(g) == flavors.pids_from_intrinsic_evol("g", 3, False)
+    assert pytest.approx(g) == flavors.pids_from_intrinsic_evol("g", 4, False)
     # S(nf=3) = u+ + d+ + s+
-    S3 = raw.copy()
-    S3.update({1: 1, -1: 1, 2: 1, -2: 1, 3: 1, -3: 1})
-    assert pytest.approx(S3) == flavors.pids_from_intrinsic_evol("S", 3)
+    S3 = get({1: 1, -1: 1, 2: 1, -2: 1, 3: 1, -3: 1})
+    assert pytest.approx(S3) == flavors.pids_from_intrinsic_evol("S", 3, False)
     # S(nf=4) = u+ + d+ + s+
-    S4 = raw.copy()
-    S4.update({1: 1, -1: 1, 2: 1, -2: 1, 3: 1, -3: 1, 4: 1, -4: 1})
-    assert pytest.approx(S4) == flavors.pids_from_intrinsic_evol("S", 4)
+    S4 = get({1: 1, -1: 1, 2: 1, -2: 1, 3: 1, -3: 1, 4: 1, -4: 1})
+    assert pytest.approx(S4) == flavors.pids_from_intrinsic_evol("S", 4, False)
     # T3 = u+ - d+
-    T3 = raw.copy()
-    T3.update({2: 1, -2: 1, 1: -1, -1: -1})
-    assert pytest.approx(T3) == flavors.pids_from_intrinsic_evol("T3", 3)
-    assert pytest.approx(T3) == flavors.pids_from_intrinsic_evol("T3", 4)
+    T3 = get({2: 1, -2: 1, 1: -1, -1: -1})
+    assert pytest.approx(T3) == flavors.pids_from_intrinsic_evol("T3", 3, False)
+    assert pytest.approx(T3) == flavors.pids_from_intrinsic_evol("T3", 4, False)
     # V15(nf=3) = V(nf=3) = u- + d- + s-
     assert pytest.approx(
-        flavors.pids_from_intrinsic_evol("V", 3)
-    ) == flavors.pids_from_intrinsic_evol("V15", 3)
+        flavors.pids_from_intrinsic_evol("V", 3, False)
+    ) == flavors.pids_from_intrinsic_evol("V15", 3, False)
     # V15(nf=4) =  u- + d- + s- - 3c- =!= V(nf=4)
     assert pytest.approx(
-        flavors.pids_from_intrinsic_evol("V", 4)
-    ) != flavors.pids_from_intrinsic_evol("V15", 4)
+        flavors.pids_from_intrinsic_evol("V", 4, False)
+    ) != flavors.pids_from_intrinsic_evol("V15", 4, False)
     # c+
-    cp = raw.copy()
-    cp.update({4: 1, -4: 1})
-    assert pytest.approx(cp) == flavors.pids_from_intrinsic_evol("c+", 3)
-    assert pytest.approx(cp) == flavors.pids_from_intrinsic_evol("c+", 4)
+    cp = get({4: 1, -4: 1})
+    assert pytest.approx(cp) == flavors.pids_from_intrinsic_evol("c+", 3, False)
+    assert pytest.approx(cp) == flavors.pids_from_intrinsic_evol("c+", 4, False)
 
 
 def test_get_range():
