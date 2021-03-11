155d1eb03e5c9b5e06d1295de78badb625c37238
giacomomagni <giac.magni@gmail.com>
2021-03-11 16:49:47 +0100

    Fix on nnlo AD

--- tests/test_ad_nnlo.py
+++ tests/test_ad_nnlo.py
@@ -4,19 +4,20 @@ import numpy as np
 
 import eko.anomalous_dimensions.nnlo as ad_nnlo
 
-NF = 5
+NF = 0
 
 
 def test_gamma_2():
     # reference values are obtained from MMa
     # non-siglet sector
-    # np.testing.assert_allclose(ad_nnlo.gamma_nsv_2(1, NF), 0, atol=2e-6)
+    np.testing.assert_allclose(
+        ad_nnlo.gamma_nsm_2(1, NF) - ad_nnlo.gamma_nsv_2(1, NF), 0, atol=2e-3
+    )  # pylint: disable=line-too-long
 
     gS2 = ad_nnlo.gamma_singlet_2(2, NF)
     # gluon momentum conservation
-    # the CA*NF term seems to be tough to compute, so raise the constraint ...
-    # np.testing.assert_allclose( gS2[0,1] + gS2[1,1] , 0,atol=4e-2 )
+    np.testing.assert_allclose(gS2[0, 1] + gS2[1, 1], 0, atol=9e-2)
     # quark momentum conservation
-    np.testing.assert_allclose(gS2[0, 0] + gS2[1, 0], 0, atol=2e-2)
+    np.testing.assert_allclose(gS2[0, 0] + gS2[1, 0], 0, atol=2e-3)
 
     assert gS2.shape == (2, 2)
