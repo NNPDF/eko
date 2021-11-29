e408c277c8ca86a37660969300d1c09c56af3287
Andrea Barontini <andreab1997@yahoo.it>
2021-11-29 11:20:55 +0100

    Testing ekobox and allowing use of cached output

--- src/ekobox/evol_pdf.py
+++ src/ekobox/evol_pdf.py
@@ -6,10 +6,11 @@ from eko import basis_rotation as br
 from . import apply, gen_info, gen_op, gen_theory
 
 
-def evolve_PDFs(
+def evolve_pdfs(
     initial_PDF_list,
     theory_card,
     operators_card,
+    path=None,
     targetgrid=None,
     install=False,
     name="Evolved_PDF",
@@ -30,6 +31,9 @@ def evolve_PDFs(
         operators_card : dict
             operators card
 
+        path : str
+            path to cached eko output (if "None" it will be recomputed)
+
         targetgrid : list(float)
             target x-grid (if different from input x-grid)
 
@@ -43,7 +47,14 @@ def evolve_PDFs(
             dict of info to add or update to default info file
 
     """
-    eko_output = eko.run_dglap(theory_card, operators_card)
+    eko_output = None
+    if path is not None:
+        ops_id = f"o{operators_card['hash'][:6]}_t{theory_card['hash'][:6]}"
+        outpath = f"{path}/{ops_id}.tar"
+        eko_output = eko.output.Output.load_tar(outpath)
+    else:
+        eko_output = eko.run_dglap(theory_card, operators_card)
+
     evolved_PDF_list = []
     for initial_PDF in initial_PDF_list:
         evolved_PDF_list.append(apply.apply_pdf(eko_output, initial_PDF, targetgrid))
@@ -78,3 +89,30 @@ def evolve_PDFs(
 
     if install:
         genpdf.install_pdf(name)
+
+
+def gen_out(theory_card, op_card, path=None):
+    """
+    Generates EKO output from theory and operators cards and, if requested,
+    dumps it in tar format
+
+    Parameters
+    ----------
+        theory_card : dict
+            theory card
+        op_card : dict
+            operators card
+        path : str
+            path of dumped output (if "None" output is not dumped)
+
+    Returns
+    -------
+        : eko.output.Output
+            eko output
+    """
+    eko_output = eko.run_dglap(theory_card, op_card)
+    if path is not None:
+        ops_id = f"o{op_card['hash'][:6]}_t{theory_card['hash'][:6]}"
+        path = f"{path}/{ops_id}.tar"
+        eko_output.dump_tar(path)
+    return eko_output
--- tests/test_ekobox/test_evol_pdf.py
+++ tests/test_ekobox/test_evol_pdf.py
@@ -1,3 +1,5 @@
+import itertools
+
 import lhapdf
 import numpy as np
 import pytest
@@ -5,6 +7,7 @@ from banana.data.genpdf import load
 from utils import cd, lhapdf_path, test_pdf
 
 from eko import basis_rotation as br
+from eko import output
 from ekobox import evol_pdf as ev_p
 from ekobox import gen_info as g_i
 from ekobox import gen_op as g_o
@@ -13,8 +16,7 @@ from ekobox import gen_theory as g_t
 
 def test_evolve_single_member(tmp_path):
     q2grid = [100.0]
-    xgrid = [1.0e-7, 0.01, 0.1, 0.2, 0.3]
-    op = g_o.gen_op_card(q2grid, update={"interpolation_xgrid": xgrid})
+    op = g_o.gen_op_card(q2grid)
     theory = g_t.gen_theory_card(
         0,
         5.0,
@@ -26,18 +28,19 @@ def test_evolve_single_member(tmp_path):
             "mc": 1.3,
             "mb": 4.75,
             "mt": 172,
-            "kcThr": 1.0,
-            "kbThr": 1.0,
-            "ktThr": 1.0,
+            "kcThr": 1,
+            "kbThr": 1,
+            "ktThr": 1,
         },
     )
     with lhapdf_path(test_pdf):
         pdf = lhapdf.mkPDF("myCT14llo_NF3", 0)
     with cd(tmp_path):
-        ev_p.evolve_PDFs(
+        ev_p.evolve_pdfs(
             [pdf],
             theory,
             op,
+            path="/home/andrea/n3pdf/eko/tests/test_ekobox/cached_out",
             info_update={"SetDesc": "MyEvolvedPDF", "MZ": 0.2, "Debug": "Debug"},
         )
     with lhapdf_path(tmp_path):
@@ -47,16 +50,18 @@ def test_evolve_single_member(tmp_path):
     assert info["SetDesc"] == "MyEvolvedPDF"
     assert info["MZ"] == theory["MZ"]
     assert info["Debug"] == "Debug"
-
-
-#    import pdb; pdb.set_trace()
-#    for Q2 in q2grid:
-#        for x in xgrid:
-#            for pid in br.flavor_basis_pids:
-#                np.testing.assert_allclose(
-#                    pdf.xfxQ2(pid, x, Q2), all_blocks[0]["data"][q2grid.index(Q2)*len(xgrid)+xgrid.index(x)][br.flavor_basis_pids.index(pid)]
-#                        ,rtol=3e-5
-#                    )
+    xgrid = op["interpolation_xgrid"]
+    for Q2 in q2grid:
+        for x in itertools.islice(xgrid, 10, 40):
+            for pid in [21, 1, -1, 2, -2, 3, -3]:
+                np.testing.assert_allclose(
+                    pdf.xfxQ2(pid, x, Q2),
+                    x
+                    * all_blocks[0]["data"][xgrid.index(x)][
+                        br.flavor_basis_pids.index(pid)
+                    ],
+                    rtol=2e-2,
+                )
 
 
 def test_evolve_more_members(tmp_path):
@@ -70,7 +75,20 @@ def test_evolve_more_members(tmp_path):
     d.mkdir()
     with lhapdf_path(d):
         with cd(tmp_path):
-            ev_p.evolve_PDFs(pdfs, theory, op, install=True, name="Debug")
+            ev_p.evolve_pdfs(pdfs, theory, op, install=True, name="Debug")
         all_blocks = (load.load_blocks_from_file("Debug", 1))[1]
         info = load.load_info_from_file("Debug")
     assert info["XMin"] == op["interpolation_xgrid"][0]
+
+
+def test_gen_and_dump_out(tmp_path):
+    op = g_o.gen_op_card(
+        [100.0], update={"interpolation_xgrid": [1e-7, 0.01, 0.1, 0.2, 0.3]}
+    )
+    theory = g_t.gen_theory_card(0, 1.0)
+
+    out = ev_p.gen_out(theory, op, path=tmp_path)
+
+    ops_id = f"o{op['hash'][:6]}_t{theory['hash'][:6]}"
+    outpath = f"{tmp_path}/{ops_id}.tar"
+    loaded_out = output.Output.load_tar(outpath)
