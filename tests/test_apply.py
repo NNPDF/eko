780ca1dba33b97caf468ef87f0696bba77dd20c5
Alessandro Candido <candido.ale@gmail.com>
2022-03-05 19:08:12 +0100

    Lift fake output and pdf

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -1,67 +1,54 @@
 # -*- coding: utf-8 -*-
-import copy
-import io
-import pathlib
-import shutil
-import tempfile
-from unittest import mock
-
 import numpy as np
-import pytest
 
-from eko import basis_rotation as br
 from eko import output
 from ekomark import apply
 
-from .test_output import FakeOutput, FakePDF
 
-
-class TestApply(FakeOutput):
-    def test_apply(self):
-        d = self.fake_output()
-        q2_out = list(d["Q2grid"].keys())[0]
+class TestApply:
+    def test_apply(self, fake_output, fake_pdf):
+        q2_out = list(fake_output["Q2grid"].keys())[0]
         # create object
-        o = output.Output(d)
+        o = output.Output(fake_output)
         # fake pdfs
-        pdf = FakePDF()
-        pdf_grid = apply.apply_pdf(o, pdf)
-        assert len(pdf_grid) == len(d["Q2grid"])
+        pdf_grid = apply.apply_pdf(o, fake_pdf)
+        assert len(pdf_grid) == len(fake_output["Q2grid"])
         pdfs = pdf_grid[q2_out]["pdfs"]
-        assert list(pdfs.keys()) == d["targetpids"]
-        ref_pid1 = d["Q2grid"][q2_out]["operators"][0, :, 1, :] @ np.ones(
-            len(d["interpolation_xgrid"])
+        assert list(pdfs.keys()) == fake_output["targetpids"]
+        ref_pid1 = fake_output["Q2grid"][q2_out]["operators"][0, :, 1, :] @ np.ones(
+            len(fake_output["interpolation_xgrid"])
         )
         np.testing.assert_allclose(pdfs[0], ref_pid1)
-        ref_pid2 = d["Q2grid"][q2_out]["operators"][1, :, 1, :] @ np.ones(
-            len(d["interpolation_xgrid"])
+        ref_pid2 = fake_output["Q2grid"][q2_out]["operators"][1, :, 1, :] @ np.ones(
+            len(fake_output["interpolation_xgrid"])
         )
         np.testing.assert_allclose(pdfs[1], ref_pid2)
         # rotate to target_grid
         target_grid = [0.75]
-        pdf_grid = apply.apply_pdf(o, pdf, target_grid)
+        pdf_grid = apply.apply_pdf(o, fake_pdf, target_grid)
         assert len(pdf_grid) == 1
         pdfs = pdf_grid[q2_out]["pdfs"]
-        assert list(pdfs.keys()) == d["targetpids"]
+        assert list(pdfs.keys()) == fake_output["targetpids"]
 
-    def test_apply_flavor(self, monkeypatch):
-        d = self.fake_output()
-        q2_out = list(d["Q2grid"].keys())[0]
+    def test_apply_flavor(self, fake_output, fake_pdf, monkeypatch):
+        q2_out = list(fake_output["Q2grid"].keys())[0]
         # create object
-        o = output.Output(d)
+        o = output.Output(fake_output)
         # fake pdfs
-        pdf = FakePDF()
         monkeypatch.setattr(
             "eko.basis_rotation.rotate_flavor_to_evolution", np.ones((2, 2))
         )
-        monkeypatch.setattr("eko.basis_rotation.flavor_basis_pids", d["targetpids"])
+        monkeypatch.setattr(
+            "eko.basis_rotation.flavor_basis_pids", fake_output["targetpids"]
+        )
         fake_evol_basis = ("a", "b")
         monkeypatch.setattr("eko.basis_rotation.evol_basis", fake_evol_basis)
-        pdf_grid = apply.apply_pdf(o, pdf, rotate_to_evolution_basis=True)
-        assert len(pdf_grid) == len(d["Q2grid"])
+        pdf_grid = apply.apply_pdf(o, fake_pdf, rotate_to_evolution_basis=True)
+        assert len(pdf_grid) == len(fake_output["Q2grid"])
         pdfs = pdf_grid[q2_out]["pdfs"]
         assert list(pdfs.keys()) == list(fake_evol_basis)
         ref_a = (
-            d["Q2grid"][q2_out]["operators"][0, :, 1, :]
-            + d["Q2grid"][q2_out]["operators"][1, :, 1, :]
-        ) @ np.ones(len(d["interpolation_xgrid"]))
+            fake_output["Q2grid"][q2_out]["operators"][0, :, 1, :]
+            + fake_output["Q2grid"][q2_out]["operators"][1, :, 1, :]
+        ) @ np.ones(len(fake_output["interpolation_xgrid"]))
         np.testing.assert_allclose(pdfs["a"], ref_a)
--- tests/test_output.py
+++ tests/test_output.py
@@ -13,14 +13,6 @@ from eko import basis_rotation as br
 from eko import output
 
 
-class FakePDF:
-    def hasFlavor(self, pid):
-        return pid == 1
-
-    def xfxQ2(self, _pid, x, _q2):
-        return x
-
-
 def eko_identity(shape):
     i, k = np.ogrid[: shape[1], : shape[2]]
     eko_identity = np.zeros(shape[1:], int)
@@ -36,51 +28,6 @@ def chk_keys(a, b):
         opb = b["Q2grid"][q2]
         assert sorted(op.keys()) == sorted(opb.keys())
         assert op["alphas"] == opb["alphas"]
-
-
-class FakeOutput:
-    shape = (2, 2)
-
-    def mkO(self):
-        ma, mae = np.random.rand(2, *self.shape)
-        return ma, mae
-
-    def mk_g(self, q2s, lpids, lx):
-        Q2grid = {}
-        for q2 in q2s:
-            Q2grid[q2] = {
-                "operators": np.random.rand(lpids, lx, lpids, lx),
-                "operator_errors": np.random.rand(lpids, lx, lpids, lx),
-                "alphas": np.random.rand(),
-            }
-        return Q2grid
-
-    def fake_output(self):
-        # build data
-        interpolation_xgrid = np.array([0.5, 1.0])
-        interpolation_polynomial_degree = 1
-        interpolation_is_log = False
-        pids = [0, 1]
-        q2_ref = 1
-        q2_out = 2
-        Q2grid = self.mk_g([q2_out], len(pids), len(interpolation_xgrid))
-        d = dict(
-            interpolation_xgrid=interpolation_xgrid,
-            targetgrid=interpolation_xgrid,
-            inputgrid=interpolation_xgrid,
-            interpolation_polynomial_degree=interpolation_polynomial_degree,
-            interpolation_is_log=interpolation_is_log,
-            q2_ref=q2_ref,
-            inputpids=pids,
-            targetpids=pids,
-            Q2grid=Q2grid,
-        )
-        return d
-
-
-@pytest.fixture
-def fake_output():
-    return FakeOutput().fake_output()
 
 
 class TestOutput:
@@ -290,13 +237,13 @@ class TestOutput:
         with pytest.raises(ValueError):
             copy.deepcopy(o1).flavor_reshape()
 
-    def test_to_evol(self, fake_output):
+    def test_to_evol(self, fake_factory):
         interpolation_xgrid = np.array([0.5, 1.0])
         interpolation_polynomial_degree = 1
         interpolation_is_log = False
         q2_ref = 1
         q2_out = 2
-        Q2grid = fake_output.mk_g(
+        Q2grid = fake_factory.mk_g(
             [q2_out], len(br.flavor_basis_pids), len(interpolation_xgrid)
         )
         d = dict(
