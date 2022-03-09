3552d01faa0c6d04ecd191ece6f9aeb83b5edd78
Niccolò Laurenti <niclaurenti@gmail.com>
2022-03-09 16:49:50 +0100

    Add function update_colors in constants.py

--- src/eko/constants.py
+++ src/eko/constants.py
@@ -8,16 +8,23 @@ NC = 3
 """the number of colors"""
 
 TR = float(1.0 / 2.0)
-"""the normalization of fundamental generators"""
+"""the normalization of fundamental generators - defaults to :math:`T_F = 1/2`"""
 
 CA = float(NC)
-"""second Casimir constant in the adjoint representation - defaults to :math:`N_C = 3"""
+"""second Casimir constant in the adjoint representation - defaults to :math:`N_C = 3`"""
 
 CF = float((NC * NC - 1.0) / (2.0 * NC))
-"""second Casimir constant in the fundamental representation - defaults to :math:`\frac{N_C^2-1}{2N_C} = 4/3"""
+"""second Casimir constant in the fundamental representation - defaults to :math:`\frac{N_C^2-1}{2N_C} = 4/3`"""
 
 eu2 = 4.0 / 9
 """up quarks charge squared"""
 
 ed2 = 1.0 / 9
 """down quarks charge squared"""
+
+
+def update_colors(nc):
+    global NC, CA, CF
+    NC = int(nc)
+    CA = float(NC)
+    CF = float((NC * NC - 1.0) / (2.0 * NC))
