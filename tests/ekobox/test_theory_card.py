15a2161c85436a1826303fae6ec0b65e1f36247b
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-03-24 11:02:49 +0100

    Rename gen_theory

--- tests/ekobox/test_theory_card.py
+++ tests/ekobox/test_theory_card.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from ekobox import gen_theory as g_t
+from ekobox import theory_card as tc
 
 
 def test_gen_theory_card():
-    theory = g_t.gen_theory_card(0, 1.0)
+    theory = tc.generate(0, 1.0)
     assert theory["PTO"] == 0
     assert theory["Q0"] == 1.0
     assert theory["mt"] == 173.07
     up_err = {"Prova": "Prova"}
     with pytest.raises(ValueError):
-        theory = g_t.gen_theory_card(0, 1.0, update=up_err)
+        theory = tc.generate(0, 1.0, update=up_err)
     up = {"mb": 132.3, "PTO": 2}
-    theory = g_t.gen_theory_card(0, 1.0, update=up)
+    theory = tc.generate(0, 1.0, update=up)
     assert theory["PTO"] == 2
     assert theory["mb"] == 132.3
 
 
 def test_export_load_theory_card(tmp_path, cd):
     with cd(tmp_path):
-        theory = g_t.gen_theory_card(2, 12.3, name="debug_theory")
-        g_t.export_theory_card("debug_theory_two", theory)
-        theory_loaded = g_t.import_theory_card("debug_theory.yaml")
-        theory_two_loaded = g_t.import_theory_card("debug_theory_two.yaml")
+        theory = tc.generate(2, 12.3, name="debug_theory")
+        tc.dump("debug_theory_two", theory)
+        theory_loaded = tc.load("debug_theory.yaml")
+        theory_two_loaded = tc.load("debug_theory_two.yaml")
         for key in theory.keys():
             assert theory[key] == theory_loaded[key] == theory_two_loaded[key]
