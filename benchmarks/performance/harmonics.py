143726f639f247d8d6d4738d8189c248bf5147cf
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-04-04 18:05:00 +0200

    Init asv

---
+++ benchmarks/performance/harmonics.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from eko import anomalous_dimensions as ad
+from eko.mellin import Path
+
+NF = 5
+
+
+class TimeSuite:
+    def setup(self):
+        ts = np.linspace(0.5, 1.0 - 1e-5, 100)
+        logx = 0.8
+        axis_offset = True
+        self.ns = []
+        for t in ts:
+            self.ns.append(Path(t, logx, axis_offset).n)
+
+    def time_as1_sing(self):
+        for n in self.ns:
+            ad.gamma_singlet(0, n, NF)
+
+    def time_as2_sing(self):
+        for n in self.ns:
+            ad.gamma_singlet(1, n, NF)
+
+    def time_as3_sing(self):
+        for n in self.ns:
+            ad.gamma_singlet(2, n, NF)
+
+
+# class MemorySuite:
+#     def mem_sf(self):
+#         return compute_sf()
+
+
+# class PeakMemorySuite:
+#     def peakmem_sf(self):
+#         compute_sf()
