8093d99da6758ae61913eff179bca26f628adeea
Alessandro Candido <candido.ale@gmail.com>
2022-04-08 17:05:22 +0200

    Init eko CLI

---
+++ src/ekobox/cli/run.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import click
+
+from ._base import command
+
+
+@command.command("run")
+@click.argument("q2")
+def subcommand(q2):
+    """Launch EKO computation.
+
+    Parameters
+    ----------
+    q2: sequence[float]
+        sequnce of q2 to compute
+
+    """
+    print(f"Running EKO for {q2}")
