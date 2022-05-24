e04269c0e93ae5782c51a26a879aaa1705d2a5fb
Niccolò Laurenti <niclaurenti@gmail.com>
2022-05-24 12:09:46 +0200

    Change gamma_0 -> gamma_as1, gamma_1 -> gamma_as2, ecc

--- tests/eko/test_gamma.py
+++ tests/eko/test_gamma.py
@@ -11,9 +11,9 @@ from eko import gamma
 def test_gamma():
     """gamma-wrapper"""
     nf = 3
-    np.testing.assert_allclose(gamma.gamma(1, nf), gamma.gamma_0())
-    np.testing.assert_allclose(gamma.gamma(2, nf), gamma.gamma_1(nf))
-    np.testing.assert_allclose(gamma.gamma(3, nf), gamma.gamma_2(nf))
-    np.testing.assert_allclose(gamma.gamma(4, nf), gamma.gamma_3(nf))
+    np.testing.assert_allclose(gamma.gamma(1, nf), gamma.gamma_as1())
+    np.testing.assert_allclose(gamma.gamma(2, nf), gamma.gamma_as2(nf))
+    np.testing.assert_allclose(gamma.gamma(3, nf), gamma.gamma_as3(nf))
+    np.testing.assert_allclose(gamma.gamma(4, nf), gamma.gamma_as4(nf))
     with pytest.raises(ValueError):
         gamma.gamma(5, 3)
