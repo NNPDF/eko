1189c790271fe109befbf06cd7f68614e2de293a
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-06-01 14:37:29 +0200

    Revert minus sign

--- src/eko/scale_variations/expanded.py
+++ src/eko/scale_variations/expanded.py
@@ -2,13 +2,10 @@
 r"""This module contains the scale variation operator for the expanded scheme (``ModSV=expanded``).
 
 The expressions can be obtained using Eqs. (3.33) and (3.38) of :cite:`AbdulKhalek:2019ihb`.
-Note, however, that our definition of the anomalous dimensions :math:`\gamma`
-includes a further minus sign with resepect to :cite:`AbdulKhalek:2019ihb`, as well as
-our definition of the coefficients of the beta function :math:`\beta_k`
-(compare Eq. (3.3) of :cite:`AbdulKhalek:2019ihb`).
-This effectively introduces a minus signs on terms which include a odd number of :math:`\gamma_j` and :math:`\beta_k` .
+Be aware that corresponding the signs of the ingredients there are a number of differences.
+However, the ultimate sign can be obtained by comparing to the exponentiated scheme in the
+trunctated solution.
 """
-
 
 import numba as nb
 import numpy as np
@@ -17,7 +14,7 @@ from .. import beta
 
 
 @nb.njit(cache=True)
-def gamma_1_variation(gamma, L):
+def variation_as1(gamma, L):
     r"""Computes the |NLO| anomalous dimension variation.
 
     Parameters
@@ -32,11 +29,11 @@ def gamma_1_variation(gamma, L):
     gamma_1 : complex
         variation to :math:`\gamma^{(1)}`
     """
-    return L * gamma[0]
+    return -L * gamma[0]
 
 
 @nb.njit(cache=True)
-def gamma_2_variation(gamma, L, beta0, g0e2):
+def variation_as2(gamma, L, beta0, g0e2):
     r"""Computes the |NNLO| anomalous dimension variation.
 
     Parameters
@@ -55,11 +52,11 @@ def gamma_2_variation(gamma, L, beta0, g0e2):
     gamma_2 : complex
         variation to :math:`\gamma^{(2)}`
     """
-    return gamma[1] * L + 1.0 / 2.0 * (beta0 * gamma[0] + g0e2) * L**2
+    return -gamma[1] * L + 1.0 / 2.0 * (beta0 * gamma[0] + g0e2) * L**2
 
 
 @nb.njit(cache=True)
-def gamma_3_variation(gamma, L, beta0, beta1, g0e2, g0e3, g1g0, g0g1):
+def variation_as3(gamma, L, beta0, beta1, g0e2, g0e3, g1g0, g0g1):
     r"""Computes the |N3LO| anomalous dimension variation.
 
     Parameters
@@ -87,11 +84,11 @@ def gamma_3_variation(gamma, L, beta0, beta1, g0e2, g0e3, g1g0, g0g1):
         variation to :math:`\gamma^{(3)}`
     """
     return (
-        gamma[2] * L
+        -gamma[2] * L
         + (1.0 / 2.0)
         * (beta1 * gamma[0] + 2.0 * beta0 * gamma[1] + g1g0 + g0g1)
         * L**2
-        + (1.0 / 6.0)
+        - (1.0 / 6.0)
         * (2.0 * beta0**2 * gamma[0] + 3.0 * beta0 * g0e2 + g0e3)
         * L**3
     )
@@ -121,14 +118,14 @@ def non_singlet_variation(gamma, a_s, order, nf, L):
     """
     sv_ker = 1.0
     if order >= 1:
-        sv_ker += a_s * gamma_1_variation(gamma, L)
+        sv_ker += a_s * variation_as1(gamma, L)
     if order >= 2:
         beta0 = beta.beta_0(nf)
-        sv_ker += a_s**2 * gamma_2_variation(gamma, L, beta0, gamma[0] ** 2)
+        sv_ker += a_s**2 * variation_as2(gamma, L, beta0, gamma[0] ** 2)
     if order >= 3:
         beta1 = beta.beta(1, nf)
         g0g1 = gamma[0] * gamma[1]
-        sv_ker += a_s**3 * gamma_3_variation(
+        sv_ker += a_s**3 * variation_as3(
             gamma, L, beta0, beta1, gamma[0] ** 2, gamma[0] ** 3, g0g1, g0g1
         )
     return sv_ker
@@ -159,18 +156,18 @@ def singlet_variation(gamma, a_s, order, nf, L):
     sv_ker = np.eye(2, dtype=np.complex_)
     gamma = np.ascontiguousarray(gamma)
     if order >= 1:
-        sv_ker += a_s * gamma_1_variation(gamma, L)
+        sv_ker += a_s * variation_as1(gamma, L)
     if order >= 2:
         beta0 = beta.beta_0(nf)
         gamma0e2 = gamma[0] @ gamma[0]
-        sv_ker += a_s**2 * gamma_2_variation(gamma, L, beta0, gamma0e2)
+        sv_ker += a_s**2 * variation_as2(gamma, L, beta0, gamma0e2)
     if order >= 3:
         beta1 = beta.beta(1, nf)
         gamma0e3 = gamma0e2 @ gamma[0]
         # here the product is not commutative
         g1g0 = gamma[1] @ gamma[0]
         g0g1 = gamma[0] @ gamma[1]
-        sv_ker += a_s**3 * gamma_3_variation(
+        sv_ker += a_s**3 * variation_as3(
             gamma, L, beta0, beta1, gamma0e2, gamma0e3, g1g0, g0g1
         )
     return sv_ker
