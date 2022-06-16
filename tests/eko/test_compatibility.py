632b55f803176c07e0b5d229aa1d73e805e4e7ab
Niccolò Laurenti <niclaurenti@gmail.com>
2022-06-16 09:53:27 +0200

    Remove unnecessary tests from test_compatibility.py

--- tests/eko/test_compatibility.py
+++ tests/eko/test_compatibility.py
@@ -20,24 +20,3 @@ operator_dict = {"ev_op_max_order": 2}
 
 def test_compatibility_operators():
     new_theory, new_operator = compatibility.update(theory1, operator_dict)
-
-
-theory2 = {
-    "alphas": 0.1180,
-    "alphaqed": 0.007496,
-    "PTO": 2,
-}
-
-
-def test_compatibility_no_QED():
-    new_theory = compatibility.update(theory2)
-
-
-theory3 = {
-    "alphas": 0.1180,
-    "alphaqed": 0.007496,
-    "QED": 0,
-}
-
-with pytest.raises(KeyError):
-    new_theory = compatibility.update(theory3)
