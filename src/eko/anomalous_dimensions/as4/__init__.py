c6b1143960dc0f757246154f547ed7b6547be841
giacomomagni <giac.magni@gmail.com>
2022-07-05 14:18:06 +0200

    fix docstring style

--- src/eko/anomalous_dimensions/as4/__init__.py
+++ src/eko/anomalous_dimensions/as4/__init__.py
@@ -18,37 +18,37 @@ from .gqg import gamma_qg
 
 @nb.njit(cache=True)
 def gamma_singlet(N, nf, sx):
-    r"""
-      Computes the |N3LO| singlet anomalous dimension matrix
+    r"""Computes the |N3LO| singlet anomalous dimension matrix
 
       .. math::
           \gamma_S^{(3)} = \left(\begin{array}{cc}
-            \gamma_{qq}^{(3)} & \gamma_{qg}^{(3)}\\
-            \gamma_{gq}^{(3)} & \gamma_{gg}^{(3)}
+          \gamma_{qq}^{(3)} & \gamma_{qg}^{(3)}\\
+          \gamma_{gq}^{(3)} & \gamma_{gg}^{(3)}
           \end{array}\right)
 
-      Parameters
-      ----------
-        N : complex
-          Mellin moment
-        nf : int
-          Number of active flavors
-        sx : list
-            harmonic sums cache
+    Parameters
+    ----------
+    N : complex
+      Mellin moment
+    nf : int
+      Number of active flavors
+    sx : list
+      harmonic sums cache
 
-      Returns
-      -------
-        gamma_S_3 : numpy.ndarray
-            |N3LO| singlet anomalous dimension matrix
-            :math:`\gamma_{S}^{(3)}(N)`
+    Returns
+    -------
+    numpy.ndarray
+        |N3LO| singlet anomalous dimension matrix
+        :math:`\gamma_{S}^{(3)}(N)`
 
-      See Also
-      --------
-        gamma_nsp : :math:`\gamma_{ns,+}^{(3)}`
-        gamma_ps : :math:`\gamma_{ps}^{(3)}`
-        gamma_qg : :math:`\gamma_{qg}^{(3)}`
-        gamma_gq : :math:`\gamma_{gq}^{(3)}`
-        gamma_gg : :math:`\gamma_{gg}^{(3)}`
+    See Also
+    --------
+    gamma_nsp : :math:`\gamma_{ns,+}^{(3)}`
+    gamma_ps : :math:`\gamma_{ps}^{(3)}`
+    gamma_qg : :math:`\gamma_{qg}^{(3)}`
+    gamma_gq : :math:`\gamma_{gq}^{(3)}`
+    gamma_gg : :math:`\gamma_{gg}^{(3)}`
+
     """
     gamma_qq = gamma_nsp(N, nf, sx) + gamma_ps(N, nf, sx)
     gamma_S_0 = np.array(
