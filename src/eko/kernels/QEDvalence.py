d8c187bf08f61df48ad8de33d89db480be584693
Niccolò Laurenti <niclaurenti@gmail.com>
2022-07-06 15:57:28 +0200

    Remove unnecessary if

--- src/eko/kernels/QEDvalence.py
+++ src/eko/kernels/QEDvalence.py
@@ -55,8 +55,6 @@ def eko_iterate(gamma_valence, a1, a0, aem, nf, order, ev_op_iterations):
         for i in range(0, order[0] + 1):
             for j in range(0, order[1] + 1):
                 betatot += a_half**2 * betaQCD[i, j] * a_half**i * aem**j
-                if (i, j) == (0, 0):
-                    continue  # this is probably useless
                 gamma += gamma_valence[i, j] * a_half**i * aem**j
         ln = gamma / betatot * delta_a
         ek = np.ascontiguousarray(ad.exp_singlet(ln)[0])
