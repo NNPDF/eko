d92464459b6b31fbf50cf5c318b1ac2ad5c7aaac
Niccolò Laurenti <niclaurenti@gmail.com>
2022-07-07 14:33:59 +0200

    Implement dispatcher for QED singlet and valence

--- src/eko/kernels/QEDsinglet.py
+++ src/eko/kernels/QEDsinglet.py
@@ -5,6 +5,7 @@ import numpy as np
 from .. import anomalous_dimensions as ad
 from .. import beta
 from . import utils
+from .singlet import lo_exact
 
 
 @nb.njit(cache=True)
@@ -52,8 +53,8 @@ def eko_iterate(gamma_singlet, a1, a0, aem, nf, order, ev_op_iterations):
         delta_a = ah - al
         gamma = np.zeros((4, 4), np.complex_)
         betatot = 0
-        for i in range(0, order[0] + 1):
-            for j in range(0, order[1] + 1):
+        for i in range(0, order[0]):
+            for j in range(0, order[1]):
                 betatot += a_half**2 * betaQCD[i, j] * a_half**i * aem**j
                 gamma += gamma_singlet[i, j] * a_half**i * aem**j
         ln = gamma / betatot * delta_a
@@ -61,3 +62,65 @@ def eko_iterate(gamma_singlet, a1, a0, aem, nf, order, ev_op_iterations):
         e = ek @ e
         al = ah
     return e
+
+
+@nb.njit(cache=True)
+def dispatcher(  # pylint: disable=too-many-return-statements
+    order, method, gamma_singlet, a1, a0, aem, nf, ev_op_iterations, ev_op_max_order
+):
+    """
+    Determine used kernel and call it.
+
+    In LO we always use the exact solution.
+
+    Parameters
+    ----------
+        order : tuple(int,int)
+            perturbative order
+        method : str
+            method
+        gamma_singlet : numpy.ndarray
+            singlet anomalous dimensions matrices
+        a1 : float
+            target coupling value
+        a0 : float
+            initial coupling value
+        nf : int
+            number of active flavors
+        ev_op_iterations : int
+            number of evolution steps
+        ev_op_max_order : tuple(int,int)
+            perturbative expansion order of U
+
+    Returns
+    -------
+        e_s : numpy.ndarray
+            singlet EKO
+    """
+    # use always exact in LO
+    #    if order == (1,0):
+    #        return lo_exact(gamma_singlet, a1, a0, nf)
+
+    if method in ["iterate-exact", "iterate-expanded"]:
+        return eko_iterate(gamma_singlet, a1, a0, aem, nf, order, ev_op_iterations)
+
+    #    if method == "perturbative-exact":
+    #        return eko_perturbative(
+    #            gamma_singlet, a1, a0, nf, order, ev_op_iterations, ev_op_max_order, True
+    #        )
+    #    if method == "perturbative-expanded":
+    #        return eko_perturbative(
+    #            gamma_singlet, a1, a0, nf, order, ev_op_iterations, ev_op_max_order, False
+    #        )
+    #    if method in ["truncated", "ordered-truncated"]:
+    #        return eko_truncated(gamma_singlet, a1, a0, nf, order, ev_op_iterations)
+    #    # These methods are scattered for nlo and nnlo
+    #    if method == "decompose-exact":
+    #        if order[0] == 2:
+    #            return nlo_decompose_exact(gamma_singlet, a1, a0, nf)
+    #        return nnlo_decompose_exact(gamma_singlet, a1, a0, nf)
+    #    if method == "decompose-expanded":
+    #        if order[0] == 2:
+    #            return nlo_decompose_expanded(gamma_singlet, a1, a0, nf)
+    #        return nnlo_decompose_expanded(gamma_singlet, a1, a0, nf)
+    raise NotImplementedError("Selected method is not implemented")
