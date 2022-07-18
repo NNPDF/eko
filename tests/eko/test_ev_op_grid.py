0ce9ead9d6c752b9aeda738855cfc1dec1f7ae68
Alessandro Candido <candido.ale@gmail.com>
2022-07-18 12:53:19 +0200

    Recover all tests

--- tests/eko/test_ev_op_grid.py
+++ tests/eko/test_ev_op_grid.py
@@ -112,14 +112,10 @@ class TestOperatorGrid:
         # we can also pass a single number
         opg = opgrid.compute()
         assert len(opg) == 2
-        assert all(
-            k in op for k in ["operators", "operator_errors"] for op in opg.values()
-        )
+        assert all(k in op for k in ["operator", "error"] for op in opg.values())
         opg = opgrid.compute(3)
         assert len(opg) == 1
-        assert all(
-            k in op for k in ["operators", "operator_errors"] for op in opg.values()
-        )
+        assert all(k in op for k in ["operator", "error"] for op in opg.values())
 
     def test_grid_computation_VFNS(self):
         """Checks that the grid can be computed"""
@@ -144,5 +140,5 @@ class TestOperatorGrid:
             )
             sv_opg = sv_opgrid.compute(3)
             np.testing.assert_allclose(
-                opg[3]["operators"], sv_opg[3]["operators"], atol=0.7 * epsilon
+                opg[3]["operator"], sv_opg[3]["operator"], atol=0.7 * epsilon
             )
