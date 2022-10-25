218f77337d2729d4e9c682e7b146ec2b401e8f77
giacomomagni <giac.magni@gmail.com>
2022-10-25 14:40:58 +0200

    add replica varation to g qq ps

--- src/eko/anomalous_dimensions/as4/gps.py
+++ src/eko/anomalous_dimensions/as4/gps.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+# pylint: skip-file
+# fmt: off
 r"""The anomalous dimension :math:`\gamma_{ps}^{(3)}`."""
 import numba as nb
 import numpy as np
 
-from ...harmonics.log_functions import lm11m1, lm12m1, lm13m1
+from ...harmonics.log_functions import lm11m1, lm12m1, lm13m1, lm14m1
 
 
 @nb.njit(cache=True)
@@ -101,30 +103,48 @@ def gamma_ps_nf1(n, sx, variation):
     S1 = sx[0][0]
     S2 = sx[1][0]
     S3 = sx[2][0]
-    common = (
-        -3498.454512979491 / np.power(-1.0 + n, 3)
-        + 5404.444444444444 / np.power(n, 7)
-        + 3425.9753086419755 / np.power(n, 6)
-        + 20515.223982421852 / np.power(n, 5)
-    )
-    if variation != "b":
-        fit_a = (
-            5616.488840096642 * (np.power(-1.0 + n, -2) - 1.0 / np.power(n, 2))
-            - 5048.182296022696 * (1 / (-1.0 + n) - 1.0 / n)
-            - 2770.502633721018 * lm11m1(n, S1)
-            + 166.47929899655728 * lm13m1(n, S1, S2, S3)
-        )
-        fit = fit_a
-    if variation != "a":
-        fit_b = (
-            10123.868772921842 * (1 / (-1.0 + n) - 1.0 / n)
-            + 256607.74954106557 * (-1.0 / (1.0 + n) + 1 / (2.0 + n))
-            - 173590.77057620537 * lm11m1(n, S1)
-            - 32501.05126256758 * lm12m1(n, S1, S2)
-        )
-        fit = fit_b
-    if variation == "best":
-        fit = (fit_a + fit_b) / 2
+    S4 = sx[3][0]
+    common = -3498.454512979491/np.power(-1. + n,3) + 5404.444444444444/np.power(n,7) + 3425.9753086419755/np.power(n,6) + 20515.223982421852/np.power(n,5) + 247.55054124312667*lm13m1(n,S1,S2,S3) + 56.46090534979424*lm14m1(n,S1,S2,S3,S4)
+    if variation == 1:
+        fit = 6056.204267761437/np.power(-1. + n,2) + 30316.627474493198/np.power(n,4) - (3513.6057300753923*n)/np.power(-1. + n,2) + (1403.200030308275*np.power(S1,2))/np.power(n,2)
+    elif variation == 2:
+        fit = 2199.5733178661926/np.power(-1. + n,2) + 50220.7610650693/np.power(n,4) - 25639.474370323645/np.power(n,3) - (30.46463739672361*np.power(S1,2))/np.power(n,2)
+    elif variation == 3:
+        fit = 7596.245323405077/np.power(-1. + n,2) - 44312.43712535094/np.power(n,4) - 11176.594525482886/np.power(n,2) + (89.08169320197096*np.power(S1,2))/np.power(n,2)
+    elif variation == 4:
+        fit = -10210.124215128611*(1/(-1. + n) - 1./n) + 8068.074302547951/np.power(-1. + n,2) - 14490.265233715903/np.power(n,4) + (59.68762365947316*np.power(S1,2))/np.power(n,2)
+    elif variation == 5:
+        fit = -5024.075626622562/np.power(-1. + n,2) + 115212.96831222711/np.power(n,4) - 7613.740540053544/(1. + n) + (2926.1163505761606*np.power(S1,2))/np.power(n,2)
+    elif variation == 6:
+        fit = 171737.67330653904/np.power(n,4) - 26758.396554806663/(2. - 3.*n + np.power(n,3)) + (7669.156759254967*n)/(2. - 3.*n + np.power(n,3)) - (8419.34201466332*np.power(n,2))/(2. - 3.*n + np.power(n,3)) + (3138.9513984212067*np.power(S1,2))/np.power(n,2)
+    elif variation == 7:
+        fit = 18207.919823884586/np.power(-1. + n,2) - 198879.253313494/np.power(n,4) - 35873.11939644635/np.power(1. + n,2) + (374.124697728681*np.power(S1,2))/np.power(n,2)
+    elif variation == 8:
+        fit = 4056.871300844477/np.power(-1. + n,2) - 1580.9624428578652/np.power(n,4) - 48639.30941850736/np.power(1. + n,3) - (57.92378481495256*np.power(S1,2))/np.power(n,2)
+    elif variation == 9:
+        fit = 15328.653205439949/np.power(-1. + n,2) - 154876.36205635773/np.power(n,4) - (10717.031829005755*S1)/np.power(n,2) + (566.2084850946995*np.power(S1,2))/np.power(n,2)
+    elif variation == 10:
+        fit = 7574.130950256209/np.power(-1. + n,2) - (2938.4183200076977*n)/np.power(-1. + n,2) - (1754.4090757178858*S1)/np.power(n,2) + (1266.1820959910863*np.power(S1,2))/np.power(n,2)
+    elif variation == 11:
+        fit = 5414.403316469745/np.power(-1. + n,2) - 19361.30773107909/np.power(n,3) - (2624.2079197404937*S1)/np.power(n,2) + (115.63872018596471*np.power(S1,2))/np.power(n,2)
+    elif variation == 12:
+        fit = 4497.2070221323/np.power(-1. + n,2) - 15656.01349052962/np.power(n,2) + (4295.232820194323*S1)/np.power(n,2) - (102.14388946214252*np.power(S1,2))/np.power(n,2)
+    elif variation == 13:
+        fit = -11263.98504105731*(1/(-1. + n) - 1./n) + 7318.657398492729/np.power(-1. + n,2) + (1106.1824299928326*S1)/np.power(n,2) + (7.405939114302806*np.power(S1,2))/np.power(n,2)
+    elif variation == 14:
+        fit = 3657.86045280617/np.power(-1. + n,2) - 4365.920100861757/(1. + n) - (4571.602465122713*S1)/np.power(n,2) + (1919.4420411747335*np.power(S1,2))/np.power(n,2)
+    elif variation == 15:
+        fit = 3431.485602677906/(2. - 3.*n + np.power(n,3)) + (11696.614112414916*n)/(2. - 3.*n + np.power(n,3)) - (3992.348524430385*np.power(n,2))/(2. - 3.*n + np.power(n,3)) - (5635.147029185682*S1)/np.power(n,2) + (1786.1715601033052*np.power(S1,2))/np.power(n,2)
+    elif variation == 16:
+        fit = 5194.538684437673/np.power(-1. + n,2) + 126262.11753376822/np.power(1. + n,2) - (48437.619142666306*S1)/np.power(n,2) + (1242.2831099052862*np.power(S1,2))/np.power(n,2)
+    elif variation == 17:
+        fit = 3940.6234297395335/np.power(-1. + n,2) - 49140.93517917013/np.power(1. + n,3) + (110.5266359153311*S1)/np.power(n,2) - (64.36057078890452*np.power(S1,2))/np.power(n,2)
+    elif variation == 18:
+        fit = 3782.941245959016/np.power(-1. + n,2) - (8396.526563401267*S1)/np.power(n,2) - (3187.7284289163363*np.power(S1,2))/np.power(n,2) + 23664.13083030526*lm12m1(n,S1,S2)
+    elif variation == 19:
+        fit = 4033.1787588235884/np.power(-1. + n,2) - (11884.628852599903*S1)/np.power(n,2) - (270.18257796608805*np.power(S1,2))/np.power(n,2) - 19866.64076207529*lm11m1(n,S1)
+    else:
+        fit = 5363.316167065476/np.power(-1. + n,2) - 1130.2162766413642/(-1. + n) - 2455.328948076197/np.power(n,4) - 2368.462215863302/np.power(n,3) - 1412.2425271585528/np.power(n,2) + 1130.2162766413642/n - (339.58021316226785*n)/np.power(-1. + n,2) - 5146.328663035657/np.power(1. + n,3) + 4757.315691437993/np.power(1. + n,2) - 630.5084547850158/(1. + n) - 1227.7321553751976/(2. - 3.*n + np.power(n,3)) + (1019.2510985089411*n)/(2. - 3.*n + np.power(n,3)) - (653.246870478616*np.power(n,2))/(2. - 3.*n + np.power(n,3)) - (4658.380578491449*S1)/np.power(n,2) + (588.5099924273683*np.power(S1,2))/np.power(n,2) - 1045.6126716881731*lm11m1(n,S1) + 1245.4805700160664*lm12m1(n,S1,S2)
     return common + fit
 
 
@@ -150,29 +170,48 @@ def gamma_ps_nf2(n, sx, variation):
     S1 = sx[0][0]
     S2 = sx[1][0]
     S3 = sx[2][0]
-    common = (
-        -568.8888888888889 / np.power(n, 7)
-        + 455.1111111111111 / np.power(n, 6)
-        - 1856.79012345679 / np.power(n, 5)
-    )
-    if variation != "b":
-        fit_a = (
-            307.3477946813065 * (np.power(-1.0 + n, -2) - 1.0 / np.power(n, 2))
-            - 260.95770652652595 * (1 / (-1.0 + n) - 1.0 / n)
-            - 289.6164417354449 * lm11m1(n, S1)
-            + 6.771796582755947 * lm13m1(n, S1, S2, S3)
-        )
-        fit = fit_a
-    if variation != "a":
-        fit_b = (
-            567.5486082299064 * (1 / (-1.0 + n) - 1.0 / n)
-            + 13995.76246367764 * (-1.0 / (1.0 + n) + 1 / (2.0 + n))
-            - 9595.211752370842 * lm11m1(n, S1)
-            - 1762.088541491044 * lm12m1(n, S1, S2)
-        )
-        fit = fit_b
-    if variation == "best":
-        fit = (fit_a + fit_b) / 2
+    S4 = sx[3][0]
+    common = -568.8888888888889/np.power(n,7) + 455.1111111111111/np.power(n,6) - 1856.79012345679/np.power(n,5) - 40.559670781893004*lm13m1(n,S1,S2,S3) - 18.106995884773664*lm14m1(n,S1,S2,S3,S4)
+    if variation == 1:
+        fit = 247.19758779416802/np.power(-1. + n,2) + 2074.5011247403218/np.power(n,4) - (150.21706080810173*n)/np.power(-1. + n,2) + (75.02063984800385*np.power(S1,2))/np.power(n,2)
+    elif variation == 2:
+        fit = 82.31518196362028/np.power(-1. + n,2) + 2925.4618402000056/np.power(n,4) - 1096.1635358250617/np.power(n,3) + (13.727225301333212*np.power(S1,2))/np.power(n,2)
+    elif variation == 3:
+        fit = 313.03890827084507/np.power(-1. + n,2) - 1116.1126197351734/np.power(n,4) - 477.83254822558797/np.power(n,2) + (18.83818532146737*np.power(S1,2))/np.power(n,2)
+    elif variation == 4:
+        fit = -436.51307742184764*(1/(-1. + n) - 1./n) + 333.2109958344149/np.power(-1. + n,2) + 158.87363445081542/np.power(n,4) + (17.581501711787457*np.power(S1,2))/np.power(n,2)
+    elif variation == 5:
+        fit = -226.5172244337121/np.power(-1. + n,2) + 5704.071365034062/np.power(n,4) - 325.50997850797233/(1. + n) + (140.12982712224627*np.power(S1,2))/np.power(n,2)
+    elif variation == 6:
+        fit = 8120.670067219227/np.power(n,4) - 1167.446971905148/(2. - 3.*n + np.power(n,3)) + (316.15608829446927*n)/(2. - 3.*n + np.power(n,3)) - (359.95182969881733*np.power(n,2))/(2. - 3.*n + np.power(n,3)) + (149.22915635277718*np.power(S1,2))/np.power(n,2)
+    elif variation == 7:
+        fit = 766.719450760205/np.power(-1. + n,2) - 7724.302214687518/np.power(n,4) - 1533.6821976427764/np.power(1. + n,2) + (31.024618755759825*np.power(S1,2))/np.power(n,2)
+    elif variation == 8:
+        fit = 161.72017657063654/np.power(-1. + n,2) + 710.7845975180813/np.power(n,4) - 2079.4746655957883/np.power(1. + n,3) + (12.553265348411983*np.power(S1,2))/np.power(n,2)
+    elif variation == 9:
+        fit = 643.6222674298624/np.power(-1. + n,2) - 5843.048169839509/np.power(n,4) - (458.18488060855356*S1)/np.power(n,2) + (39.236770085098755*np.power(S1,2))/np.power(n,2)
+    elif variation == 10:
+        fit = 351.06602146998284/np.power(-1. + n,2) - (110.85823271530677*n)/np.power(-1. + n,2) - (120.0504114085077*S1)/np.power(n,2) + (65.64479950792175*np.power(S1,2))/np.power(n,2)
+    elif variation == 11:
+        fit = 269.58559133594/np.power(-1. + n,2) - 730.4475144025814/np.power(n,3) - (152.86546772966202*S1)/np.power(n,2) + (22.23804409954797*np.power(S1,2))/np.power(n,2)
+    elif variation == 12:
+        fit = 234.9823629423017/np.power(-1. + n,2) - 590.6572168807367/np.power(n,2) + (108.18550877168953*S1)/np.power(n,2) + (14.021720692724948*np.power(S1,2))/np.power(n,2)
+    elif variation == 13:
+        fit = -424.9583752186764*(1/(-1. + n) - 1./n) + 341.4277251589498/np.power(-1. + n,2) - (12.1283648148571*S1)/np.power(n,2) + (18.154726639345217*np.power(S1,2))/np.power(n,2)
+    elif variation == 14:
+        fit = 203.3161850269302/np.power(-1. + n,2) - 164.71384733147758/(1. + n) - (226.33516951804955*S1)/np.power(n,2) + (90.29045412205663*np.power(S1,2))/np.power(n,2)
+    elif variation == 15:
+        fit = 260.0911311152919/(2. - 3.*n + np.power(n,3)) + (506.59567994091395*n)/(2. - 3.*n + np.power(n,3)) - (150.6200457533072*np.power(n,2))/(2. - 3.*n + np.power(n,3)) - (266.4597052191757*S1)/np.power(n,2) + (85.26253488261445*np.power(S1,2))/np.power(n,2)
+    elif variation == 16:
+        fit = 261.2907190988198/np.power(-1. + n,2) + 4763.513456671208/np.power(1. + n,2) - (1881.2762145085085*S1)/np.power(n,2) + (64.74315819187083*np.power(S1,2))/np.power(n,2)
+    elif variation == 17:
+        fit = 213.98403435481964/np.power(-1. + n,2) - 1853.9488373207428/np.power(1. + n,3) - (49.691648766849966*S1)/np.power(n,2) + (15.447178703830566*np.power(S1,2))/np.power(n,2)
+    elif variation == 18:
+        fit = 208.0351304477161/np.power(-1. + n,2) - (370.638764046496*S1)/np.power(n,2) - (102.38867826526625*np.power(S1,2))/np.power(n,2) + 892.7808898852011*lm12m1(n,S1,S2)
+    elif variation == 19:
+        fit = 217.4758857841338/np.power(-1. + n,2) - (502.2350220325821*S1)/np.power(n,2) + (7.682095082408119*np.power(S1,2))/np.power(n,2) - 749.5123039076917*lm11m1(n,S1)
+    else:
+        fit = 243.2879473584018/np.power(-1. + n,2) - 45.34060277055389/(-1. + n) + 263.73155920527955/np.power(n,4) - 96.13742369619173/np.power(n,3) - 56.23630342664867/np.power(n,2) + 45.34060277055389/n - (13.740804922284658*n)/np.power(-1. + n,2) - 207.02228962718584/np.power(1. + n,3) + 169.99111889623322/np.power(1. + n,2) - 25.80125399154999/(1. + n) - 47.75557056788716/(2. - 3.*n + np.power(n,3)) + (43.30272464396754*n)/(2. - 3.*n + np.power(n,3)) - (26.87220397116445*np.power(n,2))/(2. - 3.*n + np.power(n,3)) - (206.93053367797646*S1)/np.power(n,2) + (40.9703801844179*np.power(S1,2))/np.power(n,2) - 39.44801599514167*lm11m1(n,S1) + 46.98846788869479*lm12m1(n,S1,S2)
     return common + fit
 
 
