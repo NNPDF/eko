dba6d74e49f087d1971b4e21a8401647bb454428
Alessandro Candido <candido.ale@gmail.com>
2022-12-03 19:08:47 +0100

    Complete runcards generating subcommand

--- src/ekobox/cli/library.py
+++ src/ekobox/cli/library.py
@@ -1,16 +1,27 @@
 """Library of reusable options and elements."""
-import logging
 import pathlib
 
 import click
 
-DESTINATION = pathlib.Path.cwd().absolute() / "theory"
-"""Default destination for generated files"""
 
-option_dest = click.option(
-    "-d",
-    "--destination",
-    type=click.Path(path_type=pathlib.Path),
-    default=DESTINATION,
-    help="Alternative destination path to store the resulting table (default: $PWD/theory)",
-)
+def destination(dest: pathlib.Path):
+    """Build click option for destination.
+
+    Parameters
+    ----------
+    dest : pathlib.Path
+        default destination
+
+    Returns
+    -------
+    click.option
+        generated option
+
+    """
+    return click.option(
+        "-d",
+        "--destination",
+        type=click.Path(path_type=pathlib.Path),
+        default=dest,
+        help="Alternative destination path to store the resulting table (default: $PWD/theory)",
+    )
