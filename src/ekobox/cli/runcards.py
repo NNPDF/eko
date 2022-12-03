222ec58b2a158533e8a0335b0dc8655a2ef968ab
Alessandro Candido <candido.ale@gmail.com>
2022-12-03 19:08:47 +0100

    Complete runcards generating subcommand

--- src/ekobox/cli/runcards.py
+++ src/ekobox/cli/runcards.py
@@ -1,10 +1,18 @@
 """Subcommand to manage runcards."""
 import logging
+import pathlib
 
+from ..cards import generate_operator as opgen
+from ..cards import generate_theory as thgen
 from . import library as lib
 from .base import command
 
 _logger = logging.getLogger(__name__)
+
+DESTINATION = pathlib.Path.cwd().absolute() / "runcards"
+"""Default destination for generated files"""
+
+option_dest = lib.destination(DESTINATION)
 
 
 @command.group("runcards")
@@ -13,12 +21,15 @@ def subcommand():
 
 
 @subcommand.command("example")
-@lib.option_dest
-def sub_example(destination):
+@option_dest
+def sub_example(destination: pathlib.Path):
     """Generate example runcards.
 
     Pay attention that they are in no way intended as defaults, but just
     examples to quickstart.
 
     """
-    _logger.info("Generated")
+    destination.mkdir(parents=True, exist_ok=True)
+    thgen(0, 1.65, path=destination / "theory.yaml")
+    opgen([1e5], path=destination / "operator.yaml")
+    _logger.info(f"Runcards generated to '{destination}'")
