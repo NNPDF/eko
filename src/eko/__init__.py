6709e6227791100f98fa62fb7d85bb65c6ae8fd8
Alessandro Candido <candido.ale@gmail.com>
2022-12-08 12:18:31 +0100

    Rearrange runner and compatibility in new subpackages

--- src/eko/__init__.py
+++ src/eko/__init__.py
@@ -1,40 +1,6 @@
 """Evolution Kernel Operators."""
-from . import io, runner, version
+from . import io, version
 from .io.struct import EKO
+from .runner import solve
 
 __version__ = version.__version__
-
-
-def run_dglap(theory_card, operators_card):
-    r"""Solve DGLAP equations in terms of evolution kernel operators (EKO).
-
-    The EKO :math:`\mathbf E_{k,j}(a_s^1\leftarrow a_s^0)` is determined in order
-    to fullfill the following evolution
-
-    .. math::
-        \mathbf f(x_k,a_s^1) = \mathbf E_{k,j}(a_s^1\leftarrow a_s^0) \mathbf f(x_j,a_s^0)
-
-    The configuration is split between the theory settings, representing
-    Standard Model parameters and other defining features of the theory
-    calculation, and the operator settings, those that are more closely related
-    to the solution of the |DGLAP| equation itself, and determine the resulting
-    operator features.
-
-    Parameters
-    ----------
-    theory_card : dict
-        theory parameters and related settings
-    operator_card : dict
-        solution configurations, and further EKO options
-
-    Returns
-    -------
-    EKO
-        computed operator
-
-    Note
-    ----
-        For further information about EKO inputs and output see :doc:`/code/IO`
-
-    """
-    return runner.Runner(theory_card, operators_card).get_output()
---
+++ src/eko/runner/commons.py
@@ -0,0 +1,11 @@
+"""Runners common utilities."""
+
+BANNER = r"""
+oooooooooooo oooo    oooo  \\ .oooooo.
+`888'     `8 `888   .8P'  //////    `Y8b
+ 888          888  d8'   \\o\/////    888
+ 888oooo8     88888     \\\\/8/////   888
+ 888    "     888`88b.      888 ///   888
+ 888       o  888  `88b.    `88b //  d88'
+o888ooooood8 o888o  o888o     `Y8bood8P'
+"""
