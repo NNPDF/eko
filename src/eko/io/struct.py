f7b7b0ea6c71f4836e8a0ad5fae8e0309207c20d
Alessandro Candido <candido.ale@gmail.com>
2022-12-10 10:17:17 +0100

    Provide context manager also from builder

--- src/eko/io/struct.py
+++ src/eko/io/struct.py
@@ -413,71 +413,6 @@ class Metadata(DictLike):
 
 
 @dataclass
-class Builder:
-    """Build EKO instances."""
-
-    path: pathlib.Path
-    """Path on disk to ."""
-    access: AccessConfigs
-    """Access related configurations."""
-
-    # optional arguments, required at build time
-    theory: Optional[TheoryCard] = None
-    operator: Optional[OperatorCard] = None
-
-    def load_cards(self, theory: TheoryCard, operator: OperatorCard):
-        """Load both theory and operator card."""
-        self.theory = theory
-        self.operator = operator
-
-        return self
-
-    def build(self):
-        """Build EKO instance.
-
-        Returns
-        -------
-        EKO
-            the constructed instance
-
-        Raises
-        ------
-        RuntimeError
-            if not enough information is available (at least one card missing)
-
-        """
-        missing = []
-        for card in ["theory", "operator"]:
-            if getattr(self, card) is None:
-                missing.append(card)
-
-        if len(missing) > 0:
-            raise RuntimeError(
-                f"Can not build an EKO, since following cards are missing: {missing}"
-            )
-        # tell the static analyzer as well
-        assert self.theory is not None
-        assert self.operator is not None
-
-        metadata = Metadata(
-            _path=self.path,
-            mu20=self.operator.mu20,
-            rotations=copy.deepcopy(self.operator.rotations),
-        )
-        InternalPaths(self.path).bootstrap(
-            theory=self.theory.raw,
-            operator=self.operator.raw,
-            metadata=metadata.raw,
-        )
-
-        return EKO(
-            _operators={mu2: None for mu2 in self.operator.mu2grid},
-            metadata=metadata,
-            access=self.access,
-        )
-
-
-@dataclass
 class EKO:
     """Operator interface.
 
@@ -937,3 +872,87 @@ class EKO:
 
         """
         return dict(Q2grid=self.mu2grid.tolist(), metadata=self.metadata.raw)
+
+
+@dataclass
+class Builder:
+    """Build EKO instances."""
+
+    path: pathlib.Path
+    """Path on disk to ."""
+    access: AccessConfigs
+    """Access related configurations."""
+
+    # optional arguments, required at build time
+    theory: Optional[TheoryCard] = None
+    operator: Optional[OperatorCard] = None
+
+    eko: Optional[EKO] = None
+
+    def load_cards(self, theory: TheoryCard, operator: OperatorCard):
+        """Load both theory and operator card."""
+        self.theory = theory
+        self.operator = operator
+
+        return self
+
+    def build(self) -> EKO:
+        """Build EKO instance.
+
+        Returns
+        -------
+        EKO
+            the constructed instance
+
+        Raises
+        ------
+        RuntimeError
+            if not enough information is available (at least one card missing)
+
+        """
+        missing = []
+        for card in ["theory", "operator"]:
+            if getattr(self, card) is None:
+                missing.append(card)
+
+        if len(missing) > 0:
+            raise RuntimeError(
+                f"Can not build an EKO, since following cards are missing: {missing}"
+            )
+        # tell the static analyzer as well
+        assert self.theory is not None
+        assert self.operator is not None
+
+        metadata = Metadata(
+            _path=self.path,
+            mu20=self.operator.mu20,
+            rotations=copy.deepcopy(self.operator.rotations),
+        )
+        InternalPaths(self.path).bootstrap(
+            theory=self.theory.raw,
+            operator=self.operator.raw,
+            metadata=metadata.raw,
+        )
+
+        self.eko = EKO(
+            _operators={mu2: None for mu2 in self.operator.mu2grid},
+            metadata=metadata,
+            access=self.access,
+        )
+
+        return self.eko
+
+    def __enter__(self):
+        """Allow Builder to be used in :obj:`with` statements."""
+        return self
+
+    def __exit__(self, exc_type: type, _exc_value, _traceback):
+        """Ensure EKO to be closed properly."""
+        if exc_type is not None:
+            return
+
+        # assign to variable to help type checker, otherwise self.eko might be
+        # a property, and its type can change at every evaluation
+        eko = self.eko
+        if eko is not None:
+            eko.close()
