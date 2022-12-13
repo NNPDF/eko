95199e95a2465eaff906821c7f7fef5bfa6f46cb
Alessandro Candido <candido.ale@gmail.com>
2022-12-13 13:17:59 +0100

    Fix ekobox product test

--- tests/ekobox/test_utils.py
+++ tests/ekobox/test_utils.py
@@ -1,39 +1,44 @@
+import copy
+
 import numpy as np
 import pytest
 
 import eko
-from eko.io import runcards
+from eko import interpolation
 from ekobox import cards, utils
 
 
 def test_ekos_product(tmp_path):
     # Generating two ekos
-    op1 = cards.generate_operator(
-        [60.0, 80.0, 100.0],
-        update={
-            "interpolation_xgrid": [0.1, 0.5, 1.0],
-            "interpolation_polynomial_degree": 1,
-        },
-    )
-    theory1 = cards.generate_theory(0, 5.0)
+    mu01 = 5.0
+    mu2grid1 = np.array([60.0, 80.0, 100.0])
+    xgrid = interpolation.XGrid([0.1, 0.5, 1.0])
 
-    op2 = cards.generate_operator(
-        [80.0, 100.0, 120.0],
-        update={
-            "interpolation_xgrid": [0.1, 0.5, 1.0],
-            "interpolation_polynomial_degree": 1,
-        },
-    )
-    theory2 = cards.generate_theory(0, 10.0)
-    theory_err = cards.generate_theory(0, 5.0)
+    theory = cards.example.theory()
+    theory.order = (1, 0)
 
-    new_theory1, new_op1 = runcards.update(theory1, op1)
-    new_theory2, new_op2 = runcards.update(theory2, op2)
+    op1 = cards.example.operator()
+    op1.mu0 = mu01
+    op1.mu2grid = mu2grid1
+    op1.rotations.xgrid = xgrid
+    op1.configs.interpolation_polynomial_degree = 1
 
-    eko_ini = eko.solve(new_theory1, new_op1, path=tmp_path / "ini.tar")
-    eko_fin = eko.solve(new_theory2, new_op2, path=tmp_path / "fin.tar")
+    mu02 = 10.0
+    mu2grid2 = np.array([80.0, 100.0, 120.0])
+
+    op2 = cards.example.operator()
+    op2.mu0 = mu02
+    op2.mu2grid = mu2grid2
+    op2.rotations.xgrid = xgrid
+    op2.configs.interpolation_polynomial_degree = 1
+
+    op_err = copy.deepcopy(op2)
+    op_err.mu0 = mu01
+
+    eko_ini = eko.solve(theory, op1, path=tmp_path / "ini.tar")
+    eko_fin = eko.solve(theory, op2, path=tmp_path / "fin.tar")
     # Test_error
-    eko_fin_err = eko.solve(theory_err, op2, path=tmp_path / "fin_err.tar")
+    eko_fin_err = eko.solve(theory, op_err, path=tmp_path / "fin_err.tar")
     with pytest.raises(ValueError):
         _ = utils.ekos_product(eko_ini, eko_fin_err)
     # product is copied
