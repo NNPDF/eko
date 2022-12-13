7267a8ed58187898b7274850f575e86052c42ccb
Alessandro Candido <candido.ale@gmail.com>
2022-12-13 22:33:37 +0100

    Fix solve test

--- tests/eko/runner/test_init.py
+++ tests/eko/runner/test_init.py
@@ -1,16 +1,20 @@
+import pathlib
+
 import eko
 
 
 class MockRunner:
-    def __init__(self, *_args):
-        pass
+    def __init__(self, theory, operator, path: pathlib.Path):
+        self.path = path
 
-    def get_output(self):
-        return "output"
+    def compute(self):
+        self.path.write_text("output", encoding="utf-8")
 
 
-def test_run(monkeypatch):
-    # just test, that it is a shortcut to get_output
+def test_run(monkeypatch, tmp_path: pathlib.Path):
+    # just test, that it is a shortcut to 'compute'
+    path = tmp_path / "eko.tar"
     monkeypatch.setattr(eko.runner.legacy, "Runner", MockRunner)
-    o = eko.solve({}, {})
-    assert o == "output"
+    eko.solve({}, {}, path=path)
+    out = path.read_text(encoding="utf-8")
+    assert out == "output"
