1d233239106b83dfc71095254ac7f8344b994f3b
giacomomagni <giac.magni@gmail.com>
2023-01-11 17:08:40 +0100

    first attempt on N3LO expanded test

--- tests/eko/scale_variations/test_expanded.py
+++ tests/eko/scale_variations/test_expanded.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from eko import basis_rotation as br
 from eko.anomalous_dimensions import gamma_ns, gamma_singlet
-from eko.beta import beta_qcd_as2
+from eko.beta import beta_qcd_as2, beta_qcd_as3
 from eko.kernels import non_singlet, singlet
 from eko.scale_variations import Modes, expanded, exponentiated
 
@@ -63,22 +63,15 @@ def test_scale_variation_a_vs_b():
         Effects due to non commutativity are neglected thus,
         the accuracy of singlet quantities is slightly worse.
         """
+        # Note that scheme B is expended in terms of alpha(xi_F*Q)
+        # so once you have computed the difference A -B 
+        # you need to subtract some terms coming from the variation of 
+        # alpha
         if pto[0] >= 2:
             diff = g[0] * k * a0 - 2 * a1 * k * g[0]
         if pto[0] >= 3:
             b0 = beta_qcd_as2(nf)
             g02 = g[0] @ g[0] if is_singlet else g[0] ** 2
-            # This would be the difference if scheme B was expanded in terms
-            # of alpha(Q). However, it is expanded in terms of alpha(xi_F*Q)
-            # so one of the terms gets canceled by the change of alpha.
-            # diff += (
-            #    -2 * a1**2 * g[1] * k
-            #    + a0**2 * g[1] * k
-            #    + a1**2 * b0 * g[0] * k**2
-            #    - 0.5 * a0**2 * b0 * g[0] * k**2
-            #    - a1 * a0 * g02 * k**2
-            #    + 0.5 * a0**2 * g02 * k**2
-            # )
             diff += (
                 -2 * a1**2 * g[1] * k
                 + a0**2 * g[1] * k
@@ -86,13 +79,35 @@ def test_scale_variation_a_vs_b():
                 - a1 * a0 * g02 * k**2
                 + 0.5 * a0**2 * g02 * k**2
             )
+        if pto[0] >= 4:
+            b1 = beta_qcd_as3(nf)
+            g0g1 = g[0] @ g[1] if is_singlet else g[0] * g[1]
+            g03 = g02 @ g[0] if is_singlet else g02 * g[0]
+            # TODO: double check this
+            diff += (
+                a0**3 * g[2] * k
+                - 2 * a1**3 * g[2] * k
+                - 1 / 2 * a0**3 * b1 * g[0] * k**2
+                + a1**3 * b1 * g[0] * k**2
+                - a0**3 * b0 * g[1] * k**2
+                + 2 * a1**3 * b0 * g[1] * k**2
+                + a0**3 * g0g1 * k**2
+                - a0**2 * a1 * g0g1 * k**2
+                - a0 * a1**2 * g0g1 * k**2
+                + 1 / 3 * a0**3 * b0**2 * g[0] * k**3
+                - 5 / 3 * a1**3 * b0**2 * g[0] * k**3
+                - 1 / 2 * a0**3 * b0 * g02 * k**3
+                + 1 / 2 * a0**2 * a1 * b0 * g02 * k**3
+                + 1 / 2 * a0 * a1**2 * b0 * g02 * k**3
+                + 1 / 6 * a0**3 * g03 * k**3
+                - 1 / 2 * a0**2 * a1 * g03 * k**3
+                + 1 / 2 * a0 * a1**2 * g03 * k**3
+                - 1 / 3 * a1**3 * g03 * k**3
+            )
         return diff
 
-    # TODO: perform this test also at N3LO, once evolution kernels
-    # will be implemented
-
     for L in [np.log(0.5), np.log(2)]:
-        for order in [(2, 0), (3, 0)]:
+        for order in [(2, 0), (3, 0), (4, 0)]:
             # Non singlet kernels
             gns = gamma_ns(order, br.non_singlet_pids_map["ns+"], n, nf)
             ker = non_singlet.dispatcher(
