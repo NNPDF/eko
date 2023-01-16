bc4a967d51b98052e2dd034b5bd16b54cc3f67b9
aschaus <adrianneschauss@gmail.com>
2023-01-16 13:41:53 +0100

    adding LO splitting functions

---
+++ src/ekore/anomalous_dimensions/polarized/spacelike/__init__.py
@@ -0,0 +1,77 @@
+r"""
+This module contains the polarized spacelike Altarelli-Parisi splitting kernels.
+
+Normalization is given by
+
+.. math::
+    \mathbf{P}(x) = \sum\limits_{j=0} a_s^{j+1} \mathbf P^{(j)}(x)
+
+with :math:`a_s = \frac{\alpha_S(\mu^2)}{4\pi}`.
+"""
+
+import numba as nb
+import numpy as np
+
+from .... import harmonics
+from . import as1
+
+
+@nb.njit(cache=True)
+def gamma_ns(order, mode, n, nf):
+    r"""Computes the tower of the non-singlet anomalous dimensions
+
+    Parameters
+    ----------
+    order : tuple(int,int)
+        perturbative orders
+    mode : 10201 | 10101 | 10200
+        sector identifier
+    n : complex
+        Mellin variable
+    nf : int
+        Number of active flavors
+
+    Returns
+    -------
+    numpy.ndarray
+        non-singlet anomalous dimensions
+
+    """
+    # cache the s-es
+    sx = harmonics.sx(n, max_weight=order[0] + 1)
+    # now combine
+    gamma_ns = np.zeros(order[0], np.complex_)
+    gamma_ns[0] = as1.gamma_ns(n, sx[0])
+    # NLO and beyond
+    if order[0] >= 2:
+        raise NotImplementedError("Polarised beyond LO is not yet implemented")
+    return gamma_ns
+
+
+@nb.njit(cache=True)
+def gamma_singlet(order, n, nf):
+    r"""Computes the tower of the singlet anomalous dimensions matrices
+
+    Parameters
+    ----------
+    order : tuple(int,int)
+        perturbative orders
+    n : complex
+        Mellin variable
+    nf : int
+        Number of active flavors
+
+    Returns
+    -------
+    numpy.ndarray
+        singlet anomalous dimensions matrices
+
+    """
+    # cache the s-es
+    sx = harmonics.sx(n, max_weight=order[0])
+
+    gamma_s = np.zeros((order[0], 2, 2), np.complex_)
+    gamma_s[0] = as1.gamma_singlet(n, sx[0], nf)
+    if order[0] >= 2:
+        raise NotImplementedError("Polarised beyond LO is not yet implemented")
+    return gamma_s
---
+++ src/ekore/anomalous_dimensions/polarized/spacelike/as1.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""This file contains the leading-order Altarelli-Parisi splitting kernels."""
+
+import numba as nb
+import numpy as np
+
+from .....eko import constants
+from ...unpolarized.space_like.as1 import gamma_ns
+
+
+@nb.njit(cache=True)
+def gamma_pqg(N, nf):
+    """
+    Computes the leading-order polarised quark-gluon anomalous dimension
+    #require citation
+
+    Parameters
+    ----------
+      N : complex
+        Mellin moment
+      nf : int
+        Number of active flavors
+
+    Returns
+    -------
+      gamma_qg : complex
+        Leading-order polarised quark-gluon anomalous dimension :math:`\\gamma_{qg}^{(0)}(N)`
+    """
+    gamma = -(N - 1) / N / (N + 1)
+    result = 2.0 * constants.TR * 2.0 * nf * gamma
+    return result
+
+
+@nb.njit(cache=True)
+def gamma_pgq(N):
+    """
+    Computes the leading-order polarised gluon-quark anomalous dimension
+
+
+    Parameters
+    ----------
+      N : complex
+        Mellin moment
+
+    Returns
+    -------
+      gamma_gq : complex
+        Leading-order gluon-quark anomalous dimension :math:`\\gamma_{gq}^{(0)}(N)`
+    """
+    gamma = -(N + 2) / N / (N + 1)
+    result = 2.0 * constants.CF * gamma
+    return result
+
+
+@nb.njit(cache=True)
+def gamma_pgg(N, s1, nf):
+    """
+    Computes the leading-order polarised gluon-gluon anomalous dimension
+
+
+    Parameters
+    ----------
+      N : complex
+        Mellin moment
+      s1 : complex
+        harmonic sum :math:`S_{1}`
+      nf : int
+        Number of active flavors
+
+    Returns
+    -------
+      gamma_gg : complex
+        Leading-order gluon-gluon anomalous dimension :math:`\\gamma_{gg}^{(0)}(N)`
+    """
+    gamma = -s1 + 2 / N / (N + 1)
+    result = constants.CA * (-4.0 * gamma - 11.0 / 3.0) + 4.0 / 3.0 * constants.TR * nf
+    return result
+
+
+@nb.njit(cache=True)
+def gamma_psinglet(N, s1, nf):
+    r"""
+      Computes the leading-order polarised singlet anomalous dimension matrix
+
+      .. math::
+          \gamma_S^{(0)} = \left(\begin{array}{cc}
+            \gamma_{qq}^{(0)} & \gamma_{qg}^{(0)}\\
+            \gamma_{gq}^{(0)} & \gamma_{gg}^{(0)}
+          \end{array}\right)
+
+      Parameters
+      ----------
+        N : complex
+          Mellin moment
+        s1 : complex
+          harmonic sum :math:`S_{1}`
+        nf : int
+          Number of active flavors
+
+      Returns
+      -------
+        gamma_S_0 : numpy.ndarray
+          Leading-order singlet anomalous dimension matrix :math:`\gamma_{S}^{(0)}(N)`
+
+      See Also
+      --------
+        gamma_ns : :math:`\gamma_{qq}^{(0)}`
+        gamma_qg : :math:`\gamma_{qg}^{(0)}`
+        gamma_gq : :math:`\gamma_{gq}^{(0)}`
+        gamma_gg : :math:`\gamma_{gg}^{(0)}`
+    """
+    gamma_pqq = gamma_ns(N, s1)
+    gamma_pS_0 = np.array(
+        [[gamma_pqq, gamma_pqg(N, nf)], [gamma_pgq(N), gamma_pgg(N, s1, nf)]],
+        np.complex_,
+    )
+    return gamma_pS_0
