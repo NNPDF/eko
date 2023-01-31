6cf3ca193ce85225018085c34e86dff099a7e5c9
giacomomagni <giac.magni@gmail.com>
2023-01-31 09:57:15 +0100

    refactor NLO polarized ad and more tests

--- src/ekore/anomalous_dimensions/polarized/space_like/as2.py
+++ src/ekore/anomalous_dimensions/polarized/space_like/as2.py
@@ -1,13 +1,12 @@
-# -- coding: utf-8 --
-"""This file contains the next-leading-order Altarelli-Parisi splitting kernels."""
+"""The next-leading-order Altarelli-Parisi splitting kernels."""
 
 import numba as nb
 import numpy as np
 
-from eko import constants
+from eko.constants import CA, CF, TR
 
 from .... import harmonics
-from ....harmonics.constants import zeta3
+from ....harmonics.constants import zeta2, zeta3
 
 # Non Singlet sector is swapped
 from ...unpolarized.space_like.as3 import gamma_nsm as gamma_nsp
@@ -16,7 +15,7 @@ from ...unpolarized.space_like.as3 import gamma_nsp as gamma_nsm
 
 @nb.njit(cache=True)
 def gamma_ps(n, nf):
-    """Compute the |NLO| polarized pure-singlet quark-quark anomalous dimension :cite:`Gluck:1995yr` (eq A.3).
+    r"""Compute the |NLO| polarized pure-singlet quark-quark anomalous dimension :cite:`Gluck:1995yr` (eq A.3).
 
     Parameters
     ----------
@@ -31,16 +30,14 @@ def gamma_ps(n, nf):
         |NLO| pure-singlet quark-quark anomalous dimension :math:`\\gamma_{ps}^{(1)}(n)`
 
     """
-    gqqps1_nfcf = (2 * (n + 2) * (1 + 2 * n + np.power(n, 3))) / (
-        np.power(1 + n, 3) * np.power(n, 3)
-    )
-    result = 4.0 * constants.TR * nf * constants.CF * gqqps1_nfcf
+    gqqps1_nfcf = (2 * (n + 2) * (1 + 2 * n + n**3)) / ((1 + n) ** 3 * n**3)
+    result = 4.0 * TR * nf * CF * gqqps1_nfcf
     return result
 
 
 @nb.njit(cache=True)
 def gamma_qg(n, nf, sx):
-    """Compute the |NLO| polarized quark-gluon singlet anomalous dimension :cite:`Gluck:1995yr` (eq A.4).
+    r"""Compute the |NLO| polarized quark-gluon singlet anomalous dimension :cite:`Gluck:1995yr` (eq A.4).
 
     Parameters
     ----------
@@ -61,34 +58,25 @@ def gamma_qg(n, nf, sx):
     S2 = sx[1]
     Sp2m = harmonics.S2((n - 1) / 2)
     gqg1_nfca = (
-        (np.power(S1, 2) - S2 + Sp2m) * (n - 1) / (n * (n + 1))
-        - 4 * S1 / (n * np.power(1 + n, 2))
-        - (
-            -2
-            - 7 * n
-            + 3 * np.power(n, 2)
-            - 4 * np.power(n, 3)
-            + np.power(n, 4)
-            + np.power(n, 5)
-        )
-        / (np.power(n, 3) * np.power(1 + n, 3))
-    ) * (2.0)
+        (S1**2 - S2 + Sp2m) * (n - 1) / (n * (n + 1))
+        - 4 * S1 / (n * (1 + n) ** 2)
+        - (-2 - 7 * n + 3 * n**2 - 4 * n**3 + n**4 + n**5)
+        / (n**3 * (1 + n) ** 3)
+    ) * 2.0
     gqg1_nfcf = (
-        (-np.power(S1, 2) + S2 + 2 * S1 / n) * (n - 1) / (n * (n + 1))
+        (-(S1**2) + S2 + 2 * S1 / n) * (n - 1) / (n * (n + 1))
         - (n - 1)
-        * (1 + 3.5 * n + 4 * np.power(n, 2) + 5 * np.power(n, 3) + 2.5 * np.power(n, 4))
-        / (np.power(n, 3) * np.power(1 + n, 3))
-        + 4 * (n - 1) / (np.power(n, 2) * np.power(1 + n, 2)) * 2
-    )
-    result = (
-        4.0 * constants.TR * nf * (constants.CA * gqg1_nfca + constants.CF * gqg1_nfcf)
-    )
+        * (1 + 3.5 * n + 4 * n**2 + 5 * n**3 + 2.5 * n**4)
+        / (n**3 * (1 + n) ** 3)
+        + 4 * (n - 1) / (n**2 * (1 + n) ** 2)
+    ) * 2
+    result = 4.0 * TR * nf * (CA * gqg1_nfca + CF * gqg1_nfcf)
     return result
 
 
 @nb.njit(cache=True)
 def gamma_gq(n, nf, sx):
-    """Compute the |NLO| polarized gluon-quark singlet anomalous dimension :cite:`Gluck:1995yr` (eq A.5).
+    r"""Compute the |NLO| polarized gluon-quark singlet anomalous dimension :cite:`Gluck:1995yr` (eq A.5).
 
     Parameters
     ----------
@@ -109,49 +97,28 @@ def gamma_gq(n, nf, sx):
     S2 = sx[1]
     Sp2m = harmonics.S2((n - 1) / 2)
     ggq1_cfcf = (
-        ((np.power(S1, 2) + S2) * (n + 2)) / (n * (n + 1))
-        - (2 * S1 * (n + 2) * (1 + 3 * n)) / (n * np.power(1 + n, 2))
-        - (
-            (n + 2)
-            * (
-                2
-                + 15 * n
-                + 8 * np.power(n, 2)
-                - 12.0 * np.power(n, 3)
-                - 9.0 * np.power(n, 4)
-            )
-        )
-        / (np.power(n, 3) * np.power(1 + n, 3))
-        + 8 * (n + 2) / (np.power(n, 2) * np.power(1 + n, 2))
+        (2 * (S1**2 + S2) * (n + 2)) / (n * (n + 1))
+        - (2 * S1 * (n + 2) * (1 + 3 * n)) / (n * (1 + n) ** 2)
+        - ((n + 2) * (2 + 15 * n + 8 * n**2 - 12.0 * n**3 - 9.0 * n**4))
+        / (n**3 * (1 + n) ** 3)
+        + 8 * (n + 2) / (n**2 * (1 + n) ** 2)
+    ) * 0.5
+    ggq1_cfca = -(
+        -(-(S1**2) - S2 + Sp2m) * (n + 2) / (n * (n + 1))
+        - S1 * (12 + 22 * n + 11 * n**2) / (3 * n**2 * (n + 1))
+        + (36 + 72 * n + 41 * n**2 + 254 * n**3 + 271 * n**4 + 76 * n**5)
+        / (9 * n**3 * (1 + n) ** 3)
     )
-    ggq1_cfca = (
-        (-np.power(S1, 2) - S2 + Sp2m) * (n + 2) / (n * (n + 1))
-        + S1 * (12 + 22 * n + 11 * np.power(n, 2)) / (3 * np.power(n, 2) * (n + 1))
-        - (
-            36
-            + 72 * n
-            + 41 * np.power(n, 2)
-            + 254 * np.power(n, 3)
-            + 271 * np.power(n, 4)
-            + 76 * np.power(n, 5)
-        )
-        / (9 * np.power(n, 3) * np.power(1 + n, 3))
+    ggq1_cfnf = (-S1 * (n + 2)) / (3 * n * (n + 1)) + ((n + 2) * (2 + 5 * n)) / (
+        9 * n * (1 + n) ** 2
     )
-    ggq1_cfnf = 4 * (
-        (-S1 * (n + 2)) / (3 * n * (n + 1))
-        + ((n + 2) * (2 + 5 * n)) / (9 * n * np.power(1 + n, 2))
-    )
-    result = constants.CF * (
-        (constants.CA * ggq1_cfca)
-        + (constants.CF * ggq1_cfcf)
-        + (4.0 * constants.TR * nf * ggq1_cfnf)
-    )
+    result = 4 * CF * (CA * ggq1_cfca + CF * ggq1_cfcf + 4.0 * TR * nf * ggq1_cfnf)
     return result
 
 
 @nb.njit(cache=True)
 def gamma_gg(n, nf, sx):
-    """Compute the |NLO| polarized gluon-gluon singlet anomalous dimension :cite:`Gluck:1995yr` (eq A.6).
+    r"""Compute the |NLO| polarized gluon-gluon singlet anomalous dimension :cite:`Gluck:1995yr` (eq A.6).
 
     Parameters
     ----------
@@ -169,11 +136,16 @@ def gamma_gg(n, nf, sx):
 
     """
     S1 = sx[0]
+    Sp1m = harmonics.S1((n - 1) / 2)
     Sp2m = harmonics.S2((n - 1) / 2)
     Sp3m = harmonics.S3((n - 1) / 2)
-    Sm1 = harmonics.Sm1(n, S1, True)
-    Sm21 = harmonics.Sm21(n, S1, Sm1, True)
-    SSCHLM = -S1 / n**2 - 5 * zeta3 / 4 - Sm21
+    S1h = harmonics.S1(n / 2)
+    SSCHLM = (
+        zeta2 / 2 * (+Sp1m - S1h + 2 / n)
+        - S1 / n**2
+        - harmonics.g_functions.mellin_g3(n, S1)
+        - 5 * zeta3 / 8
+    )
     ggg1_caca = (
         -4 * S1 * Sp2m
         - Sp3m
@@ -181,45 +153,28 @@ def gamma_gg(n, nf, sx):
         + 8 * Sp2m / (n * (n + 1))
         + 2.0
         * S1
-        * (
-            72
-            + 144 * n
-            + 67 * np.power(n, 2)
-            + 134 * np.power(n, 3)
-            + 67 * np.power(n, 4)
-        )
-        / (9 * np.power(n, 2) * np.power(n + 1, 2))
+        * (72 + 144 * n + 67 * n**2 + 134 * n**3 + 67 * n**4)
+        / (9 * n**2 * (n + 1) ** 2)
         - (
             144
             + 258 * n
-            + 7 * np.power(n, 2)
-            + 698 * np.power(n, 3)
-            + 469 * np.power(n, 4)
-            + 144 * np.power(n, 5)
-            + 48 * np.power(n, 6)
+            + 7 * n**2
+            + 698 * n**3
+            + 469 * n**4
+            + 144 * n**5
+            + 48 * n**6
         )
-        / (9 * np.power(n, 3) * np.power(1 + n, 3))
-    ) * (0.5)
+        / (9 * n**3 * (1 + n) ** 3)
+    ) * 0.5
     ggg1_canf = (
         -5 * S1 / 9
-        + (-3 + 13 * n + 16 * np.power(n, 2) + 6 * np.power(n, 3) + 3 * np.power(n, 4))
-        / (9 * np.power(n, 2) * np.power(1 + n, 2))
+        + (-3 + 13 * n + 16 * n**2 + 6 * n**3 + 3 * n**4)
+        / (9 * n**2 * (1 + n) ** 2)
     ) * 4
-    ggg1_cfnf = (
-        4
-        + 2 * n
-        - 8 * np.power(n, 2)
-        + np.power(n, 3)
-        + 5 * np.power(n, 4)
-        + 3 * np.power(n, 5)
-        + np.power(n, 6)
-    ) / (np.power(n, 3) * np.power(1 + n, 3))
-    # fmt: on
-    result = 4 * (
-        constants.CA * constants.CA * ggg1_caca
-        + constants.TR * nf * (constants.CA * ggg1_canf + constants.CF * ggg1_cfnf)
+    ggg1_cfnf = (4 + 2 * n - 8 * n**2 + n**3 + 5 * n**4 + 3 * n**5 + n**6) / (
+        n**3 * (1 + n) ** 3
     )
-
+    result = 4 * (CA**2 * ggg1_caca + TR * nf * (CA * ggg1_canf + CF * ggg1_cfnf))
     return result
 
 
--- tests/ekore/anomalous_dimensions/polarized/space_like/test_ad_as2.py
+++ tests/ekore/anomalous_dimensions/polarized/space_like/test_ad_as2.py
@@ -1,7 +1,8 @@
-# Test LO Polarized splitting functions
+# Test NLO Polarized splitting functions
 import numpy as np
 
 import ekore.anomalous_dimensions.polarized.space_like.as2 as as2
+from eko.constants import CA, CF, TR
 from ekore import harmonics
 
 nf = 5
@@ -11,3 +12,44 @@ def test_qg_helicity_conservation():
     N = complex(1.0, 0.0)
     sx = harmonics.sx(N, max_weight=4)
     np.testing.assert_almost_equal(as2.gamma_qg(N, nf, sx), 0)
+
+
+def test_ps_momentum():
+    N = complex(2.0, 0.0)
+    np.testing.assert_allclose(-as2.gamma_ps(N, nf), -4.0 * TR * nf * CF * 13 / 27)
+
+
+def test_qg_momentum():
+    N = complex(2.0, 0.0)
+    sx = harmonics.sx(N, 2)
+    np.testing.assert_allclose(
+        -as2.gamma_qg(N, nf, sx),
+        4
+        * nf
+        * (0.574074 * CF - 2 * CA * (-7 / 18 + 1 / 6 * (5 - np.pi**2 / 3)))
+        * TR,
+    )
+
+
+def test_gq_momentum():
+    N = complex(2.0, 0.0)
+    sx = harmonics.sx(N, 2)
+    np.testing.assert_allclose(
+        -as2.gamma_gq(N, nf, sx),
+        4
+        * (
+            -2.074074074074074 * CF**2
+            + CA * CF * (29 / 54 - 2 / 3 * (1 / 2 - np.pi**2 / 3))
+            + (4 * CF * nf * TR) / 27
+        ),
+    )
+
+
+def test_gg_momentum():
+    N = complex(2.0, 0.0)
+    sx = harmonics.sx(N, 2)
+    np.testing.assert_almost_equal(
+        -as2.gamma_gg(N, nf, sx),
+        4
+        * (-1.7537256813471833 * CA**2 + ((29 * CA) / 27 - (28 * CF) / 27) * nf * TR),
+    )
