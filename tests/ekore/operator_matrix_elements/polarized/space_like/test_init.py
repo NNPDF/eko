bad407b95696956aa151f6ff7d32b3f336914569
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2023-02-02 15:21:06 +0100

    Add empty ekore tests

---
+++ tests/ekore/operator_matrix_elements/polarized/space_like/test_init.py
@@ -0,0 +1,10 @@
+import pytest
+
+import ekore.operator_matrix_elements.polarized.space_like as ome_ps
+
+
+def test_init():
+    with pytest.raises(NotImplementedError):
+        ome_ps.A_non_singlet((1, 0), 1.0, [], 4, 0.0)
+    with pytest.raises(NotImplementedError):
+        ome_ps.A_singlet((1, 0), 1.0, [], 4, 0.0, False)
