d020b3c6402a6cbde079ae1abd087b6e6ef43f11
giacomomagni <giac.magni@gmail.com>
2023-02-08 15:18:48 +0100

    remove MSHT source file

--- extras/n3lo_bench/MSHT/f2py.sh
+++ extras/n3lo_bench/MSHT/f2py.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 MODE=2
-FILES='split.F'
+FILES='split.F trans_mat_elems.F'
 
-# move back to root
-# cd ..
+if [ ! -d 'N3LO_additions' ]; then
+    git clone https://github.com/MSHTPDF/N3LO_additions
+fi
+
+cd 'N3LO_additions'
 
 if [ "$MODE" -eq "1" ];
 then
-  f2py3 --overwrite-signature -m msht_n3lo -h msth_n3lo.template1.pyf $FILES \
-  only: pgg3a pgq3a  pqg3a  pqqps3a p3nsa p3nsb p3nsc
+  f2py3 --overwrite-signature -m msht_n3lo -h ../msth_n3lo.template.pyf $FILES \
+  only: pgg3a pgq3a pqg3a pqqps3a p3nsa p3nsb p3nsc ahg3a ahq3a aqqh3ab agqh3a aggh3a
 fi;
 
 if [ "$MODE" -eq "2" ];
  then
-  f2py3 --f77flags="-ffixed-line-length-none -ffree-line-length-none" -m msht_n3lo -c msth_n3lo.template.pyf  $FILES \
-  -L/Users/giacomomagni/.conda/envs/eko_dev/lib --fcompiler=gnu95 
+  f2py3 --f77flags="-ffixed-line-length-none -ffree-line-length-none" -m msht_n3lo -c ../msth_n3lo.template.pyf  $FILES \
+  --fcompiler=gnu95
 fi;
-
