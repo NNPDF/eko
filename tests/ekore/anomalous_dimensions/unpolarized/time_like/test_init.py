feb2dce7b69fd01c3bf64d001fe2aab41ef7cdec
t7phy <t7phy.in@gmail.com>
2023-02-09 15:22:49 +0100

    removal of tests with NotImplementedError

--- tests/ekore/anomalous_dimensions/unpolarized/time_like/test_init.py
+++ tests/ekore/anomalous_dimensions/unpolarized/time_like/test_init.py
@@ -2,9 +2,10 @@ import pytest
 
 import ekore.anomalous_dimensions.unpolarized.time_like as ad_ut
 
+# tests to be added later
 
-def test_init():
-    with pytest.raises(NotImplementedError):
-        ad_ut.gamma_ns((1, 0), 0, 1.0, 4)
-    with pytest.raises(NotImplementedError):
-        ad_ut.gamma_singlet((1, 0), 1.0, 4)
+# def test_init():
+#    with pytest.raises(NotImplementedError):
+#        ad_ut.gamma_ns((1, 0), 0, 1.0, 4)
+#    with pytest.raises(NotImplementedError):
+#        ad_ut.gamma_singlet((1, 0), 1.0, 4)
