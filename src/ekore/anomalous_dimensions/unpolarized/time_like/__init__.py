561648eab08d934aa2525c7e4934d1a3f359de11
t7phy <t7phy.in@gmail.com>
2023-02-09 02:40:02 +0100

    pylint test 2

--- src/ekore/anomalous_dimensions/unpolarized/time_like/__init__.py
+++ src/ekore/anomalous_dimensions/unpolarized/time_like/__init__.py
@@ -1,9 +1,11 @@
 import numba as nb
 
-@nb.njit(cache=True)
-def gamma_ns():
-    return 0
 
 @nb.njit(cache=True)
-def gamma_singlet():
+def gamma_ns(order, mode0, n, nf):
+    return 0
+
+
+@nb.njit(cache=True)
+def gamma_singlet(order, n, nf):
     return 0
