13197aa354f19779e5783b35437b8f7f780714ae
Alessandro Candido <candido.ale@gmail.com>
2023-02-10 15:04:28 +0100

    Replace wrong matching fragment

--- tests/eko/io/test_exceptions.py
+++ tests/eko/io/test_exceptions.py
@@ -7,5 +7,5 @@ from eko.io import exceptions
 
 def test_oplocerr():
     p = pathlib.Path.home() / "path" / "to" / "missing" / "eko.tar"
-    with pytest.raises(exceptions.OperatorLocationError, match="not-found"):
+    with pytest.raises(exceptions.OperatorLocationError, match="missing"):
         raise exceptions.OperatorLocationError(p)
