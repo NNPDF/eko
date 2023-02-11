0c4dd7b694b0c1446d1499e0ffa85e24c2e73ce1
Alessandro Candido <candido.ale@gmail.com>
2023-02-11 04:10:03 +0100

    Test 5-dim operator conversion

---
+++ tests/eko/io/test_legacy.py
@@ -0,0 +1,16 @@
+import pytest
+
+from eko.io import legacy
+
+
+def test_op5to4():
+    with pytest.raises(RuntimeError, match="not found"):
+        legacy.op5to4([], {})
+
+    mu2 = 1.959
+    op = 29348.2342
+    err = 54225.24
+    op4s = legacy.op5to4([mu2], {legacy.OPERATOR: [op], legacy.ERROR: [err]})
+    assert mu2 in op4s
+    assert op4s[mu2].operator == op
+    assert op4s[mu2].error == err
