26f61c595c9c0cd104a3d8f26245cb386de4b42c
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2023-02-15 18:41:48 +0100

    Fix LHA SV

--- benchmarks/lha_paper_bench.py
+++ benchmarks/lha_paper_bench.py
@@ -77,10 +77,12 @@ class LHABenchmark(Runner):
         low = self.theory.copy()
         low["PTO"] = pto
         low["fact_to_ren_scale_ratio"] = np.sqrt(1.0 / 2.0)
+        low["XIF"] = np.sqrt(1.0 / 2.0)
         low["ModSV"] = "exponentiated"
         high = self.theory.copy()
         high["PTO"] = pto
         high["fact_to_ren_scale_ratio"] = np.sqrt(2.0)
+        high["XIF"] = np.sqrt(2.0)
         high["ModSV"] = "exponentiated"
         return [high, low]
 
@@ -243,12 +245,11 @@ class BenchmarkFFNS_polarized(BenchmarkFFNS):
 
 
 if __name__ == "__main__":
-
     # Benchmark to LHA
     obj = BenchmarkFFNS_polarized()
     # obj = BenchmarkFFNS()
-    obj.benchmark_plain(1)
-    # obj.benchmark_sv(2)
+    # obj.benchmark_plain(1)
+    obj.benchmark_sv(1)
 
     # # VFNS benchmarks with LHA settings
     # programs = ["LHA", "pegasus", "apfel"]
