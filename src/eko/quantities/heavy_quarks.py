183982b36e13a6e421503248f57cc6fa8ad069ce
Alessandro Candido <candido.ale@gmail.com>
2023-02-24 10:18:04 +0100

    Add scales from ratios function

--- src/eko/quantities/heavy_quarks.py
+++ src/eko/quantities/heavy_quarks.py
@@ -5,6 +5,8 @@ import enum
 from ..io.dictlike import DictLike
 from ..io.types import LinearScale, reference_running
 
+FLAVORS = "cbt"
+
 
 def _heavy_quark(quarkattr):
     """Generate heavy quark properties container classes.
@@ -30,7 +32,7 @@ def _heavy_quark(quarkattr):
             Consequently it allows iteration and containing check.
 
             """
-            return getattr(self, "cbt"[key])
+            return getattr(self, FLAVORS[key])
 
     return HeavyQuarks
 
@@ -40,6 +42,21 @@ QuarkMassRef = reference_running(QuarkMass)
 HeavyQuarkMasses = _heavy_quark(QuarkMassRef)
 MatchingScale = LinearScale
 MatchingScales = _heavy_quark(MatchingScale)
+MatchingRatio = float
+MatchingRatios = _heavy_quark(MatchingRatio)
+
+
+def scales_from_ratios(
+    ratios: MatchingRatios, masses: HeavyQuarkMasses
+) -> MatchingScales:
+    """Convert ratios to linear scales.
+
+    .. todo::
+
+        make this a method
+
+    """
+    return masses
 
 
 # TODO: upgrade all the following to StrEnum (requires py>=3.11)
