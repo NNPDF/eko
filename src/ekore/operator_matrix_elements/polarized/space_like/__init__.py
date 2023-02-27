bd7c280094642abe2c687edb6b76f53b06546fb1
aschaus <adrianneschauss@gmail.com>
2023-02-27 16:05:58 +0100

    Check required on format option of OME eqs.

--- src/ekore/operator_matrix_elements/polarized/space_like/__init__.py
+++ src/ekore/operator_matrix_elements/polarized/space_like/__init__.py
@@ -1,13 +1,55 @@
 r"""The polarized, space-like |OME|."""
 
 import numba as nb
+import numpy as np
+
+from . import as1
 
 
 @nb.njit(cache=True)
-def A_non_singlet(_matching_order, _n, _sx, _nf, _L):
-    raise NotImplementedError("Polarised, space-like is not yet implemented")
+def A_singlet(matching_order, n, sx, nf, L, is_msbar, sx_ns=None):
+    r"""
+    Computes the tower of the singlet |OME|.
+
+    Parameters
+    ----------
+        matching_order : tuple(int,int)
+            perturbative matching_order
+        n : complex
+            Mellin variable
+        sx : list
+            singlet like harmonic sums cache
+        nf: int
+            number of active flavor below threshold
+        L : float
+            :math:``\ln(\mu_F^2 / m_h^2)``
+        is_msbar: bool
+            add the |MSbar| contribution
+        sx_ns : list
+            non-singlet like harmonic sums cache
+
+    Returns
+    -------
+        A_singlet : numpy.ndarray
+            singlet |OME|
+
+    See Also
+    --------
+        ekore.matching_conditions.nlo.A_singlet_1 : :math:`A^{S,(1)}(N)`
+        ekore.matching_conditions.nlo.A_hh_1 : :math:`A_{HH}^{(1)}(N)`
+        ekore.matching_conditions.nlo.A_gh_1 : :math:`A_{gH}^{(1)}(N)`
+        ekore.matching_conditions.nnlo.A_singlet_2 : :math:`A_{S,(2)}(N)`
+    """
+    A_s = np.zeros((matching_order[0], 3, 3), np.complex_)
+    if matching_order[0] == 1:
+        A_s[0] = as1.A_singlet(n, L)
+    else:
+        raise NotImplementedError(
+            "Polarised, space-like is not yet implemented at this order"
+        )
+    return A_s
 
 
 @nb.njit(cache=True)
-def A_singlet(_matching_order, _n, _sx, _nf, _L, _is_msbar, _sx_ns=None):
+def A_non_singlet(_matching_order, _n, _sx, _nf, _L, _is_msbar, _sx_ns=None):
     raise NotImplementedError("Polarised, space-like is not yet implemented")
---
+++ src/ekore/operator_matrix_elements/polarized/space_like/as1.py
@@ -0,0 +1,131 @@
+r"""
+This module contains the |NLO| |OME| (OMEs) in the polarized case
+for the matching conditions in the |VFNS|.
+Heavy quark contribution for intrinsic evolution are not considered for the polarized case at this order.
+The matching conditions for the |VFNS| at :math:`\mu_F^2 \neq m_H^2`
+are provided in :cite:`Bierenbaum_2023`.
+"""
+import numba as nb
+import numpy as np
+
+from eko.constants import TR
+
+
+@nb.njit(cache=True)
+def A_hg(n, L):  # method 1 -> Following the un-polarized format in eko
+    r"""
+     |NLO| heavy-gluon |OME| :math:`A_{Hg}^{S,(1)}` as defined in Eq. 104 of :cite:`Bierenbaum_2023`.
+
+    Parameters
+    ----------
+        n : complex
+            Mellin moment
+        L : float
+            :math:`\ln(\mu_F^2 / m_h^2)`
+
+    Returns
+    -------
+        A_hg : complex
+            |NLO| heavy-gluon |OME| :math:`A_{Hg}^{S,(1)}`
+    """
+    den = 1 / (n * (1 + n))
+    num = 4 * (n - 1)
+    return num * den * L * TR
+
+
+@nb.njit(cache=True)
+def A_hg(
+    n, L
+):  # method 2 -> following the Blumlein format without the - (method 3-> direct insertion of splitting funct from eko)
+    r"""
+    |NLO| heavy-gluon |OME| :math:`A_{Hg}^{S,(1)}` as defined in Eq. 104 of :cite:`Bierenbaum_2023`.
+    Parameters
+    ----------
+        n : complex
+            Mellin moment
+        L : float
+            :math:`\ln(\mu_F^2 / m_h^2)`
+
+    Returns
+    -------
+        A_hg : complex
+            |NLO| heavy-gluon |OME| :math:`A_{Hg}^{S,(1)}`
+    """
+    DPqg0hat = (8 * TR * (n - 1)) / (n * (1 + n))
+    return 1 / 2 * DPqg0hat * L
+
+
+@nb.njit(cache=True)
+def A_gg(L):  # method 1
+    r"""
+    |NLO| gluon-gluon |OME| :math:`A_{gg,H}^{S,(1)}` as defined in Eq. 186 of :cite:`Bierenbaum_2023`.
+
+    Parameters
+    ----------
+        L : float
+            :math:`\ln(\mu_F^2 / m_h^2)`
+
+    Returns
+    -------
+        A_gg : complex
+            |NLO| gluon-gluon |OME| :math:`A_{gg,H}^{S,(1)}`
+    """
+    return -4.0 / 3.0 * L * TR
+
+
+@nb.njit(cache=True)
+def A_gg(L):  # method 2
+    r"""
+    |NLO| gluon-gluon |OME| :math:`A_{gg,H}^{S,(1)}` as defined in Eq. 186 of :cite:`Bierenbaum_2023`.
+
+    Parameters
+    ----------
+        L : float
+            :math:`\ln(\mu_F^2 / m_h^2)`
+
+    Returns
+    -------
+        A_gg : complex
+            |NLO| gluon-gluon |OME| :math:`A_{gg,H}^{S,(1)}`
+    """
+    beta_0 = -4.0 / 3.0 * TR
+    return beta_0 * L
+
+
+@nb.njit(cache=True)
+def A_singlet(n, L):
+    r"""
+    Computes the |NLO| singlet |OME|.
+    .. math::
+        A^{S,(1)} = \left(\begin{array}{cc}
+        A_{gg,H}^{S,(1)} & 0 & 0\\
+        0 & 0 & 0 \\
+        A_{hg}^{S,(1)} & 0 & 0
+        \end{array}\right)
+
+    Parameters
+    ----------
+    n : complex
+        Mellin moment
+     L : float
+        :math:`\ln(\mu_F^2 / m_h^2)`
+
+    Returns
+      -------
+    A_S : numpy.ndarray
+        |NLO| singlet |OME| :math:`A^{S,(1)}`
+
+      See Also
+      --------
+        A_gg : :math:`A_{gg,H}^{S,(1)}`
+        A_hg : :math:`A_{hg}^{S,(1)}`
+    """
+    A_S = np.array(
+        [
+            [A_gg(L), 0.0, 0.0],
+            [0 + 0j, 0 + 0j, 0 + 0j],
+            [A_hg(n, L), 0.0, 0.0],
+        ],
+        np.complex_,
+    )
+    return A_S
