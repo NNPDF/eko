d129b56e0a65b465d7fb07925899ec76c54d486b
t7phy <t7phy.in@gmail.com>
2023-03-15 22:59:38 +0100

    test_init NA

--- tests/ekore/anomalous_dimensions/unpolarized/time_like/test_init.py
+++ tests/ekore/anomalous_dimensions/unpolarized/time_like/test_init.py
@@ -2,9 +2,8 @@ import pytest
 
 import ekore.anomalous_dimensions.unpolarized.time_like as ad_ut
 
-
-def test_init():
-    with pytest.raises(NotImplementedError):
-        ad_ut.gamma_ns((1, 0), 0, 1.0, 4)
-    with pytest.raises(NotImplementedError):
-        ad_ut.gamma_singlet((1, 0), 1.0, 4)
+# def test_init():
+#     with pytest.raises(NotImplementedError):
+#         ad_ut.gamma_ns((1, 0), 0, 1.0, 4)
+#     with pytest.raises(NotImplementedError):
+#         ad_ut.gamma_singlet((1, 0), 1.0, 4)
