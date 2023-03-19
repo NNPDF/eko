6d6d9e60efd917ccdcf851ec46d89c67f233a54e
Alessandro Candido <candido.ale@gmail.com>
2023-03-19 17:33:20 +0100

    Introduce types in thresholds

--- src/eko/thresholds.py
+++ src/eko/thresholds.py
@@ -1,11 +1,14 @@
 r"""Holds the classes that define the |FNS|."""
 import logging
+from dataclasses import astuple, dataclass
+from typing import List, Optional
 
 import numpy as np
 
 logger = logging.getLogger(__name__)
 
 
+@dataclass
 class PathSegment:
     """Oriented path in the threshold landscape.
 
@@ -28,23 +31,22 @@ class PathSegment:
         number of active flavors
     """
 
-    def __init__(self, q2_from, q2_to, nf):
-        self.q2_from = q2_from
-        self.q2_to = q2_to
-        self.nf = nf
+    q2_from: float
+    q2_to: float
+    nf: int
 
     @property
-    def is_downward_q2(self):
+    def is_downward_q2(self) -> bool:
         """Return True if ``q2_from`` is bigger than ``q2_to``."""
         return self.q2_from > self.q2_to
 
     @property
     def tuple(self):
-        """Return tuple representation suitable for hashing."""
-        return (self.q2_from, self.q2_to, self.nf)
+        """Deprecated: use directly `dataclasses.astuple`."""
+        return astuple(self)
 
-    def __repr__(self):
-        """Return string representation."""
+    def __str__(self):
+        """Textual representation, mainly for logging purpose."""
         return f"PathSegment({self.q2_from} -> {self.q2_to}, nf={self.nf})"
 
 
@@ -58,33 +60,33 @@ class ThresholdsAtlas:
 
     def __init__(
         self,
-        masses,
-        q2_ref=None,
-        nf_ref=None,
-        thresholds_ratios=None,
-        max_nf=None,
+        masses: List[float],
+        q2_ref: Optional[float] = None,
+        nf_ref: Optional[int] = None,
+        thresholds_ratios: Optional[List[float]] = None,
+        max_nf: Optional[int] = None,
     ):
         """Create basic atlas.
 
         Parameters
         ----------
-        masses : list(float)
+        masses :
             list of quark masses squared
-        q2_ref : float
+        q2_ref :
             reference scale
-        nf_ref : int
+        nf_ref :
             number of active flavors at the reference scale
-        thresholds_ratios : list(float)
+        thresholds_ratios :
             list of ratios between matching scales and masses squared
-        max_nf : int
-            maximum number of active flavors
+        max_nf :
+            maximum number of active flavors, if `None` no maximum is set
+
         """
-        masses = list(masses)
-        if not np.allclose(masses, sorted(masses)):
+        sorted_masses = sorted(masses)
+        if not np.allclose(masses, sorted_masses):
             raise ValueError("masses need to be sorted")
-        masses = sorted(masses)
         # combine them
-        thresholds = self.build_area_walls(masses, thresholds_ratios, max_nf)
+        thresholds = self.build_area_walls(sorted_masses, thresholds_ratios, max_nf)
         self.area_walls = [0] + thresholds + [np.inf]
 
         # check nf_ref
@@ -114,13 +116,13 @@ class ThresholdsAtlas:
         self.thresholds_ratios = thresholds_ratios
         logger.info(str(self))
 
-    def __repr__(self):
-        """Return string representation."""
+    def __str__(self):
+        """Textual representation, mainly for logging purpose."""
         walls = " - ".join([f"{w:.2e}" for w in self.area_walls])
         return f"ThresholdsAtlas [{walls}], ref={self.q2_ref} @ {self.nf_ref}"
 
     @classmethod
-    def ffns(cls, nf, q2_ref=None):
+    def ffns(cls, nf: int, q2_ref: Optional[float] = None):
         """Create a |FFNS| setup.
 
         The function creates simply sufficient thresholds at ``0`` (in the
@@ -129,37 +131,43 @@ class ThresholdsAtlas:
 
         Parameters
         ----------
-        nf : int
+        nf :
             number of light flavors
-        q2_ref : float
+        q2_ref :
             reference scale
+
         """
         return cls([0] * (nf - 3) + [np.inf] * (6 - nf), q2_ref)
 
     @staticmethod
-    def build_area_walls(masses, thresholds_ratios=None, max_nf=None):
+    def build_area_walls(
+        masses: List[float],
+        thresholds_ratios: Optional[List[float]] = None,
+        max_nf: Optional[int] = None,
+    ):
         r"""Create the object from the informations on the run card.
 
         The thresholds are computed by :math:`(m_q \cdot k_q^{Thr})`.
 
         Parameters
         ----------
-        masses : list(float)
+        masses :
             heavy quark masses squared
-        thresholds_ratios : list(float)
+        thresholds_ratios :
             list of ratios between matching scales and masses squared
-        max_nf : int
+        max_nf :
             maximum number of flavors
 
         Returns
         -------
         list
             threshold list
+
         """
         if len(masses) != 3:
             raise ValueError("There have to be 3 quark masses")
         if thresholds_ratios is None:
-            thresholds_ratios = (1.0, 1.0, 1.0)
+            thresholds_ratios = [1.0, 1.0, 1.0]
         if len(thresholds_ratios) != 3:
             raise ValueError("There have to be 3 quark threshold ratios")
         if max_nf is None:
@@ -172,21 +180,28 @@ class ThresholdsAtlas:
         thresholds = thresholds[: max_nf - 3]
         return thresholds
 
-    def path(self, q2_to, nf_to=None, q2_from=None, nf_from=None):
+    def path(
+        self,
+        q2_to: float,
+        nf_to: Optional[int] = None,
+        q2_from: Optional[float] = None,
+        nf_from: Optional[int] = None,
+    ):
         """Get path from ``q2_from`` to ``q2_to``.
 
         Parameters
         ----------
-        q2_to: float
+        q2_to:
             target value of q2
-        q2_from: float
+        q2_from:
             starting value of q2
 
         Returns
         -------
         list(PathSegment)
             List of :class:`PathSegment` to go through in order to get from ``q2_from``
-            to ``q2_to``.
+            to ``q2_to``
+
         """
         # fallback to init config
         if q2_from is None:
@@ -229,12 +244,13 @@ class ThresholdsAtlas:
         -------
         int
             number of active flavors
+
         """
         ref_idx = np.digitize(q2, self.area_walls)
         return 2 + ref_idx
 
 
-def is_downward_path(path):
+def is_downward_path(path: List[PathSegment]) -> bool:
     """Determine if a path is downward.
 
     Criterias are:
@@ -246,20 +262,21 @@ def is_downward_path(path):
 
     Parameters
     ----------
-    path : list(PathSegment)
+    path :
         path
 
     Returns
     -------
     bool
         True for a downward path
+
     """
     if len(path) == 1:
         return path[0].is_downward_q2
     return path[1].nf < path[0].nf
 
 
-def flavor_shift(is_downward):
+def flavor_shift(is_downward: bool) -> int:
     """Determine the shift to number of light flavors.
 
     Parameters
@@ -271,5 +288,6 @@ def flavor_shift(is_downward):
     -------
     int
         shift to number of light flavors which can be 3 or 4
+
     """
     return 4 if is_downward else 3
