126143c58251596fb37000f4b79aa3bae0cd1f43
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2023-04-05 14:00:53 +0200

    Fix pydocstyle in ekomark

--- src/ekomark/__init__.py
+++ src/ekomark/__init__.py
@@ -1,11 +1,9 @@
-"""
-Additional package to benchmark eko.
-"""
+"""Benchmark package for eko."""
 from eko import basis_rotation as br
 
 
 def pdfname(pid_or_name):
-    """Return pdf name"""
+    """Return pdf name."""
     if isinstance(pid_or_name, int):
         return br.flavor_basis_names[br.flavor_basis_pids.index(pid_or_name)]
     return pid_or_name
---
+++ src/ekomark/data/__init__.py
@@ -0,0 +1,1 @@
+"""EKO database configuration."""
