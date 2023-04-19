b7531f164b02371d344ee893771224ec3a8e8e43
t7phy <t7phy.in@gmail.com>
2023-04-19 23:38:53 +0200

    use as1 matching conditions for nnlo ADs

--- src/ekore/operator_matrix_elements/unpolarized/time_like/__init__.py
+++ src/ekore/operator_matrix_elements/unpolarized/time_like/__init__.py
@@ -25,10 +25,11 @@ def A_non_singlet(matching_order, N, L):
         non-singlet |OME|
 
     """
-    if matching_order[0] > 1:
-        raise Exception("Time-like matching conditions are only known upto NLO")
     A_ns = np.zeros((matching_order[0], 2, 2), np.complex_)
-    A_ns[0] = as1.A_ns()
+    if matching_order[0] >= 1:
+        A_ns[0] = as1.A_ns()
+    if matching_order[0] == 2:
+        A_ns[1] = as1.A_ns()
     return A_ns
 
 
@@ -51,8 +52,9 @@ def A_singlet(matching_order, N, L):
         singlet |OME|
 
     """
-    if matching_order[0] > 1:
-        raise Exception("Time-like matching conditions are only known upto NLO")
     A_singlet = np.zeros((matching_order[0], 3, 3), np.complex_)
-    A_singlet[0] = as1.A_singlet(N, L)
+    if matching_order[0] >= 1:
+        A_singlet[0] = as1.A_singlet(N, L)
+    if matching_order[0] >= 2:
+        A_singlet[0] = as1.A_singlet(N, L)
     return A_singlet
--- tests/ekore/operator_matrix_elements/unpolarized/time_like/test_init.py
+++ tests/ekore/operator_matrix_elements/unpolarized/time_like/test_init.py
@@ -6,3 +6,5 @@ import ekore.operator_matrix_elements.unpolarized.time_like as ome
 def test_shapes():
     assert ome.A_singlet((1, 0), complex(0, 1), 0).shape == (1, 3, 3)
     assert ome.A_non_singlet((1, 0), complex(0, 1), 0).shape == (1, 2, 2)
+    assert ome.A_singlet((2, 0), complex(0, 1), 0).shape == (2, 3, 3)
+    assert ome.A_non_singlet((2, 0), complex(0, 1), 0).shape == (2, 2, 2)
