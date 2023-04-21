af5adefe27c0d6d2d3ee390ea65593ba32f5042d
giacomomagni <giac.magni@gmail.com>
2023-04-21 16:27:47 +0200

    black and isort

--- extras/ad_n3lo/singlet_nf3/translate_singlet_nf3.py
+++ extras/ad_n3lo/singlet_nf3/translate_singlet_nf3.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-
 import pathlib
+
 import eko
 
 eko_path = eko.__path__
@@ -11,7 +10,7 @@ for fp in fps:
         nls = []
         # translate
         for l in oi:
-            l = l.replace("Sigma_Summation_Objects_Private_MyPower","np.power")
+            l = l.replace("Sigma_Summation_Objects_Private_MyPower", "np.power")
             l = l.replace("Power", "np.power")
             l = l.replace("Log", "np.log")
             l = l.replace("ln2", "np.log(2)")
@@ -23,20 +22,22 @@ for fp in fps:
         out[fp.stem] = nls
 
     # output
-    gamma_name = str(fp).split('/')[-1].split('3')[0]
-    if gamma_name =="gqq":
+    gamma_name = str(fp).split("/")[-1].split("3")[0]
+    if gamma_name == "gqq":
         gamma_name = "gqqPS"
-    with open(f"{eko_path}/anomalous_dimensions/as4/{gamma_name}_1.py", "w", encoding="utf-8") as oo:
-        oo.write(
-            "# -*- coding: utf-8 -*-\n"
-        )
+    with open(
+        f"{eko_path}/anomalous_dimensions/as4/{gamma_name}_1.py", "w", encoding="utf-8"
+    ) as oo:
+        oo.write("# -*- coding: utf-8 -*-\n")
         oo.write('"""\n')
-        oo.write(f'This module contains the anomalous dimension :math:`\\gamma_{gamma_name[1:]}^(3)`\n')
+        oo.write(
+            f"This module contains the anomalous dimension :math:`\\gamma_{gamma_name[1:]}^(3)`\n"
+        )
         oo.write('"""\n')
         oo.write("import numba as nb\n")
         oo.write("import numpy as np\n")
         oo.write("\n\n")
-        oo.write('@nb.njit(cache=True)\n')
+        oo.write("@nb.njit(cache=True)\n")
         oo.write(f"def gamma_{gamma_name[1:]}_nf3(n, sx):\n")
         oo.write("\treturn ")
         for l in nls:
--- extras/n3lo_bench/MSHT/setup.py
+++ extras/n3lo_bench/MSHT/setup.py
@@ -1,18 +1,19 @@
-# -*- coding: utf-8 -*-
 # Installation script for python
-from setuptools import setup
 import pathlib
+
+from setuptools import setup
 
 # find the shared object
 so_name = pathlib.Path(__file__).parent.glob("msht_n3lo.*.so")
 so_name = list(so_name)[0]
 
-setup(name='msht_n3lo',
-      author="",
-      version='0.1.0',
-      description='msht_n3lo',
-      packages=[''],
-      package_data = {
-          '' : [str(so_name)],
-      },
+setup(
+    name="msht_n3lo",
+    author="",
+    version="0.1.0",
+    description="msht_n3lo",
+    packages=[""],
+    package_data={
+        "": [str(so_name)],
+    },
 )
