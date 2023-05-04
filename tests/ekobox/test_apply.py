5515530deade47bc200e1ea4475ce50596f2227e
Alessandro Candido <candido.ale@gmail.com>
2023-05-04 11:41:11 +0200

    Fix test apply

--- tests/ekobox/test_apply.py
+++ tests/ekobox/test_apply.py
@@ -11,13 +11,13 @@ class TestApply:
         # fake pdfs
         pdf_grid = apply.apply_pdf(eko, fake_pdf)
         assert len(pdf_grid) == len(eko.evolgrid)
-        pdfs = pdf_grid[ep_out]["pdfs"]
+        pdfs = pdf_grid[ep_out[0]]["pdfs"]
         assert list(pdfs.keys()) == list(eko.bases.targetpids)
         # rotate to target_grid
         target_grid = [0.75]
         pdf_grid = apply.apply_pdf(eko, fake_pdf, target_grid)
         assert len(pdf_grid) == 1
-        pdfs = pdf_grid[ep_out]["pdfs"]
+        pdfs = pdf_grid[ep_out[0]]["pdfs"]
         assert list(pdfs.keys()) == list(eko.bases.targetpids)
 
     def test_apply_flavor(self, eko_factory: EKOFactory, fake_pdf, monkeypatch):
@@ -37,5 +37,5 @@ class TestApply:
         monkeypatch.setattr("eko.basis_rotation.evol_basis", fake_evol_basis)
         pdf_grid = apply.apply_pdf(eko, fake_pdf, rotate_to_evolution_basis=True)
         assert len(pdf_grid) == len(eko.evolgrid)
-        pdfs = pdf_grid[ep_out]["pdfs"]
+        pdfs = pdf_grid[ep_out[0]]["pdfs"]
         assert list(pdfs.keys()) == list(fake_evol_basis)
