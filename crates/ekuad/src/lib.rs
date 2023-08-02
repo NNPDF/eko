86b0b60ae2b600c06ecd565008ace92314dd0887
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2023-08-02 17:10:07 +0200

    Pass array through Rust to Numba

--- crates/ekuad/src/lib.rs
+++ crates/ekuad/src/lib.rs
@@ -7,10 +7,10 @@ pub unsafe extern "C" fn quad_ker(x: f64, extra: *mut c_void) -> f64 {
     let ex = *(extra as *mut Extra);
     ekore::ciao(x * ex.slope, ex.shift);
     let ar = [0.1,0.023];
-    (ex.py)(x * ex.slope, ex.shift, ar.as_ptr(), 2)
+    (ex.py)(x * ex.slope, ex.shift, ar.as_ptr(), 2, ex.areas, ex.areas_len)
 }
 
-type PyT = unsafe extern "C" fn(f64, f64, *const f64, u16)-> f64;
+type PyT = unsafe extern "C" fn(f64, f64, *const f64, u16, *const f64, u16)-> f64;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
@@ -18,9 +18,11 @@ pub struct Extra {
     pub slope: f64,
     pub shift: f64,
     pub py: PyT,
+    pub areas: *const f64,
+    pub areas_len: u16
 }
 
-pub unsafe extern "C" fn my_py(_x: f64, _y: f64, _ar: *const f64, _len: u16) -> f64 {
+pub unsafe extern "C" fn my_py(_x: f64, _y: f64, _ar: *const f64, _len: u16, _areas: *const f64, _areas_len: u16) -> f64 {
     0.
 }
 
@@ -32,5 +34,7 @@ pub unsafe extern "C" fn dummy() -> Extra {
         slope: 0.,
         shift: 0.,
         py: my_py,
+        areas: [].as_ptr(),
+        areas_len: 0
     }
 }
--- crates/integrate.py
+++ crates/integrate.py
@@ -1,5 +1,6 @@
 import ekuad
 import numba as nb
+import numpy as np
 from scipy import LowLevelCallable, integrate
 
 
@@ -9,16 +10,23 @@ from scipy import LowLevelCallable, integrate
         nb.types.double,
         nb.types.CPointer(nb.types.double),
         nb.types.uint16,
+        nb.types.CPointer(nb.types.double),
+        nb.types.uint16,
     )
 )
-def true_py(x, y, ar_raw, len):
+def true_py(x, y, ar_raw, len, areas_raw, areas_len):
     ar = nb.carray(ar_raw, len)
-    return x + 1.23 * y + ar[0] + ar[1]
+    areas = nb.carray(areas_raw, areas_len)
+    return x + 1.23 * y + ar[0] + ar[1] + np.sum(areas)
 
 
+areas = [2e6, 3e7]
 extra = ekuad.lib.dummy()
 extra.slope = 10.0
 extra.shift = 1000.0
+areas_ffi = ekuad.ffi.new("double[2]", areas)
+extra.areas = areas_ffi
+extra.areas_len = 2
 extra.py = ekuad.ffi.cast("void *", true_py.address)
 func = LowLevelCallable(ekuad.lib.quad_ker, ekuad.ffi.addressof(extra))
 
