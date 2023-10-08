9393ea2c125cab80f970e62b9328f74059118458
Niccolò Laurenti <niclaurenti@gmail.com>
2023-10-08 15:43:31 +0200

    Add pto = 3 in some qed tests

--- tests/eko/kernels/test_kernels_QEDsinglet.py
+++ tests/eko/kernels/test_kernels_QEDsinglet.py
@@ -21,7 +21,7 @@ def test_zero(monkeypatch):
     nf = 3
     ev_op_iterations = 2
     ev_op_max_order = (3, 0)
-    for qcd in range(1, 3 + 1):
+    for qcd in range(1, 4 + 1):
         for qed in range(1, 2 + 1):
             order = (qcd, qed)
             gamma_s = (
@@ -75,7 +75,7 @@ def test_zero_true_gamma(monkeypatch):
     nf = 3
     ev_op_iterations = 2
     ev_op_max_order = (3, 0)
-    for qcd in range(1, 3 + 1):
+    for qcd in range(1, 4 + 1):
         for qed in range(1, 2 + 1):
             order = (qcd, qed)
             n = np.random.rand()
