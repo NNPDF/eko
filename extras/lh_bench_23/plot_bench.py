6519169cc984aebf34430c136a438399ede2aaea
giacomomagni <giac.magni@gmail.com>
2023-10-23 14:06:52 +0200

    refactor plotting scripts

--- extras/lh_bench_23/plot_bench.py
+++ extras/lh_bench_23/plot_bench.py
@@ -1,193 +1,51 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 from cfg import here, table_dir, xgrid
-
-plot_dir = here / "plots"
-plot_dir.mkdir(exist_ok=True)
+from utils import (
+    compute_n3lo_avg_err,
+    compute_n3lo_nnlo_diff,
+    lha_labels,
+    load_n3lo_tables,
+    load_nnlo_table,
+    plot_diff_to_nnlo,
+    plot_pdfs,
+)
 
 USE_LINX = True
 REL_DIFF = True
-scheme = "FFNS"
-sv = "central"
+SCHEME = "VFNS"
+SV = "central"
 
-if scheme == "FFNS":
-    pdf_labels = [
-        "u_v",
-        "d_v",
-        r"L^- = \bar{d} - \bar{u}",
-        r"L^+ = 2(\bar{u} + \bar{d})",
-        "s_v",
-        "s^+",
-        "c^+",
-        "g",
-    ]
-elif scheme == "VFNS":
-    pdf_labels = [
-        "u_v",
-        "d_v",
-        r"L^- = \bar{d} - \bar{u}",
-        r"L^+ = 2(\bar{u} + \bar{d})",
-        "s^+",
-        "c^+",
-        "b^+",
-        "g",
-    ]
-XCUT = 4 if USE_LINX else 0
-xscale = "linx" if USE_LINX else "logx"
-xgrid = xgrid[XCUT:]
-plot_name = f"lh_n3lo_bench_{scheme}_{xscale}"
+plot_dir = here / "plots"
+n3lo_table_dir = table_dir  # / SCHEME
 
-fhmv_dfs = []
-eko_dfs = []
-# eko_dfs_4mom = []
+pdf_labels = lha_labels(SCHEME)
 
 # load tables
-n3lo_table_dir = table_dir  # / scheme
-for p in n3lo_table_dir.iterdir():
-    if scheme not in p.stem:
-        continue
-    if "FHMV" in p.stem:
-        fhmv_dfs.append(pd.read_csv(p))
-    # elif "EKO-4mom" in p.stem:
-    #     eko_dfs_4mom.append(pd.read_csv(p))
-    elif "EKO" in p.stem:
-        eko_dfs.append(pd.read_csv(p))
-
-# load NNLO
-if scheme == "FFNS":
-    tab = 14
-elif scheme == "VFNS":
-    tab = 15
-if sv == "central":
-    part = 1
-
-nnlo_central = pd.read_csv(f"{table_dir}/table{tab}-part{part}.csv")
-nnlo_central = nnlo_central[XCUT:]
+eko_dfs = load_n3lo_tables(n3lo_table_dir, SCHEME, approx="EKO")
+fhmv_dfs = load_n3lo_tables(n3lo_table_dir, SCHEME, approx="FHMV")
+nnlo_central = load_nnlo_table(table_dir, SCHEME, SV)
 
 # compute avg and std
-eko_central = np.mean(eko_dfs, axis=0)[XCUT:]
-eko_std = np.std(eko_dfs, axis=0)[XCUT:]
-fhmv_central = np.mean(fhmv_dfs, axis=0)[XCUT:]
-fhmv_std = np.std(fhmv_dfs, axis=0)[XCUT:]
-# eko_4mom_central = np.mean(eko_dfs_4mom, axis=0)[XCUT:]
-# eko_4mom_std = np.std(eko_dfs_4mom, axis=0)[XCUT:]
+eko_res = compute_n3lo_avg_err(eko_dfs)
+fhmv_res = compute_n3lo_avg_err(fhmv_dfs)
+# eko_4mom_res = = compute_n3lo_avg_err(eko_dfs_4mom)
 
+n3lo_dfs = [
+    (eko_res, "aN3LO EKO"),
+    (fhmv_res, "aN3LO FHMV"),
+    # (eko_4mom_res, "aN3LO EKO 4 mom"),
+]
 
-# absolute plots
-fig, axs = plt.subplots(2, 4, figsize=(15, 7))
-fig.suptitle("$Q: \\sqrt{2} \\to 100 \\ GeV$")
-for i, ax in enumerate(
-    axs.reshape(
-        8,
-    )
-):
-    ax.errorbar(
-        xgrid,
-        eko_central[:, i + 1],
-        yerr=eko_std[:, i + 1],
-        fmt="x",
-        label="aN3LO EKO",
-        capsize=5,
-    )
-    # ax.errorbar(
-    #     xgrid,
-    #     eko_4mom_central[:, i + 1],
-    #     yerr=eko_4mom_std[:, i + 1],
-    #     fmt="x",
-    #     label="aN3LO EKO (4 moments)",
-    #     capsize=5,
-    # )
-    ax.errorbar(
-        xgrid,
-        fhmv_central[:, i + 1],
-        yerr=fhmv_std[:, i + 1],
-        fmt="o",
-        label="aN3LO FHMV",
-        capsize=5,
-    )
-    ax.errorbar(xgrid, nnlo_central.values[:, i + 1], fmt=".", label="NNLO")
-    ax.hlines(
-        0,
-        xgrid.min() - xgrid.min() / 3,
-        1,
-        linestyles="dotted",
-        color="black",
-        linewidth=0.5,
-    )
-    if not USE_LINX:
-        ax.set_xscale("log")
-    ax.set_xlabel("$x$")
-    ax.set_ylabel(f"${pdf_labels[i]}$")
-    ax.set_xlim(xgrid.min() - xgrid.min() / 3, 1)
+# PDFs plots
+plot_pdfs(xgrid, n3lo_dfs, nnlo_central, SCHEME, pdf_labels, USE_LINX, plot_dir)
 
-plt.legend()
-plt.tight_layout()
-plt.savefig(f"{plot_dir}/{plot_name}.pdf")
+# relative diff plots
+eko_diff = compute_n3lo_nnlo_diff(eko_res, nnlo_central, REL_DIFF)
+fhmv_diff = compute_n3lo_nnlo_diff(fhmv_res, nnlo_central, REL_DIFF)
+n3lo_dfs = [
+    (eko_diff, "aN3LO EKO"),
+    (fhmv_diff, "aN3LO FHMV"),
+    # (eko_4mom_res, "aN3LO EKO 4 mom"),
+]
 
 # relative, absolute diff plots
-norm = nnlo_central
-if not REL_DIFF:
-    norm = pd.DataFrame(np.full(nnlo_central.shape, 1), columns=nnlo_central.columns)
-eko_diff = (eko_central - nnlo_central) / norm
-eko_diff_std = np.abs(eko_std / norm)
-fhmv_diff = (fhmv_central - nnlo_central) / norm
-fhmv_diff_std = np.abs(fhmv_std / norm)
-# eko_4mom_diff = (eko_4mom_central - nnlo_central) / norm
-# eko_4mom_diff_std = np.abs(eko_4mom_std / norm)
-
-fig, axs = plt.subplots(2, 4, figsize=(15, 7))
-if REL_DIFF:
-    fig.suptitle("Relative difference to NNLO, $Q: \\sqrt{2} \\to 100 \\ GeV$")
-else:
-    fig.suptitle("Absolute difference to NNLO, $Q: \\sqrt{2} \\to 100 \\ GeV$")
-
-for i, ax in enumerate(
-    axs.reshape(
-        8,
-    )
-):
-    ax.errorbar(
-        xgrid,
-        eko_diff.values[:, i + 1],
-        yerr=eko_diff_std.values[:, i + 1],
-        fmt="x",
-        label="aN3LO EKO",
-        capsize=5,
-    )
-    # ax.errorbar(
-    #     xgrid,
-    #     eko_4mom_diff.values[:, i + 1],
-    #     yerr=eko_4mom_diff_std.values[:, i + 1],
-    #     fmt="x",
-    #     label="aN3LO EKO (4 moments)",
-    #     capsize=5,
-    # )
-    ax.errorbar(
-        xgrid,
-        fhmv_diff.values[:, i + 1],
-        yerr=fhmv_diff_std.values[:, i + 1],
-        fmt="o",
-        label="aN3LO FHMV",
-        capsize=5,
-    )
-    ax.hlines(
-        0,
-        xgrid.min() - xgrid.min() / 3,
-        1,
-        linestyles="dotted",
-        color="black",
-        linewidth=0.5,
-    )
-    if not USE_LINX:
-        ax.set_xscale("log")
-    ax.set_xlabel("$x$")
-    ax.set_ylabel(f"${pdf_labels[i]}$")
-    ax.set_xlim(xgrid.min() - xgrid.min() / 3, 1)
-
-plt.legend()
-plt.tight_layout()
-if REL_DIFF:
-    plt.savefig(f"{plot_dir}/{plot_name}_diff.pdf")
-else:
-    plt.savefig(f"{plot_dir}/{plot_name}_abs_diff.pdf")
+plot_diff_to_nnlo(xgrid, n3lo_dfs, SCHEME, pdf_labels, USE_LINX, plot_dir, REL_DIFF)
--- extras/lh_bench_23/plot_bench_evol.py
+++ extras/lh_bench_23/plot_bench_evol.py
@@ -1,211 +1,47 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 from cfg import here, table_dir, xgrid
-
-plot_dir = here / "plots_evol"
-plot_dir.mkdir(exist_ok=True)
+from utils import (
+    compute_n3lo_avg_err,
+    compute_n3lo_nnlo_diff,
+    evol_labels,
+    load_n3lo_tables,
+    load_nnlo_table,
+    plot_diff_to_nnlo,
+    plot_pdfs,
+)
 
 USE_LINX = False
+REL_DIFF = True
+SCHEME = "FFNS"
+SV = "central"
 
-scheme = "VFNS"
-sv = "central"
+plot_dir = here / "plots_evol"
+n3lo_table_dir = table_dir  # / SCHEME
 
-if scheme == "FFNS":
-    pdf_labels = [
-        "V",
-        "V_3",
-        "V_8",
-        "T_3",
-        "T_8",
-        "T_{15}",
-        r"\Sigma",
-        "g",
-    ]
-elif scheme == "VFNS":
-    pdf_labels = [
-        "V",
-        "V_3",
-        "T_3",
-        "T_8",
-        "T_{15}",
-        "T_{24}",
-        r"\Sigma",
-        "g",
-    ]
-
-XCUT = 4 if USE_LINX else 0
-xscale = "linx" if USE_LINX else "logx"
-xgrid = xgrid[XCUT:]
-plot_name = f"lh_n3lo_bench_{scheme}_{xscale}"
-
-
-def rotate_to_evol(df, scheme):
-    if scheme == "FFNS":
-        matrix = [
-            [1, 1, 0, 0, 0, 0, 0, 0],  # V
-            [1, -1, 0, 0, 0, 0, 0, 0],  # V3
-            [1, 1, 0, 0, -2, 0, 0, 0],  # V8
-            [1, -1, -2, 0, 0, 0, 0, 0],  # T3
-            [1, 1, 0, 1, 0, -2, 0, 0],  # T8
-            [1, 1, 0, 1, 0, 1, -3, 0],  # T15
-            [1, 1, 0, 1, 0, 1, 1, 0],  # S
-            [0, 0, 0, 0, 0, 0, 0, 1],  # g
-        ]
-    elif scheme == "VFNS":
-        matrix = [
-            [1, 1, 0, 0, 0, 0, 0, 0],  # V, V8
-            [1, -1, 0, 0, 0, 0, 0, 0],  # V3
-            [1, -1, -2, 0, 0, 0, 0, 0],  # T3
-            [1, 1, 0, 1, -2, 0, 0, 0],  # T8
-            [1, 1, 0, 1, 1, -3, 0, 0],  # T15
-            [1, 1, 0, 1, 1, 1, -4, 0],  # T24
-            [1, 1, 0, 1, 1, 1, 1, 0],  # S
-            [0, 0, 0, 0, 0, 0, 0, 1],  # g
-        ]
-
-    return (matrix @ df.values[:, 1:].T).T
-
-
-fhmv_dfs = []
-eko_dfs = []
-# eko_dfs_4mom = []
+pdf_labels = evol_labels(SCHEME)
 
 # load tables
-for p in table_dir.iterdir():
-    if scheme not in p.stem:
-        continue
-    if "FHMV" in p.stem:
-        fhmv_dfs.append(rotate_to_evol(pd.read_csv(p), scheme))
-    # elif "EKO-4mom" in p.stem:
-    #     eko_dfs_4mom.append(rotate_to_evol(pd.read_csv(p), scheme))
-    elif "EKO" in p.stem:
-        eko_dfs.append(rotate_to_evol(pd.read_csv(p), scheme))
-
-# load NNLO
-if scheme == "FFNS":
-    tab = 14
-elif scheme == "VFNS":
-    tab = 15
-if sv == "central":
-    part = 1
-nnlo_central = pd.read_csv(f"{table_dir}/table{tab}-part{part}.csv")
-nnlo_central = rotate_to_evol(nnlo_central, scheme)[XCUT:]
+eko_dfs = load_n3lo_tables(n3lo_table_dir, SCHEME, approx="EKO", rotate_to_evol=True)
+fhmv_dfs = load_n3lo_tables(n3lo_table_dir, SCHEME, approx="FHMV", rotate_to_evol=True)
+nnlo_central = load_nnlo_table(table_dir, SCHEME, SV, rotate_to_evol=True)
 
 # compute avg and std
-eko_central = np.mean(eko_dfs, axis=0)[XCUT:]
-eko_std = np.std(eko_dfs, axis=0)[XCUT:]
-fhmv_central = np.mean(fhmv_dfs, axis=0)[XCUT:]
-fhmv_std = np.std(fhmv_dfs, axis=0)[XCUT:]
-# eko_4mom_central = np.mean(eko_dfs_4mom, axis=0)[XCUT:]
-# eko_4mom_std = np.std(eko_dfs_4mom, axis=0)[XCUT:]
+eko_res = compute_n3lo_avg_err(eko_dfs)
+fhmv_res = compute_n3lo_avg_err(fhmv_dfs)
 
+n3lo_dfs = [
+    (eko_res, "aN3LO EKO"),
+    (fhmv_res, "aN3LO FHMV"),
+]
 
 # absolute plots
-fig, axs = plt.subplots(2, 4, figsize=(15, 7))
-fig.suptitle("$Q: \\sqrt{2} \\to 100 \\ GeV$")
-for i, ax in enumerate(
-    axs.reshape(
-        8,
-    )
-):
-    ax.errorbar(
-        xgrid,
-        eko_central[:, i],
-        yerr=eko_std[:, i],
-        fmt="x",
-        label="aN3LO EKO",
-        capsize=5,
-    )
-    # ax.errorbar(
-    #     xgrid,
-    #     eko_4mom_central[:, i],
-    #     yerr=eko_4mom_std[:, i],
-    #     fmt="x",
-    #     label="aN3LO EKO (4 moments)",
-    #     capsize=5,
-    # )
-    ax.errorbar(
-        xgrid,
-        fhmv_central[:, i],
-        yerr=fhmv_std[:, i],
-        fmt="o",
-        label="aN3LO FHMV",
-        capsize=5,
-    )
-    ax.errorbar(xgrid, nnlo_central[:, i], fmt=".", label="NNLO")
-    ax.hlines(
-        0,
-        xgrid.min() - xgrid.min() / 3,
-        1,
-        linestyles="dotted",
-        color="black",
-        linewidth=0.5,
-    )
-    if not USE_LINX:
-        ax.set_xscale("log")
-    ax.set_xlabel("$x$")
-    ax.set_ylabel(f"${pdf_labels[i]}$")
-    ax.set_xlim(xgrid.min() - xgrid.min() / 3, 1)
+plot_pdfs(xgrid, n3lo_dfs, nnlo_central, SCHEME, pdf_labels, USE_LINX, plot_dir)
 
-plt.legend()
-plt.tight_layout()
-plt.savefig(f"{plot_dir}/{plot_name}.pdf")
+# relative, absolute diff plots
+eko_diff = compute_n3lo_nnlo_diff(eko_res, nnlo_central, REL_DIFF)
+fhmv_diff = compute_n3lo_nnlo_diff(fhmv_res, nnlo_central, REL_DIFF)
+n3lo_dfs = [
+    (eko_diff, "aN3LO EKO"),
+    (fhmv_diff, "aN3LO FHMV"),
+]
 
-# relative diff plots
-eko_diff = (eko_central - nnlo_central) / nnlo_central
-eko_diff_std = np.abs(eko_std / nnlo_central)
-fhmv_diff = (fhmv_central - nnlo_central) / nnlo_central
-fhmv_diff_std = np.abs(fhmv_std / nnlo_central)
-# eko_4mom_diff = (eko_4mom_central - nnlo_central) / nnlo_central
-# eko_4mom_diff_std = np.abs(eko_4mom_std / nnlo_central)
-
-fig, axs = plt.subplots(2, 4, figsize=(15, 7))
-fig.suptitle("Relative difference to NNLO, $Q: \\sqrt{2} \\to 100 \\ GeV$")
-
-for i, ax in enumerate(
-    axs.reshape(
-        8,
-    )
-):
-    ax.errorbar(
-        xgrid,
-        eko_diff[:, i],
-        yerr=eko_diff_std[:, i],
-        fmt="x",
-        label="aN3LO EKO",
-        capsize=5,
-    )
-    # ax.errorbar(
-    #     xgrid,
-    #     eko_4mom_diff[:, i],
-    #     yerr=eko_4mom_diff_std[:, i],
-    #     fmt="x",
-    #     label="aN3LO EKO (4 moments)",
-    #     capsize=5,
-    # )
-    ax.errorbar(
-        xgrid,
-        fhmv_diff[:, i],
-        yerr=fhmv_diff_std[:, i],
-        fmt="o",
-        label="aN3LO FHMV",
-        capsize=5,
-    )
-    ax.hlines(
-        0,
-        xgrid.min() - xgrid.min() / 3,
-        1,
-        linestyles="dotted",
-        color="black",
-        linewidth=0.5,
-    )
-    if not USE_LINX:
-        ax.set_xscale("log")
-    ax.set_xlabel("$x$")
-    ax.set_ylabel(f"${pdf_labels[i]}$")
-    ax.set_xlim(xgrid.min() - xgrid.min() / 3, 1)
-
-plt.legend()
-plt.tight_layout()
-plt.savefig(f"{plot_dir}/{plot_name}_diff.pdf")
+plot_diff_to_nnlo(xgrid, n3lo_dfs, SCHEME, pdf_labels, USE_LINX, plot_dir, REL_DIFF)
