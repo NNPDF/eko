8f87134ff4e9868de481f06a7483fb780b6771da
giacomomagni <giac.magni@gmail.com>
2023-11-08 09:08:17 +0100

    hard code QED matching to 0

--- src/eko/runner/parts.py
+++ src/eko/runner/parts.py
@@ -88,8 +88,9 @@ def evolve_configs(eko: EKO) -> dict:
         n_integration_cores=ocard.configs.n_integration_cores,
         ModSV=ocard.configs.scvar_method,
         n3lo_ad_variation=tcard.n3lo_ad_variation,
-        matching_order=tcard.matching_order if tcard.matching_order is not None
         # Here order is shifted by one, no QED matching is available so far.
+        matching_order=tcard.matching_order
+        if tcard.matching_order is not None
         else (tcard.order[0] - 1, 0),
     )
 
