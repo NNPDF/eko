d297c7d1150f198901ab41c68c128cf0604568f4
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2024-02-14 13:37:01 +0200

    Add test_expanded_is_linear

--- tests/eko/scale_variations/test_expanded.py
+++ tests/eko/scale_variations/test_expanded.py
@@ -1,6 +1,35 @@
 import numpy as np
 
+from eko import basis_rotation as br
+from eko.couplings import CouplingEvolutionMethod, Couplings, CouplingsInfo
+from eko.kernels import non_singlet, singlet
+from eko.quantities.heavy_quarks import QuarkMassScheme
 from eko.scale_variations import Modes, expanded
+from ekore.anomalous_dimensions.unpolarized.space_like import gamma_ns, gamma_singlet
+
+NF = 4
+Q02 = 1.65**2
+Q12 = 100**2
+EV_METHOD = "truncated"
+
+
+def compute_a_s(q2, order):
+    sc = Couplings(
+        couplings=CouplingsInfo(
+            alphas=0.1181,
+            alphaem=0.007496,
+            scale=91.00,
+            max_num_flavs=4,
+            num_flavs_ref=4,
+        ),
+        order=order,
+        method=CouplingEvolutionMethod.EXPANDED,
+        masses=np.array([0.0, np.inf, np.inf]),
+        hqm_scheme=QuarkMassScheme.POLE,
+        thresholds_ratios=np.array([1.0, 1.0, 1.0]),
+    )
+    # the multiplication for xif2 here it's done explicitly outside
+    return sc.a_s(scale_to=q2)
 
 
 def test_modes():
@@ -85,3 +114,82 @@ def test_valence_sv_dispacher_qed():
             ),
             np.eye(2),
         )
+
+
+def test_expanded_is_linear():
+    r"""Test is linear."""
+    for order in [(1, 0), (2, 0), (3, 0), (4, 0)]:
+        for n in [2.0, 3.0, 10.0]:
+            rel_err_ns = []
+            rel_err_s = []
+            for L in [0.3, 0.5, 0.7]:
+                xif2 = np.exp(L)
+                # compute values of alphas
+                a0 = compute_a_s(Q02, order)
+                a1 = compute_a_s(Q12, order)
+                a1_b = compute_a_s(Q12 * xif2, order)
+                # Non singlet kernels
+                gns = gamma_ns(
+                    order,
+                    br.non_singlet_pids_map["ns+"],
+                    n,
+                    NF,
+                    n3lo_ad_variation=(0, 0, 0, 0, 0, 0, 0),
+                )
+
+                # build scheme B solution
+                ker_b = non_singlet.dispatcher(
+                    order, EV_METHOD, gns, a1, a0, NF, ev_op_iterations=1
+                )
+                sv_b = non_singlet.dispatcher(
+                    order, EV_METHOD, gns, a1_b, a0, NF, ev_op_iterations=1
+                )
+                sv_b = sv_b * expanded.non_singlet_variation(gns, a1_b, order, NF, L)
+
+                rel_err_ns.append(sv_b / ker_b)
+
+                # Singlet kernels
+                gs = gamma_singlet(
+                    order, n, NF, n3lo_ad_variation=(0, 0, 0, 0, 0, 0, 0)
+                )
+
+                # build scheme B solution
+                ker_b = singlet.dispatcher(
+                    order,
+                    EV_METHOD,
+                    gs,
+                    a1,
+                    a0,
+                    NF,
+                    ev_op_iterations=1,
+                    ev_op_max_order=1,
+                )
+                sv_b = singlet.dispatcher(
+                    order,
+                    EV_METHOD,
+                    gs,
+                    a1_b,
+                    a0,
+                    NF,
+                    ev_op_iterations=1,
+                    ev_op_max_order=1,
+                )
+                sv_b = expanded.singlet_variation(gs, a1_b, order, NF, L, 2) @ sv_b
+                rel_err_s.append(sv_b @ np.linalg.inv(ker_b))
+
+            # there must be something
+            for err in rel_err_ns:
+                assert np.abs(err) != 1.0
+            for err in np.array(rel_err_s).flatten():
+                assert np.abs(err) != 1.0
+            # error has to increase
+            np.testing.assert_allclose(
+                rel_err_ns,
+                sorted(rel_err_ns, reverse=True),
+                err_msg=f"{order=},{n=},non-singlet",
+            )
+            np.testing.assert_allclose(
+                rel_err_s,
+                sorted(rel_err_s, key=np.max, reverse=True),
+                err_msg=f"{order=},{n=},singlet",
+            )
