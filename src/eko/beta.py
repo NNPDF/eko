14ccc9058954a9b1354adc235572356c99b33d1a
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2024-10-14 15:48:10 +0300

    Update eko.beta doc

--- src/eko/beta.py
+++ src/eko/beta.py
@@ -1,7 +1,24 @@
-r"""Compute the QCD beta function coefficients.
+r"""Compute the beta function coefficients.
 
-See :doc:`pQCD ingredients </theory/pQCD>`.
+The strong coupling :math:`a_s(\mu^2)` and the electro-magnetic coupling
+:math:`a_{em}(\mu^2)` obey separate, but coupled |RGE|:
+
+.. math::
+
+    \mu^2 \frac{da_s(\mu^2)}{d\mu^2} = \beta_{QCD}^{(n_f)}(a_s(\mu^2), a_{em}(\mu^2))
+        = - \sum_{j=2}\sum_{k=0} \beta_{QCD}^{(j,k),(n_f)}
+                  \left(a_s(\mu^2)\right)^j \left(a_{em}(\mu^2)\right)^k\\
+
+    \mu^2 \frac{da_{em}(\mu^2)}{d\mu^2} = \beta_{QED}^{(n_f)}(a_s(\mu^2), a_{em}(\mu^2))
+        = - \sum_{j=0}\sum_{k=2} \beta_{QED}^{(j,k),(n_f)}
+                  \left(a_s(\mu^2)\right)^j \left(a_{em}(\mu^2)\right)^k
+
+See the :mod:`eko.couplings` module for solutions.
+When considering QED corrections the two |RGE| need two be solved
+simultaneously.
 """
+
+from typing import Tuple
 
 import numba as nb
 
@@ -10,165 +27,155 @@ from .constants import zeta3
 
 
 @nb.njit(cache=True)
-def beta_qcd_as2(nf):
+def beta_qcd_as2(nf: int) -> float:
     r"""Compute the first coefficient of the QCD beta function.
 
-    Implements Eq. (3.1) of :cite:`Herzog:2017ohr`.
+    Implements :eqref:`3.1` of :cite:`Herzog:2017ohr`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_qcd_as2 : float
-            first coefficient of the QCD beta function :math:`\\beta_qcd_as2^{n_f}`
+    float
+        first coefficient of the QCD beta function :math:`\beta_{QCD}^{(2,0),(n_f)}`
     """
-    beta_qcd_as2 = 11.0 / 3.0 * constants.CA - 4.0 / 3.0 * constants.TR * nf
-    return beta_qcd_as2
+    return 11.0 / 3.0 * constants.CA - 4.0 / 3.0 * constants.TR * nf
 
 
 @nb.njit(cache=True)
-def beta_qed_aem2(nf, nl):
+def beta_qed_aem2(nf: int, nl: int) -> float:
     r"""Compute the first coefficient of the QED beta function.
 
-    Implements Eq. (7) of :cite:`Surguladze:1996hx`.
+    Implements :eqref:`7` of :cite:`Surguladze:1996hx`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
-        nl : int
-            number of leptons partecipating to alphaem running
+    nf :
+        number of active flavors
+    nl :
+        number of leptons
 
     Returns
     -------
-        beta_qed_aem2 : float
-            first coefficient of the QED beta function :math:`\\beta_qed_aem2^{n_f}`
+    float
+        first coefficient of the QED beta function :math:`\beta_{QED}^{(0,2),(n_f)}`
     """
     nu = constants.uplike_flavors(nf)
     nd = nf - nu
-    beta_qed_aem2 = (
-        -4.0 / 3 * (nl + constants.NC * (nu * constants.eu2 + nd * constants.ed2))
-    )
-    return beta_qed_aem2
+    return -4.0 / 3 * (nl + constants.NC * (nu * constants.eu2 + nd * constants.ed2))
 
 
 @nb.njit(cache=True)
-def beta_qcd_as3(nf):
+def beta_qcd_as3(nf: int) -> float:
     r"""Compute the second coefficient of the QCD beta function.
 
-    Implements Eq. (3.2) of :cite:`Herzog:2017ohr`.
+    Implements :eqref:`3.2` of :cite:`Herzog:2017ohr`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_qcd_as3 : float
-            second coefficient of the QCD beta function :math:`\\beta_qcd_as3^{n_f}`
+    float
+        second coefficient of the QCD beta function :math:`\beta_{QCD}^{(3,0),(n_f)}`
     """
     TF = constants.TR * nf
     b_ca2 = 34.0 / 3.0 * constants.CA * constants.CA
     b_ca = -20.0 / 3.0 * constants.CA * TF
     b_cf = -4.0 * constants.CF * TF
-    beta_qcd_as3 = b_ca2 + b_ca + b_cf
-    return beta_qcd_as3
+    return b_ca2 + b_ca + b_cf
 
 
 @nb.njit(cache=True)
-def beta_qed_aem3(nf, nl):
+def beta_qed_aem3(nf: int, nl: int) -> float:
     r"""Compute the second coefficient of the QED beta function.
 
-    Implements Eq. (7) of :cite:`Surguladze:1996hx`.
+    Implements :eqref:`7` of :cite:`Surguladze:1996hx`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
-        nl : int
-            number of leptons partecipating to alphaem running
+    nf :
+        number of active flavors
+    nl :
+        number of leptons
 
     Returns
     -------
-        beta_qed_aem3 : float
-            second coefficient of the QED beta function :math:`\\beta_qed_aem3^{n_f}`
+    float
+        second coefficient of the QED beta function :math:`\beta_{QED}^{(0,3),(n_f)}`
     """
     nu = constants.uplike_flavors(nf)
     nd = nf - nu
-    beta_qed_aem3 = -4.0 * (
-        nl + constants.NC * (nu * constants.eu2**2 + nd * constants.ed2**2)
-    )
-    return beta_qed_aem3
+    return -4.0 * (nl + constants.NC * (nu * constants.eu2**2 + nd * constants.ed2**2))
 
 
 @nb.njit(cache=True)
-def beta_qcd_as2aem1(nf):
+def beta_qcd_as2aem1(nf: int) -> float:
     r"""Compute the first QED correction of the QCD beta function.
 
-    Implements Eq. (7) of :cite:`Surguladze:1996hx`.
+    Implements :eqref:`7` of :cite:`Surguladze:1996hx`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_as2aem1 : float
-            first QED correction of the QCD beta function :math:`\\beta_as2aem1^{n_f}`
+    float
+        first QED correction of the QCD beta function :math:`\beta_{QCD}^{(2,1),(n_f)}`
     """
     nu = constants.uplike_flavors(nf)
     nd = nf - nu
-    beta_qcd_as2aem1 = -4.0 * constants.TR * (nu * constants.eu2 + nd * constants.ed2)
-    return beta_qcd_as2aem1
+    return -4.0 * constants.TR * (nu * constants.eu2 + nd * constants.ed2)
 
 
 @nb.njit(cache=True)
-def beta_qed_aem2as1(nf):
+def beta_qed_aem2as1(nf: int) -> float:
     r"""Compute the first QCD correction of the QED beta function.
 
-    Implements Eq. (7) of :cite:`Surguladze:1996hx`.
+    Implements :eqref:`7` of :cite:`Surguladze:1996hx`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_aem2as1 : float
-            first QCD correction of the QED beta function :math:`\\beta_aem2as1^{n_f}`
+    float
+        first QCD correction of the QED beta function :math:`\beta_{QED}^{(1,2),(n_f)}`
     """
     nu = constants.uplike_flavors(nf)
     nd = nf - nu
-    beta_qed_aem2as1 = (
+    return (
         -4.0 * constants.CF * constants.NC * (nu * constants.eu2 + nd * constants.ed2)
     )
-    return beta_qed_aem2as1
 
 
 @nb.njit(cache=True)
-def beta_qcd_as4(nf):
+def beta_qcd_as4(nf: int) -> float:
     r"""Compute the third coefficient of the QCD beta function.
 
-    Implements Eq. (3.3) of :cite:`Herzog:2017ohr`.
+    Implements :eqref:`3.3` of :cite:`Herzog:2017ohr`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_qcd_as4 : float
-            third coefficient of the QCD beta function :math:`\\beta_qcd_as4^{n_f}`
+    float
+        third coefficient of the QCD beta function :math:`\beta_{QCD}^{(4,0),(n_f)}`
     """
     TF = constants.TR * nf
-    beta_qcd_as4 = (
+    return (
         2857.0 / 54.0 * constants.CA * constants.CA * constants.CA
         - 1415.0 / 27.0 * constants.CA * constants.CA * TF
         - 205.0 / 9.0 * constants.CF * constants.CA * TF
@@ -176,50 +183,48 @@ def beta_qcd_as4(nf):
         + 44.0 / 9.0 * constants.CF * TF * TF
         + 158.0 / 27.0 * constants.CA * TF * TF
     )
-    return beta_qcd_as4
 
 
 @nb.njit(cache=True)
-def beta_qcd_as5(nf):
+def beta_qcd_as5(nf: int) -> float:
     r"""Compute the fourth coefficient of the QCD beta function.
 
-    Implements Eq. (3.6) of :cite:`Herzog:2017ohr`.
+    Implements :eqref:`3.6` of :cite:`Herzog:2017ohr`.
 
     Parameters
     ----------
-        nf : int
-            number of active flavors
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_qcd_as5 : float
-            fourth coefficient of the QCD beta function :math:`\\beta_qcd_as5^{n_f}`
+    float
+        fourth coefficient of the QCD beta function :math:`\beta_{QCD}^{(5,0),(n_f)}`
     """
-    beta_qcd_as5 = (
+    return (
         149753.0 / 6.0
         + 3564.0 * zeta3
         + nf * (-1078361.0 / 162.0 - 6508.0 / 27.0 * zeta3)
         + nf**2 * (50065.0 / 162.0 + 6472.0 / 81.0 * zeta3)
         + 1093.0 / 729.0 * nf**3
     )
-    return beta_qcd_as5
 
 
 @nb.njit(cache=True)
-def beta_qcd(k, nf):
-    r"""Compute value of a beta_qcd coefficients.
+def beta_qcd(k: Tuple[int, int], nf: int) -> float:
+    r"""Compute QCD beta coefficient.
 
     Parameters
     ----------
-        k : tuple(int, int)
-            perturbative order
-        nf : int
-            number of active flavors
+    k :
+        perturbative orders
+    nf :
+        number of active flavors
 
     Returns
     -------
-        beta_qcd : float
-            beta_qcd_k(nf)
+    float
+        :math:`\beta_{QCD}^{k,(nf)}`
     """
     beta_ = 0
     if k == (2, 0):
@@ -238,22 +243,22 @@ def beta_qcd(k, nf):
 
 
 @nb.njit(cache=True)
-def beta_qed(k, nf, nl):
-    r"""Compute value of a beta_qed coefficients.
+def beta_qed(k: Tuple[int, int], nf: int, nl: int) -> float:
+    r"""Compute QED beta coefficient.
 
     Parameters
     ----------
-        k : tuple(int, int)
-            perturbative order
-        nf : int
-            number of active flavors
-        nl : int
-            number of leptons partecipating to alphaem running
+    k :
+        perturbative order
+    nf :
+        number of active flavors
+    nl :
+        number of leptons
 
     Returns
     -------
-        beta_qed : float
-            beta_qed_k(nf)
+    float
+        :math:`\beta_{QED}^{k,(nf)}`
     """
     beta_ = 0
     if k == (0, 2):
@@ -268,40 +273,40 @@ def beta_qed(k, nf, nl):
 
 
 @nb.njit(cache=True)
-def b_qcd(k, nf):
-    r"""Compute b_qcd coefficient.
+def b_qcd(k: Tuple[int, int], nf: int) -> float:
+    r"""Compute normalized QCD beta coefficient.
 
     Parameters
     ----------
-        k : tuple(int, int)
-            perturbative order
-        nf : int
-            number of active flavors
+    k :
+        perturbative order
+    nf :
+        number of active flavors
 
     Returns
     -------
-        b_qcd : float
-            b_qcd_k(nf)
+    float
+        :math:`b_{QCD}^{k,(nf)} = \beta_{QCD}^{k,(nf)} / \beta_{QCD}^{(2,0),(nf)}`
     """
     return beta_qcd(k, nf) / beta_qcd((2, 0), nf)
 
 
 @nb.njit(cache=True)
-def b_qed(k, nf, nl):
-    r"""Compute b_qed coefficient.
+def b_qed(k: Tuple[int, int], nf: int, nl: int) -> float:
+    r"""Compute normalized QED beta coefficient.
 
     Parameters
     ----------
-        k : tuple(int, int)
-            perturbative order
-        nf : int
-            number of active flavors
-        nl : int
-            number of leptons partecipating to alphaem running
+    k :
+        perturbative order
+    nf :
+        number of active flavors
+    nl :
+        number of leptons
 
     Returns
     -------
-        b_qed : float
-            b_qed_k(nf)
+    float
+        :math:`b_{QED}^{k,(nf)} = \beta_{QED}^{k,(nf)} / \beta_{QED}^{(0,2),(nf)}`
     """
     return beta_qed(k, nf, nl) / beta_qed((0, 2), nf, nl)
