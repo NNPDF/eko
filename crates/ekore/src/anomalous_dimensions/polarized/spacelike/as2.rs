d4377e3c2ef13d9561fce76d8b4765adf989adba
giacomomagni <giac.magni@gmail.com>
2024-10-21 13:05:52 +0200

    pre-commit

--- crates/ekore/src/anomalous_dimensions/polarized/spacelike/as2.rs
+++ crates/ekore/src/anomalous_dimensions/polarized/spacelike/as2.rs
@@ -128,8 +128,8 @@ pub fn gamma_singlet(c: &mut Cache, nf: u8) -> [[Complex<f64>; 2]; 2] {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use super::super::as1::gamma_gq as as1_gamma_gq;
+    use super::*;
     use crate::harmonics::cache::Cache;
     use crate::{assert_approx_eq_cmplx, cmplx};
     use num::complex::Complex;
@@ -199,7 +199,7 @@ mod tests {
         const N: Complex<f64> = cmplx!(1., 0.);
         let mut c = Cache::new(N);
         let me_ps = gamma_ps(&mut c, NF);
-        let as1_gq = - 2.0 * (NF as f64) * as1_gamma_gq(&mut c, NF);
+        let as1_gq = -2.0 * (NF as f64) * as1_gamma_gq(&mut c, NF);
         assert_approx_eq_cmplx!(f64, me_ps, as1_gq);
         // TODO: activate this test once the beta function will be available
         // let me_gg = gamma_gg(&mut c, NF);
