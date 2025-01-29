e343d0c26e9c35eaed04859ff1f3cd8d62c1df98
Eva Groenendijk <evagroenendijk@MacBook-Air-van-Eva-3.local>
2025-01-29 09:13:02 +0100

    Revert "Improved test legacy script for v0.14"
    This reverts commit b57186ce498dd3837d64940343576fb09a27a331.

--- download-test-assets.sh
+++
@@ -1,8 +0,0 @@
-# upload: scp v0.15.tar nnpdf@data.nnpdf.science:WEB/eko/test-data
-
-curl -s -C - -o './tests/data/v0.15.tar' 'https://data.nnpdf.science/eko/test-data/v0.15.tar'
-curl -s -C - -o './tests/data/v0.13.tar' 'https://data.nnpdf.science/eko/test-data/ekov013.tar'
-curl -s -C - -o './tests/data/v0.14.tar' 'https://data.nnpdf.science/eko/test-data/ekov014.tar'
-
-
-
