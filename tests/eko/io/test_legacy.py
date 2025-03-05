4c07cc8d26ddb55fab521fc38489d41b6d10445b
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2025-03-05 10:55:24 +0200

    Polish test_legacy more

--- tests/eko/io/test_legacy.py
+++ tests/eko/io/test_legacy.py
@@ -21,12 +21,13 @@ EP = (10000.0, 4)
 def test_read_legacy():
     for name in ["v0.13.tar", "v0.14.tar", "v0.0.tar"]:
         with eko.EKO.read(TEST_DATA_DIR / name) as evolution_operator:
+            # Check the cards
             assert isinstance(
                 evolution_operator.theory_card, eko.io.runcards.TheoryCard
-            )  # Check that theory card is read as theory card
+            )
             assert isinstance(
                 evolution_operator.operator_card, eko.io.runcards.OperatorCard
-            )  # Check that operator card is read as operator card
+            )
 
             # Check if the operator has the correct dimensions
             with evolution_operator.operator(EP) as op:
@@ -43,11 +44,10 @@ def test_read_legacy():
         )
 
         # Make the test
-        pdf_test = {}
-        pdf_bench = {}
         for flav in br.flavor_basis_pids:
-            pdf_test[flav] = evolved_pdfs[EP][flav]
-            pdf_bench[flav] = lha_benchmark["values"][EP[0]][flav]
             np.testing.assert_allclose(
-                pdf_test[flav], (pdf_bench[flav] / x_grid), rtol=5e-4, atol=5e-6
+                evolved_pdfs[EP][flav],
+                (lha_benchmark["values"][EP[0]][flav] / x_grid),
+                rtol=5e-4,
+                atol=5e-6,
             )
