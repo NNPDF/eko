8e3324c993b5886b2e1c70aa62b7aada304254b5
Eva Groenendijk <evagroenendijk@MacBook-Air-van-Eva-9.local>
2025-05-08 10:45:25 +0200

    Rename test objects to data version

--- tests/data/assets.sh
+++ tests/data/assets.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
 # This script downloads the test assets to run both Rust and Python unit tests.
 # The script is to be executed in the root directory of the repository.
 
@@ -9,10 +8,10 @@ PYDATADIR="tests/data/"
 RUSTDATADIR="crates/dekoder/tests/data/"
 
 # download from the server
-curl -s -C - -o "./${PYDATADIR}v0.13.tar" "${URL}v0.13.tar"
-curl -s -C - -o "./${PYDATADIR}v0.14.tar" "${URL}v0.14.tar"
-curl -s -C - -o "./${PYDATADIR}v0.15.tar" "${URL}v0.15.tar"
+curl -s -C - -o "./${PYDATADIR}v1-0.13.tar" "${URL}v1-0.13.tar"
+curl -s -C - -o "./${PYDATADIR}v1-0.14.tar" "${URL}v1-0.14.tar"
+curl -s -C - -o "./${PYDATADIR}v3.tar" "${URL}v3.tar"
 # expose to Rust
-if [ ! -f "./${RUSTDATADIR}v0.15.tar" ]; then
-    ln -s "./../../../../${PYDATADIR}v0.15.tar" "./${RUSTDATADIR}"
+if [ ! -f "./${RUSTDATADIR}v3.tar" ]; then
+    ln -s "./../../../../${PYDATADIR}v3.tar" "./${RUSTDATADIR}"
 fi
