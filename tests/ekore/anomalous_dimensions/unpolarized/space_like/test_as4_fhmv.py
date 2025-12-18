extend tests and fix minor signs

tests/ekore/anomalous_dimensions/unpolarized/space_like/test_as4_fhmv.py
@@ -170,11 +170,11 @@ def test_vogt_parametriztions():
     for N in [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]:
         cache = h.cache.reset()
         for variation in [0, 1, 2]:
-            for nf in [3, 4, 5]:
+            for nf in [3, 4, 5, 6]:
                 np.testing.assert_allclose(
                     gps.gamma_ps(N, nf, cache, variation),
                     qq3ps_moment(N, nf),
-                    rtol=4e-4,
+                    rtol=4e-4 if nf != 6 else 2e-3,
                 )
                 np.testing.assert_allclose(
                     gqg.gamma_qg(N, nf, cache, variation),
@@ -190,7 +190,7 @@ def test_vogt_parametriztions():
                     np.testing.assert_allclose(
                         ggq.gamma_gq(N, nf, cache, variation),
                         gq3_moment(N, nf),
-                        rtol=4e-4,
+                        rtol=4e-4 if nf != 6 else 2e-3,
                     )
 
     with pytest.raises(NotImplementedError):