edbe7c20cf288d612a317dfb827ae8edfbdb5fdf
AkshatRai07 <sanamrai2007@gmail.com>
2026-06-08 19:52:11 +0530

    Replaced OME Ekore with ekors

--- crates/eko/src/lib.rs
+++ crates/eko/src/lib.rs
@@ -4,6 +4,9 @@ use ekore::anomalous_dimensions::polarized::spacelike as ad_ps;
 use ekore::anomalous_dimensions::unpolarized::spacelike as ad_us;
 // use ekore::anomalous_dimensions::unpolarized::timelike as ad_ut; TODO
 use ekore::harmonics::cache::Cache;
+// use ekore::operator_matrix_elements::polarized::spacelike as ome_ps; TODO
+use ekore::operator_matrix_elements::unpolarized::spacelike as ome_us;
+// use ekore::operator_matrix_elements::unpolarized::timelike as ome_ut; TODO
 use num::Complex;
 use numpy::{IntoPyArray, PyArray1};
 use pyo3::prelude::*;
@@ -83,6 +86,9 @@ type GammaQCDSinglet = PyResult<(Py<PyArray1<f64>>, Py<PyArray1<f64>>)>;
 
 /// Type for qcd gamma ns return
 type GammaQCDNS = PyResult<(Py<PyArray1<f64>>, Py<PyArray1<f64>>)>;
+
+/// Type for OME return values.
+type OmeResult = PyResult<(Py<PyArray1<f64>>, Py<PyArray1<f64>>)>;
 
 // TODO: once the Rust kernels grows we take be the computation of the integration path back to here
 // in the old implementation it was ~
@@ -182,14 +188,106 @@ pub fn qcd_gamma_ns(
     ))
 }
 
+/// Compute the singlet operator matrix element (OME) array in Mellin space.
+///
+/// # Errors
+/// Returns a `PyNotImplementedError` in the following cases:
+/// * Polarized time-like (not implemented).
+/// * Polarized spacelike (not yet implemented).
+/// * Unpolarized time-like (not yet implemented).
+#[pyfunction]
+#[allow(clippy::too_many_arguments)]
+pub fn ome_singlet(
+    py: Python<'_>,
+    is_polarized: bool,
+    is_time_like: bool,
+    order_qcd: usize,
+    re_n: f64,
+    im_n: f64,
+    nf: u8,
+    l: f64,
+    _is_msbar: bool,
+) -> OmeResult {
+    if is_polarized && is_time_like {
+        return Err(pyo3::exceptions::PyNotImplementedError::new_err(
+            "Polarized time-like OME singlet is not implemented",
+        ));
+    }
+    if is_polarized {
+        return Err(pyo3::exceptions::PyNotImplementedError::new_err(
+            "Polarized spacelike OME singlet is not yet implemented in Rust",
+        ));
+    }
+    if is_time_like {
+        return Err(pyo3::exceptions::PyNotImplementedError::new_err(
+            "Unpolarized time-like OME singlet is not yet implemented in Rust",
+        ));
+    }
+    let mut c = Cache::new(Complex::new(re_n, im_n));
+    let raw = unravel::<3>(ome_us::A_singlet(order_qcd, &mut c, nf, l), order_qcd);
+    Ok((
+        raw.re.into_pyarray_bound(py).unbind(),
+        raw.im.into_pyarray_bound(py).unbind(),
+    ))
+}
+
+/// Compute the non-singlet operator matrix element (OME) array **A** in Mellin space.
+///
+/// # Errors
+/// Returns a `PyNotImplementedError` in the following cases:
+/// * Polarized time-like (not implemented).
+/// * Polarized spacelike (not yet implemented).
+/// * Unpolarized time-like (not yet implemented).
+#[pyfunction]
+#[allow(clippy::too_many_arguments)]
+pub fn ome_non_singlet(
+    py: Python<'_>,
+    is_polarized: bool,
+    is_time_like: bool,
+    matching_order_qcd: usize,
+    re_n: f64,
+    im_n: f64,
+    nf: u8,
+    l: f64,
+) -> OmeResult {
+    if is_polarized && is_time_like {
+        return Err(pyo3::exceptions::PyNotImplementedError::new_err(
+            "Polarized time-like OME non-singlet is not implemented",
+        ));
+    }
+    if is_polarized {
+        return Err(pyo3::exceptions::PyNotImplementedError::new_err(
+            "Polarized spacelike OME non-singlet is not yet implemented in Rust",
+        ));
+    }
+    if is_time_like {
+        return Err(pyo3::exceptions::PyNotImplementedError::new_err(
+            "Unpolarized time-like OME non-singlet is not yet implemented in Rust",
+        ));
+    }
+    let mut c = Cache::new(Complex::new(re_n, im_n));
+    let raw = unravel::<2>(
+        ome_us::A_non_singlet(matching_order_qcd, &mut c, nf, l),
+        matching_order_qcd,
+    );
+    Ok((
+        raw.re.into_pyarray_bound(py).unbind(),
+        raw.im.into_pyarray_bound(py).unbind(),
+    ))
+}
+
 /// Python extension module exposing EKO's Rust kernels.
 ///
 /// Currently exposes:
 /// - [`qcd_gamma_singlet`]: QCD singlet anomalous dimension matrix in Mellin space
 /// - [`qcd_gamma_ns`]: QCD non-singlet anomalous dimension array in Mellin space
+/// - [`ome_singlet`]: Singlet OME in Mellin space
+/// - [`ome_non_singlet`]: Non Singlet OME in Mellin space
 #[pymodule]
 fn ekors(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(qcd_gamma_singlet, m)?)?;
     m.add_function(wrap_pyfunction!(qcd_gamma_ns, m)?)?;
+    m.add_function(wrap_pyfunction!(ome_singlet, m)?)?;
+    m.add_function(wrap_pyfunction!(ome_non_singlet, m)?)?;
     Ok(())
 }
