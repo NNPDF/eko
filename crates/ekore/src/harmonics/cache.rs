77fef21e2e45a85decb85aa90ba8594a146a6280
AkshatRai07 <sanamrai2007@gmail.com>
2026-07-20 21:21:42 +0530

    Removed hidden fn ref

--- crates/ekore/src/harmonics/cache.rs
+++ crates/ekore/src/harmonics/cache.rs
@@ -81,7 +81,7 @@ const CACHE_SIZE: usize = 20;
 /// Hold all cached values.
 ///
 /// We use implementations from [\[Vogt:2004ns\]](crate::bib::Vogt2004ns) [\[KOLBIG1972221\]](crate::bib::KOLBIG1972221)
-/// [CERNlib](http://cernlib.web.cern.ch/cernlib/), name from [\[MuselliPhD\]](crate::bib::MuselliPhD)
+/// [CERNlib](http://cernlib.web.cern.ch/cernlib/)
 pub struct Cache {
     /// Mellin N
     n: Complex<f64>,
